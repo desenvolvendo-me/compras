d0d49332f091b89df01508841cab275f167a3f3e
Dmitry Nix <dmitry.nix@gmail.com>
2012-01-26 15:56:34 -0300

    Removed unused attr_modal at DisseminationSource model

--- app/models/dissemination_source.rb
+++ app/models/dissemination_source.rb
@@ -1,8 +1,6 @@
 class DisseminationSource < ActiveRecord::Base
   attr_accessible :description, :communication_source_id
 
-  attr_modal :communication_source
-
   belongs_to :communication_source
 
   validates :description, :communication_source, :presence => true
