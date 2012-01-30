32defcfaf2d3dcdc9c4b21eafed6aa96d7fec2f1
Dmitry Nix <dmitry.nix@gmail.com>
2012-01-30 11:30:41 -0300

    Removed acts_as_nested_set from disemination_source (Because dissemination_source is not created on other forms, but selected)

--- app/models/dissemination_source.rb
+++ app/models/dissemination_source.rb
@@ -5,8 +5,6 @@ class DisseminationSource < ActiveRecord::Base
 
   has_and_belongs_to_many :administractive_acts
 
-  acts_as_nested_set
-
   validates :name, :communication_source, :presence => true
 
   filterize
