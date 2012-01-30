b4dce411da8c0e53f5869eb8cbf669281066abbf
Dmitry Nix <dmitry.nix@gmail.com>
2012-01-30 13:26:28 -0300

    Corrected order of model relations setup (before validations) at communication_source

--- app/models/communication_source.rb
+++ app/models/communication_source.rb
@@ -1,10 +1,10 @@
 class CommunicationSource < ActiveRecord::Base
   attr_accessible :name
 
+  has_many :dissemination_sources
+
   validates :name, :presence => true
   validates :name, :uniqueness => true
-
-  has_many :dissemination_sources
 
   filterize
   orderize
