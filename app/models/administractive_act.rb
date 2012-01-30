3392b3d471a37bb4083358a233811a632fce21f8
Eduardo Resende Pereira <ed.resende@gmail.com>
2012-01-30 10:30:46 -0200

    Administractive Act CRUD: adding habtm relationship with Dissemination Sources.
    [Finishes #24012717]

--- app/models/administractive_act.rb
+++ app/models/administractive_act.rb
@@ -1,14 +1,20 @@
 class AdministractiveAct < ActiveRecord::Base
   attr_accessible :act_number, :type_of_administractive_act_id, :text_legal_nature, :creation_date,
                   :publication_date, :vigor_date, :end_date, :content, :budget_law_percent,
-                  :revenue_antecipation_percent, :authorized_debt_value
+                  :revenue_antecipation_percent, :authorized_debt_value, :dissemination_source_ids
 
   attr_protected :type_of_administractive_act
 
+  attr_accessor :dissemination_source
+
   belongs_to :type_of_administractive_act
+
+  has_and_belongs_to_many :dissemination_sources
 
   orderize :act_number
   filterize
+
+  accepts_nested_attributes_for :dissemination_sources
 
   validates :act_number, :type_of_administractive_act_id, :creation_date, :publication_date, :vigor_date, :end_date,
             :content, :budget_law_percent, :revenue_antecipation_percent, :authorized_debt_value, :presence => true
--- app/models/dissemination_source.rb
+++ app/models/dissemination_source.rb
@@ -3,6 +3,10 @@ class DisseminationSource < ActiveRecord::Base
 
   belongs_to :communication_source
 
+  has_and_belongs_to_many :administractive_acts
+
+  acts_as_nested_set
+
   validates :name, :communication_source, :presence => true
 
   filterize
