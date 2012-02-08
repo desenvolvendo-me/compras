ec7a75b429ec7dbd8ac95d67984b30cdbf1e1945
Marcelo Cajueiro <marcelocajueiro@gmail.com>
2012-02-08 10:46:59 -0200

    Added model test to the cannot_have_more_than_once_item_with_the_same_material validation of PurchaseSolicitation;
    [Finishes #24632063]

--- spec/models/purchase_solicitation_spec.rb
+++ spec/models/purchase_solicitation_spec.rb
@@ -17,9 +17,31 @@ describe PurchaseSolicitation do
   it {should belong_to :organogram }
   it {should have_and_belong_to_many :budget_allocations }
 
-  it { should validate_presence_of :accounting_year }
-  it { should validate_presence_of :request_date }
-  it { should validate_presence_of :delivery_location_id }
-  it { should validate_presence_of :responsible_id }
-  it { should validate_presence_of :kind }
+  context "validations" do
+    it { should validate_presence_of :accounting_year }
+    it { should validate_presence_of :request_date }
+    it { should validate_presence_of :delivery_location_id }
+    it { should validate_presence_of :responsible_id }
+    it { should validate_presence_of :kind }
+
+    it "the items with the same material should be invalid except the first" do
+      item_one = subject.items.build(:material_id => 1)
+      item_two = subject.items.build(:material_id => 1)
+
+      subject.valid?
+
+      item_one.errors.messages[:material_id].should be_nil
+      item_two.errors.messages[:material_id].should include "não é permitido adicionar mais de um item com o mesmo material."
+    end
+
+    it "the items with the different material should be valid" do
+      item_one = subject.items.build(:material_id => 1)
+      item_two = subject.items.build(:material_id => 2)
+
+      subject.valid?
+
+      item_one.errors.messages[:material_id].should be_nil
+      item_two.errors.messages[:material_id].should be_nil
+    end
+  end
 end
