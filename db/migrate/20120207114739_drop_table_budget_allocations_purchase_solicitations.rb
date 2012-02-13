3e136938f7a6041aff295393bb30dca3d17da43a
Eduardo Resende Pereira <ed.resende@gmail.com>
2012-02-13 20:57:52 -0200

    Fixing migration to use symbols instead of strings

--- db/migrate/20120207114739_drop_table_budget_allocations_purchase_solicitations.rb
+++ db/migrate/20120207114739_drop_table_budget_allocations_purchase_solicitations.rb
@@ -1,6 +1,6 @@
 class DropTableBudgetAllocationsPurchaseSolicitations < ActiveRecord::Migration
   def change
-    remove_foreign_key "purchase_solicitations", "budget_allocations"
+    remove_foreign_key :purchase_solicitations, :budget_allocations
 
     drop_table :budget_allocations_purchase_solicitations
   end
