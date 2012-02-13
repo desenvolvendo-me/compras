2803cc0c77fdd4a69e5ed04209044077de3eb553
Eduardo Resende Pereira <ed.resende@gmail.com>
2012-02-13 11:26:05 -0200

    Improving capybara click_button helper to be able to accept confirmations.
    Signed-off-by: Gabriel Sobrinho <gabriel.sobrinho@gmail.com>

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -51,6 +51,26 @@ module Capybara
           driver.execute_script 'this.confirm = this._confirm'
         end
       end
+
+      # Finds a link by id or text and clicks it. Also looks at image alt text inside the link.
+      #
+      # You can confirm a javascript dialog using the confirm option:
+      #
+      #   click_link 'Destroy', :confirm => true
+      def click_button(locator, options = {})
+        confirm = options.delete(:confirm)
+
+        if confirm
+          driver.execute_script 'this._confirm = this.confirm'
+          driver.execute_script "this.confirm = function (message) { return message == #{confirm.to_json} }"
+        end
+
+        super(locator)
+
+        if confirm
+          driver.execute_script 'this.confirm = this._confirm'
+        end
+      end
     end
   end
 end
