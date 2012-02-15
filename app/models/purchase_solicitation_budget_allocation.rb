b47cd4ca19d62d1dcae9bc32ab6fd428c00fa7df
Marcelo Cajueiro <marcelocajueiro@gmail.com>
2012-02-15 08:38:18 -0200

    Removed unnecessary "attr_accessor" from purchase solicitation budget allocation model;

--- app/models/purchase_solicitation_budget_allocation.rb
+++ app/models/purchase_solicitation_budget_allocation.rb
@@ -2,8 +2,6 @@ class PurchaseSolicitationBudgetAllocation < ActiveRecord::Base
   attr_accessible :purchase_solicitation_id, :budget_allocation_id, :estimated_value, :blocked
   attr_accessible :economic_classification_of_expenditure_id
 
-  attr_accessor :budget_allocation_code
-
   validates :budget_allocation_id, :estimated_value, :presence => true
 
   belongs_to :purchase_solicitation
