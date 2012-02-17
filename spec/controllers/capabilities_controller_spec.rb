cbe4efcd2883f155ff7f2ece2c583abdaf9f59ac
Eduardo Resende Pereira <ed.resende@gmail.com>
2012-02-17 18:40:46 -0200

    Capability: Adding test for source assignment on action create
    [Finishes #25106507]

--- spec/controllers/capabilities_controller_spec.rb
+++ spec/controllers/capabilities_controller_spec.rb
@@ -1,4 +1,14 @@
 require 'spec_helper'
 
 describe CapabilitiesController do
+  before do
+    controller.stub(:authenticate_user!)
+    controller.stub(:authorize_resource!)
+  end
+
+  it 'uses manual as default value for source' do
+    post :create
+
+    assigns(:capability).source.should eq Source::MANUAL
+  end
 end
