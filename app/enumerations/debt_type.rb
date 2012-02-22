8d61717a48b77d622ef7223d983e16a0bc3d498b
Dmitry Nix <dmitry.nix@gmail.com>
2012-02-22 17:34:56 -0300

    Using project identation at DebtType enumeration

--- app/enumerations/debt_type.rb
+++ app/enumerations/debt_type.rb
@@ -1,5 +1,5 @@
 class DebtType < EnumerateIt::Base
-  associate_values :nothing => 0,
-    :contract => 1,
-    :secutities => 2
+  associate_values :nothing    => 0,
+                   :contract   => 1,
+                   :secutities => 2
 end
