82d1784487aaf0fffee57cc4466d766f60d48999
Eduardo Resende Pereira <ed.resende@gmail.com>
2012-02-24 20:16:31 -0200

    Fixing order on Function model

--- app/models/function.rb
+++ app/models/function.rb
@@ -1,16 +1,17 @@
 class Function < ActiveRecord::Base
   attr_accessible :code, :administractive_act_id, :description
 
-  delegate :vigor_date, :to => :administractive_act, :allow_nil => true, :prefix => true
-
-  orderize :code
-  filterize
-
   belongs_to :administractive_act
+
   has_many :subfunctions, :dependent => :restrict
+
+  delegate :vigor_date, :to => :administractive_act, :allow_nil => true, :prefix => true
 
   validates :code, :presence => true, :uniqueness => true, :numericality => true
   validates :description, :presence => true
+
+  orderize :code
+  filterize
 
   def to_s
     "#{code} - #{description}"
