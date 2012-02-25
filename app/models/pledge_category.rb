989edea7e8602e1fab2b3608908f4fa825ed3be4
Eduardo Resende Pereira <ed.resende@gmail.com>
2012-02-25 17:57:30 -0200

    Correcting order on Pledge category model

--- app/models/pledge_category.rb
+++ app/models/pledge_category.rb
@@ -1,16 +1,16 @@
 class PledgeCategory < ActiveRecord::Base
   attr_accessible :description, :status
 
-  orderize :description
-  filterize
+  has_enumeration_for :status, :create_helpers => true, :with => PledgeCategoryStatus
+  has_enumeration_for :source, :create_helpers => true
 
   has_many :pledges, :dependent => :restrict
 
   validates :description, :presence => true, :uniqueness => true
   validates :status, :presence => true
 
-  has_enumeration_for :status, :create_helpers => true, :with => PledgeCategoryStatus
-  has_enumeration_for :source, :create_helpers => true
+  orderize :description
+  filterize
 
   def to_s
     description
