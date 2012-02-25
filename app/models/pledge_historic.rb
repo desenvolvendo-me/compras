d69b273e27432e1b238ba9b603714a1816d097ce
Eduardo Resende Pereira <ed.resende@gmail.com>
2012-02-25 17:03:26 -0200

    correcting order on Pledge Historic model

--- app/models/pledge_historic.rb
+++ app/models/pledge_historic.rb
@@ -1,14 +1,15 @@
 class PledgeHistoric < ActiveRecord::Base
   attr_accessible :description, :entity_id, :year
 
-  orderize :description
-  filterize
-
   belongs_to :entity
+
   has_many :pledges, :dependent => :restrict
 
   validates :description, :entity, :year, :presence => true
   validates :year, :mask => "9999"
+
+  orderize :description
+  filterize
 
   def to_s
     description
