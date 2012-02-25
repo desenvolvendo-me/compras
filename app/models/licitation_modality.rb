b2ab2b64214cbe805826dac4ed8c5b12760c0b5e
Eduardo Resende Pereira <ed.resende@gmail.com>
2012-02-25 17:08:07 -0200

    Correcting order on licitation modality model

--- app/models/licitation_modality.rb
+++ app/models/licitation_modality.rb
@@ -1,21 +1,21 @@
 class LicitationModality < ActiveRecord::Base
   attr_accessible :administractive_act_id, :description, :initial_value, :final_value
 
-  delegate :publication_date, :to => :administractive_act, :prefix => true, :allow_nil => true
-
   belongs_to :administractive_act
+
   has_many :pledges, :dependent => :restrict
 
-  orderize :description
-  filterize
+  delegate :publication_date, :to => :administractive_act, :prefix => true, :allow_nil => true
 
   validates :description, :administractive_act, :presence => true
   validates :initial_value, :presence => true, :numericality => true
   validates :final_value, :presence => true
   validates :final_value, :numericality => true
   validates :final_value, :numericality => { :greater_than_or_equal_to => :initial_value, :message => :should_not_be_less_than_initial_value }
-
   validates :initial_value, :uniqueness => { :scope => :final_value, :message => :initial_and_final_value_range_taken }
+
+  orderize :description
+  filterize
 
   def to_s
     description
