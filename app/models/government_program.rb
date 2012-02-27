4bfe89feb91084fbcaf372de20e2b5ad80158c5a
Dmitry Nix <dmitry.nix@gmail.com>
2012-02-27 08:25:50 -0300

    Changed position of has_enumeration_for at government_program model

--- app/models/government_program.rb
+++ app/models/government_program.rb
@@ -1,10 +1,10 @@
 class GovernmentProgram < ActiveRecord::Base
   attr_accessible :entity_id, :year, :description, :status
 
+  has_enumeration_for :status
+
   belongs_to :entity
   has_many :budget_allocations, :dependent => :restrict
-
-  has_enumeration_for :status
 
   validates :entity, :year, :description, :status, :presence => true
   validates :year, :mask => '9999'
