f7b69428f00a0051894b6faa36a318da7ae572f7
Eduardo Resende Pereira <ed.resende@gmail.com>
2012-02-27 22:27:16 -0300

    Reserve allocation type (Seed): Changing transaction to englobe all proccess.

--- db/seeds/reserve_allocation_types.rb
+++ db/seeds/reserve_allocation_types.rb
@@ -1,6 +1,6 @@
 # encoding: utf-8
-["Comum", "Licitação", "Cotas mensais", "Limitações de empenho"].each do |description|
-  ActiveRecord::Base.transaction do
+ActiveRecord::Base.transaction do
+  ["Comum", "Licitação", "Cotas mensais", "Limitações de empenho"].each do |description|
     ReserveAllocationType.create!(:description => description, :status => Status::ACTIVE)
   end
 end
