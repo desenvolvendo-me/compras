b589119e4f4766b5d0b464ac47ccb64ae8d91243
Dmitry Nix <dmitry.nix@gmail.com>
2012-03-06 10:58:36 -0300

    Add mising spec to ensure default value to DirectPurchasesController

---
+++ spec/controllers/direct_purchases_controller_spec.rb
@@ -0,0 +1,13 @@
+require 'spec_helper'
+
+describe DirectPurchasesController do
+  before do
+    sign_in User.make!(:sobrinho_as_admin_and_employee)
+  end
+
+  it 'uses current employee as default value for employee' do
+    get :new
+
+    assigns(:direct_purchase).employee.should eq controller.current_user.employee
+  end
+end
