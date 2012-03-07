7e0b313ab90989d951d573e9dc090adb67bcde72
Eduardo Resende Pereira <ed.resende@gmail.com>
2012-03-07 09:38:45 -0300

    Adding attr_modal to ReserveFund
    [Finishes #26022763]

--- app/models/reserve_fund.rb
+++ app/models/reserve_fund.rb
@@ -4,6 +4,9 @@ class ReserveFund < ActiveRecord::Base
   attr_accessible :licitation_modality_id, :creditor_id, :status, :date, :historic
   attr_accessible :licitation, :process
 
+  attr_modal :licitation_modality_id, :creditor_id, :status
+  attr_modal :entity_id, :year, :budget_allocation_id, :reserve_allocation_type_id
+
   attr_accessor :licitation, :process
 
   has_enumeration_for :status, :with => ReserveFundStatus, :create_helpers => true
