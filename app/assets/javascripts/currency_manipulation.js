b01294bc315ae6ad4db06f195fb0c8fc031493d4
Eduardo Resende Pereira <ed.resende@gmail.com>
2012-03-09 09:21:16 -0300

    Improving parsePtBrFloat function

--- app/assets/javascripts/currency_manipulation.js
+++ app/assets/javascripts/currency_manipulation.js
@@ -3,5 +3,5 @@ function floatToPtBrString(value){
 }
 
 function parsePtBrFloat(value){
-  return parseFloat(value.replace(/\./gi,'').replace(/\,/,'.'));
+  return parseFloat(value.replace(/\./g,'').replace(',','.'));
 }
