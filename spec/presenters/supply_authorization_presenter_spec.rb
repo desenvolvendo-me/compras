14437e0651e939b243d60afc9c3ec03edde623e5
Marcelo Cajueiro <marcelocajueiro@gmail.com>
2012-03-15 13:27:51 -0300

    Minor refactor on the supply authorization presenter spec and fix to receive helpers class and routes class as nil;

--- spec/presenters/supply_authorization_presenter_spec.rb
+++ spec/presenters/supply_authorization_presenter_spec.rb
@@ -3,14 +3,22 @@ require 'presenter_helper'
 require 'app/presenters/supply_authorization_presenter'
 
 describe SupplyAuthorizationPresenter do
+  subject do
+    described_class.new(supply_authorization, nil, nil)
+  end
+
+  let :supply_authorization do
+    double('SupplyAuthorization')
+  end
+
   context '#message' do
     it 'show singular message' do
-      subject = described_class.new(double(:items_count => 1))
+      subject.stub(:items_count => 1)
       subject.message.should eq 'Pedimos fornecer-nos o material e ou execução do serviço abaixo discriminado, respeitando as especificações e condições constantes nesta autorização de fornecimento.'
     end
 
     it 'show pluralized message' do
-      subject = described_class.new(double(:items_count => 2))
+      subject.stub(:items_count => 2)
       subject.message.should eq 'Pedimos fornecer-nos os materiais e ou execução dos serviços abaixo discriminados, respeitando as especificações e condições constantes nesta autorização de fornecimento.'
     end
   end
