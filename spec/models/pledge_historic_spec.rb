6f6377e98aaefd0fb2ffa020d3d1c12e41d0fecf
Pedro Brasileiro <pedrobrasileiro@gmail.com>
2012-03-16 16:41:35 -0300

    Fix Pledge Historic Test
    [#25628299]

--- spec/models/pledge_historic_spec.rb
+++ spec/models/pledge_historic_spec.rb
@@ -13,28 +13,16 @@ describe PledgeHistoric do
 
   it { should validate_presence_of :description }
 
-  it "year and entity nulls permitted to Source::DEFAULT" do
-    subject.source = Source::DEFAULT
-    subject.description = 'Description test'
-    subject.year = nil
-    subject.entity = nil
-
-    subject.should be_valid
-    
-    subject.errors.messages[:year].should be_nil
-    subject.errors.messages[:entity].should be_nil
-  end
-
-  it "year and entity nulls not permitted to Source::MANUAL" do
+  it "should validate presence of year and entity dependent of source" do
     subject.source = Source::MANUAL
-    subject.description = 'Description test'
-    subject.year = nil
-    subject.entity = nil
-
-    subject.should_not be_valid
     
-    subject.errors.messages[:year].should_not be_nil
-    subject.errors.messages[:entity].should_not be_nil
+    subject.should validate_presence_of(:year)
+    subject.should validate_presence_of(:entity)
+    
+    subject.source = Source::DEFAULT
+
+    subject.should_not validate_presence_of(:year)
+    subject.should_not validate_presence_of(:entity)
   end
   
   it { should allow_value('2012').for(:year) }
