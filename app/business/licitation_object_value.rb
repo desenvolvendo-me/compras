0c3c0300f546bf8bce4a7cfc2fa7670006008483
Eduardo Resende Pereira <ed.resende@gmail.com>
2012-03-20 09:05:42 -0300

    Licitation Object Value PORO: refactory - case not needed

--- app/business/licitation_object_value.rb
+++ app/business/licitation_object_value.rb
@@ -8,40 +8,6 @@ class LicitationObjectValue
   end
 
   def value
-    case modality
-    when :purchase
-      case limit
-      when :licitation_exemption
-        object.purchase_licitation_exemption
-      when :invitation_letter
-        object.purchase_invitation_letter
-      when :taking_price
-        object.purchase_taking_price
-      when :public_concurrency
-        object.purchase_public_concurrency
-      end
-
-    when :build
-      case limit
-      when :licitation_exemption
-        object.build_licitation_exemption
-      when :invitation_letter
-        object.build_invitation_letter
-      when :taking_price
-        object.build_taking_price
-      when :public_concurrency
-        object.build_public_concurrency
-      end
-
-    when :special
-      case limit
-      when :auction
-        object.special_auction
-      when :unenforceability
-        object.special_unenforceability
-      when :contest
-        object.special_contest
-      end
-    end
+    object.send("#{modality}_#{limit}".to_sym)
   end
 end
