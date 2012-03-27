dcd0edd8303e26752946e967c4f067ada5a548c6
Eduardo Resende Pereira <ed.resende@gmail.com>
2012-03-27 19:28:38 -0300

    Breaking line on Publication circulation type enumeration for better readability.

--- app/enumerations/publication_circulation_type.rb
+++ app/enumerations/publication_circulation_type.rb
@@ -1,3 +1,4 @@
 class PublicationCirculationType < EnumerateIt::Base
-  associate_values :municipal_journal, :regional_newspaper, :state_newspaper, :national_newspaper, :union_official_daily, :state_official_daily, :city_official_daily, :public_mural, :internet
+  associate_values :municipal_journal, :regional_newspaper, :state_newspaper, :national_newspaper, :union_official_daily,
+                   :state_official_daily, :city_official_daily, :public_mural, :internet
 end
