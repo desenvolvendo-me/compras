3f74828eb8a968a4900e1652684007d16a13dac5
Dmitry Nix <dmitry.nix@gmail.com>
2012-03-29 14:14:36 -0300

    Use number instead order attribute to pledge_expiration

--- app/models/pledge_expiration.rb
+++ app/models/pledge_expiration.rb
@@ -1,8 +1,6 @@
 class PledgeExpiration < ActiveRecord::Base
   attr_accessible :expiration_date, :value, :number
 
-  attr_accessor :order
-
   belongs_to :pledge
 
   validates :expiration_date, :value, :presence => true
