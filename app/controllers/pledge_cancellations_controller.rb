7b69a3890c0f8a3900535c208d232d5ee9afc950
Dmitry Nix <dmitry.nix@gmail.com>
2012-03-30 13:21:41 -0300

    PledgeCancellation date should be as default as last date if exist
    [#26752189]

--- app/controllers/pledge_cancellations_controller.rb
+++ app/controllers/pledge_cancellations_controller.rb
@@ -1,3 +1,10 @@
 class PledgeCancellationsController < CrudController
   actions :all, :except => [:update, :destroy]
+
+  def new
+    object = build_resource
+    object.date = resource_class.last.date if resource_class.any?
+
+    super
+  end
 end
