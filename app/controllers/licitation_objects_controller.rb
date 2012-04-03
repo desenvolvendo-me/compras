67be038c51975cf7976db64fb6ee6276dfdf7720
José Gomes Júnior <zegomesjf@gmail.com>
2012-04-03 09:53:03 -0300

    Making the Licitation controller more dry.
    [#27250867]

--- app/controllers/licitation_objects_controller.rb
+++ app/controllers/licitation_objects_controller.rb
@@ -1,24 +1,22 @@
 class LicitationObjectsController < CrudController
-  
+
   def new
     object = build_resource
-    object.purchase_licitation_exemption  = 0.00
-    object.purchase_invitation_letter     = 0.00
-    object.purchase_taking_price          = 0.00
-    object.purchase_public_concurrency    = 0.00
-    object.build_licitation_exemption     = 0.00
-    object.build_invitation_letter        = 0.00
-    object.build_taking_price             = 0.00
-    object.build_public_concurrency       = 0.00
-    object.special_auction                = 0.00
-    object.special_unenforceability       = 0.00
-    object.special_contest                = 0.00
+    initializes_values(object)
 
     super
   end
-  
+
   def create
     object = build_resource
+    initializes_values(object)
+
+    super
+  end
+
+  private
+
+  def initializes_values(object)
     object.purchase_licitation_exemption  = 0.00
     object.purchase_invitation_letter     = 0.00
     object.purchase_taking_price          = 0.00
@@ -30,10 +28,5 @@ class LicitationObjectsController < CrudController
     object.special_auction                = 0.00
     object.special_unenforceability       = 0.00
     object.special_contest                = 0.00
-
-    super
   end
-
 end
-
-
