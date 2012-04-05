e25cff69600f724026b4cc6bd2f7adcb0cd29fb9
José Gomes Júnior <zegomesjf@gmail.com>
2012-04-05 09:24:08 -0300

    Use Date Class instead string on Regulatory Act Blueprint.

--- spec/blueprints/regulatory_acts.rb
+++ spec/blueprints/regulatory_acts.rb
@@ -3,10 +3,10 @@ RegulatoryAct.blueprint(:sopa) do
   act_number { "1234" }
   regulatory_act_type { RegulatoryActType.make!(:lei) }
   legal_text_nature { LegalTextNature.make!(:civica) }
-  creation_date { "01/01/2012" }
-  publication_date { "02/01/2012" }
-  vigor_date { "03/01/2012" }
-  end_date { "09/01/2012" }
+  creation_date { Date.new(2012,1,1) }
+  publication_date { Date.new(2012,1,2) }
+  vigor_date { Date.new(2012,1,3) }
+  end_date { Date.new(2012,1,9) }
   content { "conteudo" }
   budget_law_percent { "5.00" }
   revenue_antecipation_percent { "3.00" }
@@ -18,10 +18,10 @@ RegulatoryAct.blueprint(:emenda) do
   act_number { "4567" }
   regulatory_act_type { RegulatoryActType.make!(:emenda) }
   legal_text_nature { LegalTextNature.make!(:civica) }
-  creation_date { "01/01/2012" }
-  publication_date { "02/01/2012" }
-  vigor_date { "03/01/2012" }
-  end_date { "09/01/2012" }
+  creation_date { Date.new(2012,1,1) }
+  publication_date { Date.new(2012,1,2) }
+  vigor_date { Date.new(2012,1,3) }
+  end_date { Date.new(2012,1,9) }
   content { "conteúdo" }
   budget_law_percent { "1.00" }
   revenue_antecipation_percent { "5.00" }
@@ -32,10 +32,10 @@ RegulatoryAct.blueprint(:medida_provisoria) do
   act_number { "8901" }
   regulatory_act_type { RegulatoryActType.make!(:emenda) }
   legal_text_nature { LegalTextNature.make!(:civica) }
-  creation_date { "01/01/2013" }
-  publication_date { "02/01/2013" }
-  vigor_date { "03/01/2013" }
-  end_date { "09/01/2013" }
+  creation_date { Date.new(2013,1,1) }
+  publication_date { Date.new(2013,1,2) }
+  vigor_date { Date.new(2013,1,3) }
+  end_date { Date.new(2013,1,9) }
   content { "conteúdo" }
   budget_law_percent { "1.00" }
   revenue_antecipation_percent { "5.00" }
