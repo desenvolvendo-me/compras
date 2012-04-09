6970e7d2e682c754b34d013ef1d2ce00e2f61f56
Dmitry Nix <dmitry.nix@gmail.com>
2012-04-09 10:23:53 -0300

    Rename wrong revenue_nature at RevenueRubricImporter

--- lib/revenue_rubric_importer.rb
+++ lib/revenue_rubric_importer.rb
@@ -1,25 +1,25 @@
 class RevenueRubricImporter < Importer
-  attr_accessor :storage, :revenue_source_storage, :revenue_nature_storage
+  attr_accessor :storage, :revenue_source_storage, :revenue_subcategory_storage
   attr_accessor :revenue_category_storage
 
-  def initialize(storage = RevenueRubric, revenue_source_storage = RevenueSource, revenue_nature_storage = RevenueNature, revenue_category_storage = RevenueCategory)
+  def initialize(storage = RevenueRubric, revenue_source_storage = RevenueSource, revenue_subcategory_storage = RevenueSubcategory, revenue_category_storage = RevenueCategory)
     self.storage = storage
     self.revenue_source_storage = revenue_source_storage
-    self.revenue_nature_storage = revenue_nature_storage
+    self.revenue_subcategory_storage = revenue_subcategory_storage
     self.revenue_category_storage = revenue_category_storage
   end
 
   protected
 
   def normalize_attributes(attributes)
-    category_code = attributes['code'][0]
-    nature_code   = attributes['code'][1]
-    source_code   = attributes['code'][2]
-    code          = attributes['code'][3]
+    category_code    = attributes['code'][0]
+    subcategory_code = attributes['code'][1]
+    source_code      = attributes['code'][2]
+    code             = attributes['code'][3]
 
     category = revenue_category_storage.find_by_code(category_code)
-    nature = revenue_nature_storage.find_by_code_and_revenue_category_id(nature_code, category.try(:id))
-    source = revenue_source_storage.find_by_code_and_revenue_nature_id(source_code, nature.try(:id))
+    subcategory = revenue_subcategory_storage.find_by_code_and_revenue_category_id(subcategory_code, category.try(:id))
+    source = revenue_source_storage.find_by_code_and_revenue_subcategory_id(source_code, subcategory.try(:id))
 
     attributes.merge('revenue_source_id' => source.try(:id), 'code' => code)
   end
