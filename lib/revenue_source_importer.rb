7a378d530199510055ad7cb5cbf67ab55483c409
Dmitry Nix <dmitry.nix@gmail.com>
2012-04-09 09:46:44 -0300

    Rename wrong revenue_nature name at RevenueSourceImporter

--- lib/revenue_source_importer.rb
+++ lib/revenue_source_importer.rb
@@ -1,23 +1,23 @@
 class RevenueSourceImporter < Importer
-  attr_accessor :storage, :revenue_nature_storage, :revenue_category_storage
+  attr_accessor :storage, :revenue_subcategory_storage, :revenue_category_storage
 
-  def initialize(storage = RevenueSource, revenue_nature_storage = RevenueNature, revenue_category_storage = RevenueCategory)
+  def initialize(storage = RevenueSource, revenue_subcategory_storage = RevenueSubcategory, revenue_category_storage = RevenueCategory)
     self.storage = storage
-    self.revenue_nature_storage = revenue_nature_storage
+    self.revenue_subcategory_storage = revenue_subcategory_storage
     self.revenue_category_storage = revenue_category_storage
   end
 
   protected
 
   def normalize_attributes(attributes)
-    category_code = attributes['code'][0]
-    nature_code   = attributes['code'][1]
-    code          = attributes['code'][2]
+    category_code    = attributes['code'][0]
+    subcategory_code = attributes['code'][1]
+    code             = attributes['code'][2]
 
     category = revenue_category_storage.find_by_code(category_code)
-    nature = revenue_nature_storage.find_by_code_and_revenue_category_id(nature_code, category.try(:id))
+    subcategory = revenue_subcategory_storage.find_by_code_and_revenue_category_id(subcategory_code, category.try(:id))
 
-    attributes.merge('revenue_nature_id' => nature.try(:id), 'code' => code)
+    attributes.merge('revenue_subcategory_id' => subcategory.try(:id), 'code' => code)
   end
 
   def file
