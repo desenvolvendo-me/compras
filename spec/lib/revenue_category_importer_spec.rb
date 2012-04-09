bd8b4262856a83b157a746e51e635371d26e46e4
Dmitry Nix <dmitry.nix@gmail.com>
2012-04-09 13:26:05 -0300

    Add missing spec to RevenueCategoryImporter

---
+++ spec/lib/revenue_category_importer_spec.rb
@@ -0,0 +1,29 @@
+# encoding: utf-8
+require 'unit_helper'
+require 'lib/importer'
+require 'lib/revenue_category_importer'
+require 'active_support/core_ext/object/try'
+
+describe RevenueCategoryImporter do
+  subject do
+    described_class.new(null_storage)
+  end
+
+  let :null_storage do
+    storage = double.as_null_object
+
+    storage.stub(:transaction) do |&block|
+      block.call
+    end
+
+    storage
+  end
+
+  it 'imports revenue tegories' do
+    null_storage.should_receive(:create!).with('code' => '1', 'description' => 'RECEITAS CORRENTES')
+    null_storage.should_receive(:create!).with('code' => '7', 'description' => 'RECEITAS CORRENTES - INTRA-ORÇAMENTÁRIAS')
+    null_storage.should_receive(:create!).with('code' => '8', 'description' => 'RECEITAS DE CAPITAL - INTRA-ORÇAMENTÁRIAS')
+
+    subject.import!
+  end
+end
