a783e5a7aa3773bf30d48852e8f41e77d490d36c
Dmitry Nix <dmitry.nix@gmail.com>
2012-04-10 20:11:49 -0300

    Refactory RevenueRubricImporter.
    Create a method only to query RevenueSource.

--- lib/revenue_rubric_importer.rb
+++ lib/revenue_rubric_importer.rb
@@ -12,15 +12,20 @@ class RevenueRubricImporter < Importer
   protected
 
   def normalize_attributes(attributes)
+    rubric_code = attributes['code'][3]
+
+    attributes.merge('revenue_source_id' => source_id_by(attributes), 'code' => rubric_code)
+  end
+
+  def source_id_by(attributes)
     category_code    = attributes['code'][0]
     subcategory_code = attributes['code'][1]
     source_code      = attributes['code'][2]
-    rubric_code      = attributes['code'][3]
 
     source = revenue_source_storage.joins { revenue_subcategory.revenue_category }.
       where { revenue_subcategory.revenue_category.code.eq(category_code) & revenue_subcategory.code.eq(subcategory_code) & code.eq(source_code) }.first
 
-    attributes.merge('revenue_source_id' => source.try(:id), 'code' => rubric_code)
+    source.try(:id)
   end
 
   def file
