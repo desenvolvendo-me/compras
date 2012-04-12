dee6024b24b6f7db70ba7482e599049408797bc9
Mateus Lorandi dos Santos <mcomogo@gmail.com>
2012-04-12 18:54:13 -0300

    AdministrativeProcess: Change up and down from migration into change

--- db/migrate/20120410135431_remove_budget_allocation_id_and_value_estimated_from_administrative_process.rb
+++ db/migrate/20120410135431_remove_budget_allocation_id_and_value_estimated_from_administrative_process.rb
@@ -1,11 +1,6 @@
 class RemoveBudgetAllocationIdAndValueEstimatedFromAdministrativeProcess < ActiveRecord::Migration
-  def up
+  def change
     remove_column :administrative_processes, :budget_allocation_id
     remove_column :administrative_processes, :value_estimated
-  end
-
-  def down
-    add_column :administrative_processes, :value_estimated, :string
-    add_column :administrative_processes, :budget_allocation_id, :string
   end
 end
