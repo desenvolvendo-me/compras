685e0ee3e417e013615fb36af02ea115b40f1f77
Mateus Lorandi dos Santos <mcomogo@gmail.com>
2012-04-13 11:47:11 -0300

    LicitationProcessImpugnment: Change blueprint

--- spec/blueprints/licitation_process_impugnments.rb
+++ spec/blueprints/licitation_process_impugnments.rb
@@ -1,7 +1,6 @@
 # encoding: utf-8
 LicitationProcessImpugnment.blueprint(:proibido_cadeiras) do
-  licitation_process_blueprint = LicitationProcess.make!(:processo_licitatorio)
-  licitation_process { licitation_process_blueprint }
+  licitation_process { LicitationProcess.make!(:processo_licitatorio) }
   impugnment_date { Date.new(2012, 4, 1) }
   related { Related::TRADING }
   person { Person.make!(:sobrinho) }
@@ -9,8 +8,8 @@ LicitationProcessImpugnment.blueprint(:proibido_cadeiras) do
   situation { Situation::PENDING }
   judgment_date { Date.new(2012, 4, 30) }
   observation { "Nenhuma Observação" }
-  envelope_delivery_date  { licitation_process_blueprint.envelope_delivery_date }
-  envelope_delivery_time  { licitation_process_blueprint.envelope_delivery_time }
-  envelope_opening_date   { licitation_process_blueprint.envelope_opening_date }
-  envelope_opening_time   { licitation_process_blueprint.envelope_opening_time }
+  envelope_delivery_date  { object.licitation_process.envelope_delivery_date }
+  envelope_delivery_time  { object.licitation_process.envelope_delivery_time }
+  envelope_opening_date   { object.licitation_process.envelope_opening_date }
+  envelope_opening_time   { object.licitation_process.envelope_opening_time }
 end
