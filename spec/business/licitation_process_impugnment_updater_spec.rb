5e93929747f52fff82ddbe04248cad5713486074
José Gomes Júnior <zegomesjf@gmail.com>
2012-04-15 00:23:50 -0300

    Removing unnecessary space in LicitationProcessImpugnment business spec.
    [Fix #27249063]

--- spec/business/licitation_process_impugnment_updater_spec.rb
+++ spec/business/licitation_process_impugnment_updater_spec.rb
@@ -2,7 +2,6 @@ require 'unit_helper'
 require 'app/business/licitation_process_impugnment_updater'
 
 describe LicitationProcessImpugnmentUpdater do
-
   it "should respond to update_licitation_process_date! method" do
     LicitationProcessImpugnmentUpdater.respond_to? :update_licitation_process_date!
   end
