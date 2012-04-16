81487e085ae2f92b7cbcfd283fa4ff4e409708c8
Dmitry Nix <dmitry.nix@gmail.com>
2012-04-16 08:36:10 -0300

    Correct spec description at RevenueCategoryImporter spec

--- spec/lib/revenue_category_importer_spec.rb
+++ spec/lib/revenue_category_importer_spec.rb
@@ -19,7 +19,7 @@ describe RevenueCategoryImporter do
     storage
   end
 
-  it 'imports revenue tegories' do
+  it 'imports revenue categories' do
     null_storage.should_receive(:create!).with('code' => '1', 'description' => 'RECEITAS CORRENTES')
     null_storage.should_receive(:create!).with('code' => '7', 'description' => 'RECEITAS CORRENTES - INTRA-ORÇAMENTÁRIAS')
     null_storage.should_receive(:create!).with('code' => '8', 'description' => 'RECEITAS DE CAPITAL - INTRA-ORÇAMENTÁRIAS')
