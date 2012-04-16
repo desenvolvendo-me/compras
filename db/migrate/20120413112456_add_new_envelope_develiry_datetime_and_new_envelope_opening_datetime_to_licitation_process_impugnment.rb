86ddfd63d27558205926b1fb7350a3a9595a127b
Mateus Lorandi dos Santos <mcomogo@gmail.com>
2012-04-16 16:05:00 -0300

    LicitationProcessImpugnment: Remove extra lines from migration.

--- db/migrate/20120413112456_add_new_envelope_develiry_datetime_and_new_envelope_opening_datetime_to_licitation_process_impugnment.rb
+++ db/migrate/20120413112456_add_new_envelope_develiry_datetime_and_new_envelope_opening_datetime_to_licitation_process_impugnment.rb
@@ -1,12 +1,8 @@
 class AddNewEnvelopeDeveliryDatetimeAndNewEnvelopeOpeningDatetimeToLicitationProcessImpugnment < ActiveRecord::Migration
   def change
     add_column :licitation_process_impugnments, :new_envelope_delivery_date, :date
-
     add_column :licitation_process_impugnments, :new_envelope_delivery_time, :time
-
     add_column :licitation_process_impugnments, :new_envelope_opening_date, :date
-
     add_column :licitation_process_impugnments, :new_envelope_opening_time, :time
-
   end
 end
