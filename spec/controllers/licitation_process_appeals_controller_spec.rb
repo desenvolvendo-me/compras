fa077c1ec7fb2ad261078b6ec52d4cb46b68b0fe
José Gomes Júnior <zegomesjf@gmail.com>
2012-04-15 23:57:50 -0300

    Log in using stub on LicitationProcessAppeals controller spec.

--- spec/controllers/licitation_process_appeals_controller_spec.rb
+++ spec/controllers/licitation_process_appeals_controller_spec.rb
@@ -2,7 +2,8 @@ require 'spec_helper'
 
 describe LicitationProcessAppealsController do
   before do
-    sign_in User.make!(:sobrinho_as_admin_and_employee)
+    controller.stub(:authenticate_user!)
+    controller.stub(:authorize_resource!)
   end
 
   context 'GET #new' do
