a511ebbb4945f238d0855887b3ecdbf0b1a172e0
Mateus Lorandi dos Santos <mcomogo@gmail.com>
2012-04-16 14:01:20 -0300

    Pledges: improve form, use of presenter

--- app/presenters/pledge_presenter.rb
+++ app/presenters/pledge_presenter.rb
@@ -1,3 +1,11 @@
 class PledgePresenter < Presenter::Proxy
   attr_modal :entity_id, :year, :emission_date
+
+  def budget_allocation_real_ammount
+    helpers.number_with_precision object.budget_allocation_real_ammount
+  end
+
+  def reserve_found_value
+    helpers.number_with_precision object.reserve_found_value
+  end
 end
