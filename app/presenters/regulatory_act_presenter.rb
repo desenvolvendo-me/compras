48810e07c10d9902cb13fb3b3174d838e905279d
Eduardo Resende Pereira <ed.resende@gmail.com>
2012-04-17 15:13:35 -0300

    Moving data attributes to presenters and removing unnecessary partial "modal_record" for:
    * Regulatory Act
    * Revenue Rubric
    
    [#28032033]

--- app/presenters/regulatory_act_presenter.rb
+++ app/presenters/regulatory_act_presenter.rb
@@ -1,3 +1,6 @@
 class RegulatoryActPresenter < Presenter::Proxy
   attr_modal :act_number, :regulatory_act_type_id, :legal_text_nature_id
+
+  attr_data 'administractive-act-type' => :regulatory_act_type
+  attr_data 'publication-date' => :publication_date, 'vigor-date' => :vigor_date
 end
---
+++ app/presenters/revenue_rubric_presenter.rb
@@ -0,0 +1,3 @@
+class RevenueRubricPresenter < Presenter::Proxy
+  attr_data 'id' => :id, 'full-code' => :full_code
+end
