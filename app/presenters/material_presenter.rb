f4754690882a2712feaeead4c281c9c1da435174
Eduardo Resende Pereira <ed.resende@gmail.com>
2012-04-17 15:12:07 -0300

    Moving data attributes to presenters and removing unnecessary partial "modal_record" for:
    * Material
    * Material Class
    * Material Group
    
    [#28032033]

--- app/presenters/material_presenter.rb
+++ app/presenters/material_presenter.rb
@@ -1,3 +1,6 @@
 class MaterialPresenter < Presenter::Proxy
   attr_modal :description
+
+  attr_data 'name' => :description, 'id' => :id
+  attr_data 'reference-unit' => :reference_unit, 'code' => :code
 end
--- app/presenters/materials_class_presenter.rb
+++ app/presenters/materials_class_presenter.rb
@@ -1,3 +1,5 @@
 class MaterialsClassPresenter < Presenter::Proxy
   attr_modal :materials_group_id, :class_number, :description
+
+  attr_data 'id' => :id, 'description' => :description, 'number' => :class_number
 end
