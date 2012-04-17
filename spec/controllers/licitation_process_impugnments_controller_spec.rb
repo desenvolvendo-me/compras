80044f66be30cf324bf44625542fcefffca6bd41
Mateus Lorandi dos Santos <mcomogo@gmail.com>
2012-04-16 23:56:06 -0300

    LicitationProcessImpugnment: Add missing controller test

--- spec/controllers/licitation_process_impugnments_controller_spec.rb
+++ spec/controllers/licitation_process_impugnments_controller_spec.rb
@@ -2,14 +2,29 @@ require 'spec_helper'
 
 describe LicitationProcessImpugnmentsController do
   before do
-    sign_in User.make!(:sobrinho_as_admin_and_employee)
+    controller.stub(:authenticate_user!)
+    controller.stub(:authorize_resource!)
   end
 
-  context 'GET #new' do
+  describe 'GET #new' do
     it 'uses pending as default value for situation' do
       get :new
 
       assigns(:licitation_process_impugnment).situation.should eq Situation::PENDING
+    end
+  end
+
+  describe 'POST #create' do
+    it 'should has default values for situation, and envelope dates' do
+      licitation_process = LicitationProcess.make!(:processo_licitatorio)
+
+      post :create, :licitation_process_impugnment => { :licitation_process_id => licitation_process.id }
+
+      assigns(:licitation_process_impugnment).situation.should eq Situation::PENDING
+      assigns(:licitation_process_impugnment).envelope_delivery_date.should eq licitation_process.envelope_delivery_date
+      assigns(:licitation_process_impugnment).envelope_delivery_time.should eq licitation_process.envelope_delivery_time
+      assigns(:licitation_process_impugnment).envelope_opening_date.should eq licitation_process.envelope_opening_date
+      assigns(:licitation_process_impugnment).envelope_opening_time.should eq licitation_process.envelope_opening_time
     end
   end
 end
