a44574ebc68767deecfe1e324a309af995cab91a
Mateus Lorandi dos Santos <mcomogo@gmail.com>
2012-04-20 09:37:00 -0300

    Refactor pledge presenter spec
    Move helper stub methods from it block to helper double

--- spec/presenters/pledge_presenter_spec.rb
+++ spec/presenters/pledge_presenter_spec.rb
@@ -11,17 +11,18 @@ describe PledgePresenter do
     double(:budget_allocation_real_ammount => 500.0, :reserve_found_value => 300.0)
   end
 
-  let(:helpers) { double }
+  let(:helpers) do
+    double.tap do |helpers|
+      helpers.stub(:number_with_precision).with(500.0).and_return("500,00")
+      helpers.stub(:number_with_precision).with(300.0).and_return("300,00")
+    end
+  end
 
   it 'should return budget_allocation_real_ammount with precision' do
-    helpers.should_receive(:number_with_precision).with(pledge.budget_allocation_real_ammount).and_return("500,00")
-
     subject.budget_allocation_real_ammount.should eq '500,00'
   end
 
   it 'should return reserve_found_value with precision' do
-    helpers.should_receive(:number_with_precision).with(pledge.reserve_found_value).and_return("300,00")
-
     subject.reserve_found_value.should eq '300,00'
   end
 end
