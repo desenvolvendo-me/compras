21aa43cc65a87af3b671e78305673dc7575ff97c
Mateus Lorandi dos Santos <mcomogo@gmail.com>
2012-04-20 09:06:48 -0300

    Accreditation link button logic move to presenter

--- app/presenters/licitation_process_presenter.rb
+++ app/presenters/licitation_process_presenter.rb
@@ -8,4 +8,12 @@ class LicitationProcessPresenter < Presenter::Proxy
   def envelope_opening_time
     helpers.l(object.envelope_opening_time, :format => :hour) if object.envelope_opening_time
   end
+
+  def build_accreditation_link
+    if object.accreditation
+      return helpers.link_to('Editar credenciamento',  routes.edit_licitation_process_accreditation_path(object), :class => "button secondary")
+    else
+      return helpers.link_to('Novo credenciamento',  routes.new_licitation_process_accreditation_path(object), :class => "button primary")
+    end
+  end
 end
