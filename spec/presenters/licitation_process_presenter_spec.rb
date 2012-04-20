4f1ddcb4d8f1a00e803bc09fd5e5f29013b0c75e
Mateus Lorandi dos Santos <mcomogo@gmail.com>
2012-04-20 09:02:27 -0300

    Add missing licitation process presenter spec

---
+++ spec/presenters/licitation_process_presenter_spec.rb
@@ -0,0 +1,29 @@
+# encoding: utf-8
+require 'presenter_helper'
+require 'app/presenters/licitation_process_presenter'
+
+describe LicitationProcessPresenter do
+  subject do
+    described_class.new(licitation_process, nil, helpers)
+  end
+
+  let :licitation_process do
+    double(:envelope_delivery_time => Time.new(2012, 1, 4, 10),
+           :envelope_opening_time => Time.new(2012, 2, 4, 11))
+  end
+
+  let :helpers do
+    double.tap do |helpers|
+      helpers.stub(:l).with(Time.new(2012, 1, 4, 10), :format => :hour).and_return('10:00')
+      helpers.stub(:l).with(Time.new(2012, 2, 4, 11), :format => :hour).and_return('11:00')
+    end
+  end
+
+  it 'should return localized envelope_delivery_time' do
+    subject.envelope_delivery_time.should eq '10:00'
+  end
+
+  it 'should return localized opening_delivery_time' do
+    subject.envelope_opening_time.should eq '11:00'
+  end
+end
