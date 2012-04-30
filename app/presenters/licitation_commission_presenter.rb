89da4ae38fa0e64b7837b6b4099ba88e3ba65fb1
Eduardo Resende Pereira <ed.resende@gmail.com>
2012-04-30 11:34:16 -0300

    Licitation Commission: using presenter to show formatted regulatory act publication date

--- app/presenters/licitation_commission_presenter.rb
+++ app/presenters/licitation_commission_presenter.rb
@@ -3,4 +3,8 @@ class LicitationCommissionPresenter < Presenter::Proxy
 
   attr_data 'president-name' => :president_name
   attr_data 'members' => :members_attr_data
+
+  def regulatory_act_publication_date
+   helpers.l object.regulatory_act_publication_date if object.regulatory_act_publication_date
+  end
 end
