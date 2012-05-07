9a7dd039ef29f045828c1a8c854d58ff96222d93
José Gomes Júnior <zegomesjf@gmail.com>
2012-05-07 14:46:39 -0300

    Add missing tests (to_s, next_number) on Licitation Notice spec.

--- spec/models/licitation_notice_spec.rb
+++ spec/models/licitation_notice_spec.rb
@@ -7,4 +7,22 @@ describe LicitationNotice do
   it { should validate_presence_of :licitation_process }
   it { should validate_presence_of :date }
   it { should validate_presence_of :number }
+
+  it 'should return id as to_s method' do
+    subject.id = '1'
+
+    subject.to_s.should eq '1'
+  end
+
+  context 'next_number' do
+    it 'should return 1 as first licitation notice' do
+      subject.stub(:last_number).and_return(0)
+      subject.next_number.should eq 1
+    end
+
+    it 'should return 2 as secondary licitation notice' do
+      subject.stub(:last_number).and_return(1)
+      subject.next_number.should eq 2
+    end
+  end
 end
