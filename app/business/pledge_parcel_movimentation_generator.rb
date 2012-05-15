e057b8def871d4dab721f11d1225e39032ecaf43
Dmitry Nix <dmitry.nix@gmail.com>
2012-05-15 11:38:52 -0300

    Refactory PledgeParcelMovimentationGenerator PORO to use fail-fast

--- app/business/pledge_parcel_movimentation_generator.rb
+++ app/business/pledge_parcel_movimentation_generator.rb
@@ -15,26 +15,24 @@ class PledgeParcelMovimentationGenerator
     value_left = value
 
     pledge_parcels_with_balance.each do |parcel|
-      if value_left.zero?
-        return
+      return if value_left.zero?
+
+      if value_left >= parcel.balance
+        current_value = parcel.balance
       else
-        if value_left >= parcel.balance
-          current_value = parcel.balance
-        else
-          current_value = value_left
-        end
-
-        pledge_parcel_movimentation_storage.create!(
-          :pledge_parcel_id => parcel.id,
-          :pledge_parcel_modificator_id => pledge_cancellation_object.id,
-          :pledge_parcel_modificator_type => pledge_cancellation_object.class.name,
-          :pledge_parcel_value_was => parcel.balance,
-          :pledge_parcel_value => parcel.balance - current_value,
-          :value => current_value
-        )
-
-        value_left -= current_value
+        current_value = value_left
       end
+
+      pledge_parcel_movimentation_storage.create!(
+        :pledge_parcel_id => parcel.id,
+        :pledge_parcel_modificator_id => pledge_cancellation_object.id,
+        :pledge_parcel_modificator_type => pledge_cancellation_object.class.name,
+        :pledge_parcel_value_was => parcel.balance,
+        :pledge_parcel_value => parcel.balance - current_value,
+        :value => current_value
+      )
+
+      value_left -= current_value
     end
   end
 end
