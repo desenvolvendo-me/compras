929aa801a195972b5273ad00585d5dadd4d042d5
Eduardo Resende Pereira <ed.resende@gmail.com>
2012-05-29 08:57:06 -0300

    Price collection proposal decorator: using super instead of repeat method name

--- app/decorators/price_collection_proposal_decorator.rb
+++ app/decorators/price_collection_proposal_decorator.rb
@@ -1,9 +1,9 @@
 class PriceCollectionProposalDecorator < Decorator
   def price_collection_date
-    helpers.l(component.price_collection_date) if component.price_collection_date
+    helpers.l(super) if super
   end
 
   def item_total_value_by_lot(lot)
-    helpers.number_with_precision component.item_total_value_by_lot(lot)
+    helpers.number_with_precision super(lot)
   end
 end
