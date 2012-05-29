ede5e7489aebdb026623d43fceb2dd45047df09a
Dmitry Nix <dmitry.nix@gmail.com>
2012-05-29 16:24:04 -0300

    Disable status at budget_allocation_type new form and set initial value as active
    [Finishes #30060781]

--- app/controllers/budget_allocation_types_controller.rb
+++ app/controllers/budget_allocation_types_controller.rb
@@ -1,6 +1,14 @@
 class BudgetAllocationTypesController < CrudController
+  def new
+    object = build_resource
+    object.status = Status::ACTIVE
+
+    super
+  end
+
   def create
     object = build_resource
+    object.status = Status::ACTIVE
     object.source = Source::MANUAL
 
     super
--- spec/controllers/budget_allocation_types_controller_spec.rb
+++ spec/controllers/budget_allocation_types_controller_spec.rb
@@ -6,9 +6,26 @@ describe BudgetAllocationTypesController do
     controller.stub(:authorize_resource!)
   end
 
-  it 'uses manual as default value for source' do
-    post :create
+  context '#new' do
+    it 'should use active as default value for status' do
+      get :new
 
-    assigns(:budget_allocation_type).source.should eq Source::MANUAL
+      assigns(:budget_allocation_type).status.should eq Status::ACTIVE
+    end
+  end
+
+  context '#create' do
+    it 'should use active as default value for status' do
+      post :create
+
+      assigns(:budget_allocation_type).status.should eq Status::ACTIVE
+    end
+
+
+    it 'uses manual as default value for source' do
+      post :create
+
+      assigns(:budget_allocation_type).source.should eq Source::MANUAL
+    end
   end
 end
