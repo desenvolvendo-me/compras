feee1e957f9caff4d4fb33b9b106d75655314cce
José Gomes Júnior <zegomesjf@gmail.com>
2012-05-31 10:54:56 -0300

    requiredField function accepts one more params.
    Sample:
    $('#id_field, #other_id').requiredField(true);
    $('#id_field, #other_id').requiredField(false);

--- app/assets/javascripts/simple_form.toogle_required_input.js
+++ app/assets/javascripts/simple_form.toogle_required_input.js
@@ -1,11 +1,15 @@
 (function ($) {
   $.fn.requiredField = function ( required ) {
     if (required){
-      var field = 'label[for|="' + $(this).attr('id') + '"]';
-      $(field).append('<abbr title="obrigatório">*</abbr>');
+      this.each( function() {
+        var field = 'label[for|="' + $(this).attr('id') + '"]';
+        $(field).append('<abbr title="obrigatório">*</abbr>');
+      });
     } else {
-      var field = 'label[for|="' + $(this).attr('id') + '"] abbr';
-      $(field).remove();
+      this.each( function() {
+        var field = 'label[for|="' + $(this).attr('id') + '"] abbr';
+        $(field).remove();
+      });
     }
   }
 })(jQuery);
