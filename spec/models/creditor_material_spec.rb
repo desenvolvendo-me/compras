d09d3d3823d6f1421bb747d30ca74b3c8e574b3f
Mateus Lorandi dos Santos <mcomogo@gmail.com>
2012-06-04 13:51:03 -0300

    Add blank line at CreditorMaterial model spec

--- spec/models/creditor_material_spec.rb
+++ spec/models/creditor_material_spec.rb
@@ -7,6 +7,7 @@ require 'app/models/material'
 describe CreditorMaterial do
   it { should validate_presence_of :creditor }
   it { should validate_presence_of :material }
+
   it { should belong_to :creditor }
   it { should belong_to :material }
 end
