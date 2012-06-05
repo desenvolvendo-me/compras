c35c2870ee567c514661648b24a2daf540663fc9
Gabriel Sobrinho <gabriel.sobrinho@gmail.com>
2012-06-05 10:27:42 -0300

    Use company size from unico
    Signed-off-by: Cairo Noleto <caironoleto@gmail.com>
    Signed-off-by: Eduardo Resende Pereira <ed.resende@gmail.com>

--- app/models/company_size.rb
+++ app/models/company_size.rb
@@ -1,16 +1,6 @@
-class CompanySize < ActiveRecord::Base
-  attr_accessible :name, :acronym, :number
+class CompanySize < Unico::CompanySize
+   has_many :companies, :dependent => :restrict
 
-  has_many :companies, :dependent => :restrict
-  has_many :creditors, :dependent => :restrict
-
-  validates :name, :acronym, :presence => true
-  validates :name, :acronym, :uniqueness => { :allow_blank => true }
-
-  filterize
-  orderize
-
-  def to_s
-    name
-  end
-end
+   filterize
+   orderize
+ end
--- spec/models/company_size_spec.rb
+++ spec/models/company_size_spec.rb
@@ -1,17 +1,9 @@
 require 'model_helper'
+require 'app/models/unico/company_size'
 require 'app/models/company_size'
 require 'app/models/company'
 require 'app/models/creditor'
 
 describe CompanySize do
-  it "return name when call to_s" do
-    subject.name = "Empresa de grande porte"
-    subject.to_s.should eq subject.name
-  end
-
   it { should have_many(:companies).dependent(:restrict) }
-  it { should have_many(:creditors).dependent(:restrict) }
-
-  it { should validate_presence_of :name }
-  it { should validate_presence_of :acronym }
 end
