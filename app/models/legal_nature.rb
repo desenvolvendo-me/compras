1910ab2b18bb8c881a9c99608dc75f04b29f27f2
Gabriel Sobrinho <gabriel.sobrinho@gmail.com>
2012-06-05 10:43:19 -0300

    Use legal nature from unico
    Signed-off-by: Cairo Noleto <caironoleto@gmail.com>
    Signed-off-by: Eduardo Resende Pereira <ed.resende@gmail.com>

--- app/models/legal_nature.rb
+++ app/models/legal_nature.rb
@@ -1,17 +1,9 @@
-class LegalNature < ActiveRecord::Base
-  attr_readonly :code, :name, :parent_id
-
-  acts_as_nested_set
+class LegalNature < Unico::LegalNature
+  has_enumeration_for :level_of_nature, :with => LevelOfLegalNature
 
   has_many :administration_types, :dependent => :restrict
   has_many :providers, :dependent => :restrict
 
-  validates :name, :code, :presence => true
-
   filterize
   orderize :created_at
-
-  def to_s
-    name
-  end
 end
--- spec/models/legal_nature_spec.rb
+++ spec/models/legal_nature_spec.rb
@@ -1,19 +1,11 @@
 # encoding: utf-8
 require 'model_helper'
+require 'app/models/unico/legal_nature'
 require 'app/models/legal_nature'
 require 'app/models/administration_type'
 require 'app/models/provider'
 
 describe LegalNature do
-  it "return the name when call to_s" do
-    subject.name = 'Administração Pública'
-    subject.to_s.should eq subject.name
-  end
-
   it { should have_many(:administration_types).dependent(:restrict) }
   it { should have_many(:providers).dependent(:restrict) }
-
-  it { should validate_presence_of :name }
-  it { should validate_presence_of :code }
-
 end
