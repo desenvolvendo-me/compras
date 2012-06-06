df6808d3e6a92d3c3fb4028408d1d95f4ab961aa
Rafael Felix <felix.rafael@gmail.com>
2012-06-06 20:23:01 -0300

    Added parenthesis on the method definition

--- app/helpers/price_collection_proposals_helper.rb
+++ app/helpers/price_collection_proposals_helper.rb
@@ -1,5 +1,5 @@
 module PriceCollectionProposalsHelper
-  def proposals_list_header parent
+  def proposals_list_header(parent)
     title = PriceCollectionProposal.model_name.human(:count => 'many')
     title = "Propostas para a Coleta #{parent}" if parent
     content_tag :h2, title
