e239d96864baa15c55d21b8a3a8b72dac2eaf570
José Gomes Júnior <zegomesjf@gmail.com>
2012-06-06 10:52:20 -0300

    remove unnecessary use of inverse_of on regularization_or_administrative_sanction.

--- app/models/regularization_or_administrative_sanction.rb
+++ app/models/regularization_or_administrative_sanction.rb
@@ -2,7 +2,7 @@ class RegularizationOrAdministrativeSanction < ActiveRecord::Base
   attr_accessible :creditor_id, :suspended_until, :occurrence
   attr_accessible :regularization_or_administrative_sanction_reason_id
 
-  belongs_to :creditor, :inverse_of => :regularization_or_administrative_sanctions
+  belongs_to :creditor
   belongs_to :regularization_or_administrative_sanction_reason
 
   delegate :administrative_sanction?, :regularization?, :description, :reason_type_humanize,
