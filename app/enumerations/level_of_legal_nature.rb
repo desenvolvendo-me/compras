ff816e89b3db681811c1a01f53fc83ce9641fbac
Cairo Noleto <caironoleto@gmail.com>
2012-06-06 11:03:56 -0300

    Using legal nature with legal of nature from unico
    Signed-off-by: Eduardo Resende Pereira <ed.resende@gmail.com>

---
+++ app/enumerations/level_of_legal_nature.rb
@@ -0,0 +1,3 @@
+class LevelOfLegalNature < EnumerateIt::Base
+  associate_values :analytical, :synthetic
+end
--- db/migrate/20120124181637_add_level_to_legal_natures.rb
+++
@@ -1,5 +0,0 @@
-class AddLevelToLegalNatures < ActiveRecord::Migration
-  def change
-    add_column :legal_natures, :level_of_nature, :string
-  end
-end
