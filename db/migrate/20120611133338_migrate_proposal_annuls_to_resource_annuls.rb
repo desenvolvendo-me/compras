11de5d86eea92ced95deb0125eb6bed572dbacd7
Rafael Felix <felix.rafael@gmail.com>
2012-06-12 10:41:24 -0300

    Using update_column instead of update_attributes
    update_column not fire validations

--- db/migrate/20120611133338_migrate_proposal_annuls_to_resource_annuls.rb
+++ db/migrate/20120611133338_migrate_proposal_annuls_to_resource_annuls.rb
@@ -1,7 +1,8 @@
 class MigrateProposalAnnulsToResourceAnnuls < ActiveRecord::Migration
   def change
     ResourceAnnul.find_each do |annul|
-      annul.update_attributes :resource_id => annul.price_collection_proposal_id, :resource_type => 'PriceCollectionProposal'
+      annul.update_column :resource_id, annul.price_collection_proposal_id
+      annul.update_column :resource_type, 'PriceCollectionProposal'
     end
   end
 end
