bb1a7364aed7ba5ceedaa564b596cc7b565483fb
Rafael Felix <felix.rafael@gmail.com>
2012-06-12 17:27:22 -0300

    when cancel a priceCollection must cancel the related proposals

--- app/business/price_collection_annulment.rb
+++ app/business/price_collection_annulment.rb
@@ -1,9 +1,28 @@
 class PriceCollectionAnnulment
-  def initialize(proposal)
-    @proposal = proposal
+  def initialize(price_collection)
+    @price_collection = price_collection
   end
 
   def change!
-    @proposal.annul! if @proposal.annul.present?
+    return unless annul.present?
+
+    if @price_collection.annul!
+      @price_collection.price_collection_proposals.each do |proposal|
+        annul_proposal(proposal)
+      end
+    end
+  end
+
+  protected
+  def annul
+    @annul ||= @price_collection.annul
+  end
+
+  def annul_proposal proposal
+    proposal_annul = proposal.build_annul(:employee_id => annul.employee_id, :date => annul.date, :description => annul.description)
+
+    if proposal_annul.save
+      proposal.annul!
+    end
   end
 end
--- spec/business/price_collection_annulment_spec.rb
+++ spec/business/price_collection_annulment_spec.rb
@@ -2,22 +2,34 @@ require 'unit_helper'
 require 'app/business/price_collection_annulment'
 
 describe PriceCollectionAnnulment do
+  let :price_collection do
+    double('PriceCollection', :price_collection_proposals => [proposal])
+  end
+
+  let :annul do
+    double('Annul', :date => 'current date', :description => 'Foo Bar', :employee_id => 1)
+  end
+
   let :proposal do
     double('Proposal')
   end
 
-  subject{ described_class.new(proposal) }
+  subject{ described_class.new(price_collection) }
 
   it 'should not annul the proposal when annul is not present' do
-    proposal.should_not_receive(:annul!)
-    proposal.stub_chain(:annul, :present?).and_return false
+    price_collection.should_not_receive(:annul!)
+    price_collection.stub_chain(:annul, :present?).and_return false
 
     subject.change!
   end
 
   it 'should annul the proposal when annul is present' do
+    price_collection.should_receive(:annul!).and_return true
+    price_collection.stub(:annul).and_return annul
+    price_collection.stub_chain(:annul, :present?).and_return true
+
     proposal.should_receive(:annul!)
-    proposal.stub_chain(:annul, :present?).and_return true
+    proposal.should_receive(:build_annul).with(:employee_id => 1, :date => 'current date', :description => 'Foo Bar').and_return double(:save => true)
 
     subject.change!
   end
