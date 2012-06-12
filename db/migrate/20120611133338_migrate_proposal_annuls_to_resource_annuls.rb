fdc10be0c8804f884377de2964ada67e26c1830c
Rafael Felix <felix.rafael@gmail.com>
2012-06-12 19:05:37 -0300

    Using update_all instead of 2 update_column

--- db/migrate/20120611133338_migrate_proposal_annuls_to_resource_annuls.rb
+++ db/migrate/20120611133338_migrate_proposal_annuls_to_resource_annuls.rb
@@ -1,8 +1,7 @@
 class MigrateProposalAnnulsToResourceAnnuls < ActiveRecord::Migration
   def change
     ResourceAnnul.find_each do |annul|
-      annul.update_column :resource_id, annul.price_collection_proposal_id
-      annul.update_column :resource_type, 'PriceCollectionProposal'
+      ResourceAnnul.update_all( {:resource_id => annul.price_collection_proposal_id, :resource_type => 'PriceCollectionProposal'}, {:id => annul.id} )
     end
   end
 end
