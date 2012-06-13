7fc0304fa098e791bbc312e0a3d67ffef59baded
Rafael Felix <felix.rafael@gmail.com>
2012-06-13 08:56:51 -0300

    do not use controller var on view
    using resource.price_collection instead of controller var.

--- app/controllers/price_collection_annuls_controller.rb
+++ app/controllers/price_collection_annuls_controller.rb
@@ -2,12 +2,12 @@ class PriceCollectionAnnulsController < CrudController
   actions :all, :except => [:destroy, :index, :show]
 
   def new
-    @price_collection = PriceCollection.find params[:price_collection_id]
+    price_collection = PriceCollection.find params[:price_collection_id]
 
     object = build_resource
     object.date = Date.current
     object.employee = current_user.authenticable
-    object.price_collection = @price_collection
+    object.price_collection = price_collection
 
     super
   end
