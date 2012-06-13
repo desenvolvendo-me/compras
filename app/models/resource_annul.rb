bcc34a6b89e5f1e37fb4add103f8463d48c16d91
Rafael Felix <felix.rafael@gmail.com>
2012-06-13 17:51:19 -0300

    annulable_id and annullable_type should be accessible

--- app/models/resource_annul.rb
+++ app/models/resource_annul.rb
@@ -1,5 +1,7 @@
 class ResourceAnnul < Compras::Model
   include Annullable
+  attr_accessible :annullable_id, :annullable_type
+
   belongs_to :annullable, :polymorphic => true
 
   validates :annullable, :presence => true
