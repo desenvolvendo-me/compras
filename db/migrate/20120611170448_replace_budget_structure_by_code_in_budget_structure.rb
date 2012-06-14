dcdd282f49836e7cade46ce082efc17473002856
Eduardo Resende Pereira <ed.resende@gmail.com>
2012-06-14 11:26:57 -0300

    removing prefix _compras from migration ReplaceBudgetStructureByCodeInBudgetStructure

--- db/migrate/20120611170448_replace_budget_structure_by_code_in_budget_structure.rb
+++ db/migrate/20120611170448_replace_budget_structure_by_code_in_budget_structure.rb
@@ -1,20 +1,20 @@
 class ReplaceBudgetStructureByCodeInBudgetStructure < ActiveRecord::Migration
   def up
-    remove_column :compras_budget_structures, :budget_structure
-    add_column :compras_budget_structures, :code, :integer
-    add_column :compras_budget_structures, :budget_structure_level_id, :integer
-    add_column :compras_budget_structures, :parent_id, :integer
+    remove_column :budget_structures, :budget_structure
+    add_column :budget_structures, :code, :integer
+    add_column :budget_structures, :budget_structure_level_id, :integer
+    add_column :budget_structures, :parent_id, :integer
 
-    add_index :compras_budget_structures, :budget_structure_level_id
-    add_index :compras_budget_structures, :parent_id
-    add_foreign_key :compras_budget_structures, :compras_budget_structure_levels, :column => :budget_structure_level_id
-    add_foreign_key :compras_budget_structures, :compras_budget_structures, :column => :parent_id
+    add_index :budget_structures, :budget_structure_level_id
+    add_index :budget_structures, :parent_id
+    add_foreign_key :budget_structures, :budget_structure_levels, :column => :budget_structure_level_id
+    add_foreign_key :budget_structures, :budget_structures, :column => :parent_id
   end
 
   def down
-    add_column :compras_budget_structures, :budget_structure, :integer
-    remove_column :compras_budget_structures, :code
-    remove_column :compras_budget_structures, :budget_structure_level_id
-    remove_column :compras_budget_structures, :parent_id
+    add_column :budget_structures, :budget_structure, :integer
+    remove_column :budget_structures, :code
+    remove_column :budget_structures, :budget_structure_level_id
+    remove_column :budget_structures, :parent_id
   end
 end
