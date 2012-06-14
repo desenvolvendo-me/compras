4a0c04477cc01f335686af781d67554778916e9d
Eduardo Resende Pereira <ed.resende@gmail.com>
2012-06-14 11:59:43 -0300

    confirming user created by db:seed rake task

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,10 @@
 # encoding: utf-8
 ActiveRecord::Base.transaction do
-  User.create!(:name => "Compras e Licitações", :email => "compras-e-licitacoes@nobesistemas.com.br", :login => "compras", :password => "123456", :password_confirmation => "123456") do |user|
+  user = User.create!(:name => "Compras e Licitações", :email => "compras-e-licitacoes@nobesistemas.com.br", :login => "compras", :password => "123456", :password_confirmation => "123456") do |user|
     user.administrator = true
   end
+
+  user.confirm!
 end
 
 Dir[Rails.root.join("db/seeds/*.rb")].each do |file|
