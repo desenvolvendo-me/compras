75595491e3d38d1b74bc2f942e3111b654329b3f
Mateus Lorandi dos Santos <mcomogo@gmail.com>
2012-06-18 08:19:19 -0300

    AdministrativeProcessLot: move the remove button from decorator to view with condition

--- app/decorators/licitation_process_lot_decorator.rb
+++ app/decorators/licitation_process_lot_decorator.rb
@@ -2,10 +2,4 @@ class LicitationProcessLotDecorator < Decorator
   def winner_proposal_total_price
     helpers.number_to_currency component.winner_proposal_total_price if component.winner_proposal_total_price
   end
-
-  def remove_item_button
-    return unless component.licitation_process_updatable?
-
-    helpers.button_tag 'Remover', :type => :button,  :class => 'button negative modal-finder-remove'
-  end
 end
--- spec/decorators/licitation_process_lot_decorator_spec.rb
+++ spec/decorators/licitation_process_lot_decorator_spec.rb
@@ -8,17 +8,4 @@ describe LicitationProcessLotDecorator do
 
     subject.winner_proposal_total_price.should eq 'R$ 9,99'
   end
-
-  it 'should return nill when remove_iten_button for a not updatable licitartion_process' do
-    component.stub(:licitation_process_updatable? => false)
-
-    subject.remove_item_button.should eq nil
-  end
-
-  it 'should return button to remove item when remove_iten_button for an updatable licitartion_process' do
-    component.stub(:licitation_process_updatable? => true)
-    helpers.stub(:button_tag).with('Remover', { :type => :button, :class => "button negative modal-finder-remove" }).and_return('button')
-
-    subject.remove_item_button.should eq 'button'
-  end
 end
