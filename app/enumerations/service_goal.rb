8f3a358ba94723f1844177d6dd74edc7afb216d6
Rafael Felix <felix.rafael@gmail.com>
2012-06-18 17:21:32 -0300

    Added founded as a service_goal
    [Finishes #31399873]

--- app/enumerations/service_goal.rb
+++ app/enumerations/service_goal.rb
@@ -1,5 +1,5 @@
 class ServiceGoal < EnumerateIt::Base
   associate_values :bank_loan, :purchase_of_goods, :public_works,
     :location, :technical_cooperation, :contract_management, :trainees,
-    :service_delivery
+    :service_delivery, :founded
 end
