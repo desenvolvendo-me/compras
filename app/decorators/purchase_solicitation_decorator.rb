822b774046a9ae7415dea226e9911576ba5ef572
Tiago Andre Geraldi <virgulla@gmail.com>
2012-06-19 15:51:03 -0300

    Created Decorator with summary to Purchase Solicitation
    [Finishes #31377665]

--- app/decorators/purchase_solicitation_decorator.rb
+++ app/decorators/purchase_solicitation_decorator.rb
@@ -1,3 +1,8 @@
+# encoding: utf-8
 class PurchaseSolicitationDecorator < Decorator
   attr_modal :accounting_year, :kind, :delivery_location_id, :budget_structure_id
+
+  def summary
+    "Estrutura orçamentária solicitante: #{budget_structure} / Responsável pela solicitação: #{responsible} / Status: #{service_status_humanize}"
+  end
 end
---
+++ spec/decorators/purchase_solicitation_decorator_spec.rb
@@ -0,0 +1,13 @@
+# encoding: utf-8
+require 'decorator_helper'
+require 'app/decorators/purchase_solicitation_decorator'
+
+describe PurchaseSolicitationDecorator do
+  it 'should return budget_structure, responsible and status on summary' do
+    subject.stub(:budget_structure).and_return('Secretaria de educação')
+    subject.stub(:responsible).and_return('Nohup')
+    subject.stub(:service_status_humanize).and_return('Pendente')
+
+    subject.summary.should eq "Estrutura orçamentária solicitante: Secretaria de educação / Responsável pela solicitação: Nohup / Status: Pendente"
+  end
+end
