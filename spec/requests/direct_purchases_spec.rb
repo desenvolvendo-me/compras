e4e26de46da25f38ed604e7c9450c05fb5ce3df8
Tiago Andre Geraldi <virgulla@gmail.com>
2012-06-19 11:00:04 -0300

    Removed Direct purchases material modal filters
    [Finishes #31391365]

--- spec/requests/direct_purchases_spec.rb
+++ spec/requests/direct_purchases_spec.rb
@@ -553,46 +553,6 @@ feature "DirectPurchases" do
     end
   end
 
-  scenario 'filtering materials on items by licitation object' do
-    LicitationObject.make!(:ponte)
-    LicitationObject.make!(:viaduto)
-
-    click_link 'Solicitações'
-
-    click_link 'Solicitações de Compra Direta'
-
-    click_link 'Gerar Compra Direta'
-
-    within_tab 'Dados gerais' do
-      fill_modal 'Objeto da licitação', :with => 'Ponte', :field => 'Descrição'
-    end
-
-    within_tab 'Dotações' do
-      click_button 'Adicionar Dotação'
-      click_button 'Adicionar Item'
-
-      fill_modal 'Material', :with => '', :field => 'Descrição' do
-        click_button 'Pesquisar'
-
-        page.should have_content 'Antivirus'
-        page.should_not have_content 'Arame comum'
-      end
-    end
-
-    within_tab 'Dados gerais' do
-      fill_modal 'Objeto da licitação', :with => 'Viaduto', :field => 'Descrição'
-    end
-
-    within_tab 'Dotações' do
-      fill_modal 'Material', :with => '', :field => 'Descrição' do
-        click_button 'Pesquisar'
-
-        page.should have_content 'Arame comum'
-        page.should_not have_content 'Antivirus'
-      end
-    end
-  end
-
   scenario 'it must have at least one budget allocation with at least one item' do
     BudgetAllocation.make!(:alocacao)
 
