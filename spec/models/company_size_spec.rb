10533a2e22fb7fa899a806eeb62e912667d315bf
Eduardo Resende Pereira <ed.resende@gmail.com>
2012-06-20 20:06:00 -0300

    CompanySize: adding missing model test: has_one extended_company_size

--- spec/models/company_size_spec.rb
+++ spec/models/company_size_spec.rb
@@ -1,6 +1,8 @@
 require 'model_helper'
 require 'app/models/unico/company_size'
 require 'app/models/company_size'
+require 'app/models/extended_company_size'
 
 describe CompanySize do
+  it { should have_one(:extended_company_size).dependent(:destroy) }
 end
