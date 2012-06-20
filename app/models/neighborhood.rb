22634e8c08bce183ac5364d057d20e2f1ffa2307
Eduardo Resende Pereira <ed.resende@gmail.com>
2012-06-20 20:20:17 -0300

    Neighborhood: removing duplication (Unico)

--- app/models/neighborhood.rb
+++ app/models/neighborhood.rb
@@ -1,6 +1,4 @@
 class Neighborhood < Unico::Neighborhood
-  attr_accessible :name, :city_id, :district_id
-
   has_many :addresses, :dependent => :restrict
 
   scope :street_id, lambda { |street_id| joins(:streets).where { streets.id.eq(street_id) } }
