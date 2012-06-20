b6a1994d628abb0ba9b6a48016be8686d84823a6
Eduardo Resende Pereira <ed.resende@gmail.com>
2012-06-20 20:08:14 -0300

    Condominium: removing Unico duplication

--- app/models/condominium.rb
+++ app/models/condominium.rb
@@ -1,14 +1,4 @@
 class Condominium < Unico::Condominium
-  attr_accessible :name, :condominium_type
-
-  has_enumeration_for :condominium_type
-
-  validates :name, :condominium_type, :presence => true
-
   filterize
   orderize
-
-  def to_s
-    name.to_s
-  end
 end
