bf25e5f38f6d56c7c1dafcc04912e6617444ab90
Rafael Felix <felix.rafael@gmail.com>
2012-06-20 08:36:23 -0300

    Created uploader for documents

---
+++ app/uploaders/document_uploader.rb
@@ -0,0 +1,13 @@
+# encoding: utf-8
+
+class DocumentUploader < CarrierWave::Uploader::Base
+  storage :file
+
+  def store_dir
+    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+  end
+
+  def extension_white_list
+    %w(jpg jpeg gif png doc docx xls xlsx pdf)
+  end
+end
