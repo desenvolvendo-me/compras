5b9e489236515de7df2eb33898e61047e5d1e220
Eduardo Resende Pereira <ed.resende@gmail.com>
2012-06-20 20:18:36 -0300

    Legal Nature: removing duplication (Unico)

--- app/models/legal_nature.rb
+++ app/models/legal_nature.rb
@@ -1,6 +1,4 @@
 class LegalNature < Unico::LegalNature
-  has_enumeration_for :level_of_nature, :with => LevelOfLegalNature
-
   has_many :administration_types, :dependent => :restrict
   has_many :providers, :dependent => :restrict
 
