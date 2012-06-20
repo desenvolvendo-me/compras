70a672fcfb4f0f97c510ca4a81a8fcbce301ce1c
Eduardo Resende Pereira <ed.resende@gmail.com>
2012-06-20 20:25:36 -0300

    Person: removing duplication from model spec (Unico)

--- spec/models/person_spec.rb
+++ spec/models/person_spec.rb
@@ -50,9 +50,7 @@ describe Person do
     end
   end
 
-  it { should belong_to :personable }
   it { should have_one :employee }
-  it { should have_many(:providers).dependent(:restrict) }
 
   it { should have_many(:providers).dependent(:restrict) }
   it { should have_many(:economic_registrations).dependent(:restrict) }
@@ -62,38 +60,6 @@ describe Person do
   it { should have_many(:creditors).dependent(:restrict) }
   it { should have_many(:licitation_process_bidders).through(:accredited_representatives) }
   it { should have_many(:accredited_representatives).dependent(:restrict) }
-
-  context "validations" do
-    it "should validates phone" do
-      subject.should_not be_valid
-      subject.errors[:phone].should be_empty
-      subject.phone = '123456'
-      subject.should_not be_valid
-      subject.errors[:phone].should include 'não é válido'
-    end
-
-    it "should validates fax" do
-      subject.should_not be_valid
-      subject.errors[:fax].should be_empty
-      subject.fax = '123456'
-      subject.should_not be_valid
-      subject.errors[:fax].should include 'não é válido'
-    end
-
-    it "should validates phone" do
-      subject.should_not be_valid
-      subject.errors[:mobile].should be_empty
-      subject.mobile = '123456'
-      subject.should_not be_valid
-      subject.errors[:mobile].should include 'não é válido'
-    end
-
-    it { should validate_presence_of :name }
-    it { should validate_presence_of :personable }
-
-    it { should allow_value('gabriel.sobrinho@gmail.com').for(:email) }
-    it { should_not allow_value('missing.host').for(:email) }
-  end
 
   it 'should be special when personable_type is SpecialEntry' do
     subject.stub(:personable_type).and_return('SpecialEntry')
