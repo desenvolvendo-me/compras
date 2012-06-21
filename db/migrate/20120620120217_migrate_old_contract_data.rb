35deb5d27d4f45b5501aefbb5b9ef7843d3fb1f9
Rafael Felix <felix.rafael@gmail.com>
2012-06-21 14:28:15 -0300

    Added classes on the migration

--- db/migrate/20120620120217_migrate_old_contract_data.rb
+++ db/migrate/20120620120217_migrate_old_contract_data.rb
@@ -1,8 +1,11 @@
 class MigrateOldContractData < ActiveRecord::Migration
+  class Contract < Compras::Model; end
+  class ServiceOrContractType < Compras::Model; end
+
   def change
     Contract.find_each do |contract|
       Contract.update_all({
-        :sequential_number => (Contract.next_sequential(contract.year, contract.entity_id)-1), 
+        :sequential_number => (Contract.next_sequential(contract.year, contract.entity_id)-1),
         :service_or_contract_type_id => ServiceOrContractType.where { service_goal.eq(contract.kind) }.first.try(:id)
       }, { :id => contract.id })
     end
