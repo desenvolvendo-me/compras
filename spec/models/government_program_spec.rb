db2e85677f22e93d5de5c6377f89dfcffa1361b3
Dmitry Nix <dmitry.nix@gmail.com>
2012-06-24 08:29:52 -0300

    Remove entity/year from government_program model spec

--- spec/models/government_program_spec.rb
+++ spec/models/government_program_spec.rb
@@ -9,17 +9,11 @@ describe GovernmentProgram do
     subject.to_s.should eq 'Habitação'
   end
 
-  it { should belong_to :entity }
   it { should belong_to :descriptor }
 
   it { should have_many(:budget_allocations).dependent(:restrict) }
 
   it { should validate_presence_of :descriptor }
-  it { should validate_presence_of :entity }
-  it { should validate_presence_of :year }
   it { should validate_presence_of :description }
   it { should validate_presence_of :status }
-
-  it { should allow_value('1999').for(:year) }
-  it { should_not allow_value('201a').for(:year) }
 end
