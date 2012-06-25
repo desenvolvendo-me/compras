de1cce772d67d30f74ad1c1e0a112707f3f56dda
José Gomes Júnior <zegomesjf@gmail.com>
2012-06-25 16:05:33 -0300

    changes the order of fields in the forms.
    [ #31397285 ]

--- spec/requests/price_collections_spec.rb
+++ spec/requests/price_collections_spec.rb
@@ -31,10 +31,10 @@ feature "PriceCollections" do
       fill_modal 'Responsável', :with => '958473', :field => 'Matrícula'
       fill_modal 'Forma de pagamento', :with => 'Dinheiro', :field => 'Descrição'
       fill_in 'Prazo de entrega', :with => '1'
+      fill_in 'Vencimento', :with => I18n.l(Date.tomorrow)
       select 'ano/anos', :from => 'Período'
       fill_in 'Validade da proposta', :with => '1'
       select 'ano/anos', :from => 'Unidade da validade da proposta'
-      fill_in 'Vencimento', :with => I18n.l(Date.tomorrow)
       fill_in 'Objeto', :with => 'objeto da coleta'
       fill_in 'Observações', :with => 'observacoes da coleta'
     end
@@ -95,10 +95,10 @@ feature "PriceCollections" do
       fill_modal 'Responsável', :with => '958473', :field => 'Matrícula'
       fill_modal 'Forma de pagamento', :with => 'Dinheiro', :field => 'Descrição'
       fill_in 'Prazo de entrega', :with => '1'
+      fill_in 'Vencimento', :with => I18n.l(Date.tomorrow)
       select 'ano/anos', :from => 'Período'
       fill_in 'Validade da proposta', :with => '1'
       select 'ano/anos', :from => 'Unidade da validade da proposta'
-      fill_in 'Vencimento', :with => I18n.l(Date.tomorrow)
       fill_in 'Objeto', :with => 'objeto da coleta'
       fill_in 'Observações', :with => 'observacoes da coleta'
     end
@@ -153,10 +153,10 @@ feature "PriceCollections" do
       page.should have_field 'Responsável', :with => 'Gabriel Sobrinho'
       page.should have_field 'Forma de pagamento', :with => 'Dinheiro'
       page.should have_field 'Prazo de entrega', :with => '1'
+      page.should have_field 'Vencimento', :with => I18n.l(Date.tomorrow)
       page.should have_select 'Período', :selected => 'ano/anos'
       page.should have_field 'Validade da proposta', :with => '1'
       page.should have_select 'Unidade da validade da proposta', :selected => 'ano/anos'
-      page.should have_field 'Vencimento', :with => I18n.l(Date.tomorrow)
       page.should have_field 'Objeto', :with => 'objeto da coleta'
       page.should have_field 'Observações', :with => 'observacoes da coleta'
     end
@@ -209,10 +209,10 @@ feature "PriceCollections" do
       fill_modal 'Responsável', :with => '12903412', :field => 'Matrícula'
       fill_modal 'Forma de pagamento', :with => 'Cheque', :field => 'Descrição'
       fill_in 'Prazo de entrega', :with => '3'
+      fill_in 'Vencimento', :with => I18n.l(Date.tomorrow + 10.days)
       select 'mês/meses', :from => 'Período'
       fill_in 'Validade da proposta', :with => '3'
       select 'mês/meses', :from => 'Unidade da validade da proposta'
-      fill_in 'Vencimento', :with => I18n.l(Date.tomorrow + 10.days)
       fill_in 'Objeto', :with => 'novo objeto da coleta'
       fill_in 'Observações', :with => 'novo observacoes da coleta'
     end
@@ -267,10 +267,10 @@ feature "PriceCollections" do
       page.should have_field 'Responsável', :with => 'Wenderson Malheiros'
       page.should have_field 'Forma de pagamento', :with => 'Cheque'
       page.should have_field 'Prazo de entrega', :with => '3'
+      page.should have_field 'Vencimento', :with => I18n.l(Date.tomorrow + 10.days)
       page.should have_select 'Período', :selected => 'mês/meses'
       page.should have_field 'Validade da proposta', :with => '3'
       page.should have_select 'Unidade da validade da proposta', :selected => 'mês/meses'
-      page.should have_field 'Vencimento', :with => I18n.l(Date.tomorrow + 10.days)
       page.should have_field 'Objeto', :with => 'novo objeto da coleta'
       page.should have_field 'Observações', :with => 'novo observacoes da coleta'
     end
@@ -324,10 +324,10 @@ feature "PriceCollections" do
       fill_modal 'Responsável', :with => '958473', :field => 'Matrícula'
       fill_modal 'Forma de pagamento', :with => 'Dinheiro', :field => 'Descrição'
       fill_in 'Prazo de entrega', :with => '1'
+      fill_in 'Vencimento', :with => I18n.l(Date.tomorrow)
       select 'ano/anos', :from => 'Período'
       fill_in 'Validade da proposta', :with => '1'
       select 'ano/anos', :from => 'Unidade da validade da proposta'
-      fill_in 'Vencimento', :with => I18n.l(Date.tomorrow)
       fill_in 'Objeto', :with => 'objeto da coleta'
       fill_in 'Observações', :with => 'observacoes da coleta'
     end
@@ -435,10 +435,10 @@ feature "PriceCollections" do
       fill_modal 'Responsável', :with => '958473', :field => 'Matrícula'
       fill_modal 'Forma de pagamento', :with => 'Dinheiro', :field => 'Descrição'
       fill_in 'Prazo de entrega', :with => '1'
+      fill_in 'Vencimento', :with => I18n.l(Date.tomorrow)
       select 'ano/anos', :from => 'Período'
       fill_in 'Validade da proposta', :with => '1'
       select 'ano/anos', :from => 'Unidade da validade da proposta'
-      fill_in 'Vencimento', :with => I18n.l(Date.tomorrow)
       fill_in 'Objeto', :with => 'objeto da coleta'
       fill_in 'Observações', :with => 'observacoes da coleta'
     end
@@ -683,10 +683,10 @@ feature "PriceCollections" do
       page.should have_disabled_field 'Responsável'
       page.should have_disabled_field 'Forma de pagamento'
       page.should have_disabled_field 'Prazo de entrega'
+      page.should have_disabled_field 'Vencimento'
       page.should have_disabled_field 'Período'
       page.should have_disabled_field 'Validade da proposta'
       page.should have_disabled_field 'Unidade da validade da proposta'
-      page.should have_disabled_field 'Vencimento'
       page.should have_disabled_field 'Objeto'
       page.should have_disabled_field 'Observações'
     end
