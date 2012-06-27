3645ae266a9627b82cb0c7068224d9d4f48fb36a
Gabriel Sobrinho <gabriel.sobrinho@gmail.com>
2012-06-27 16:30:05 -0300

    Reflect decimal precision on price format
    Author:    Gabriel Sobrinho <gabriel.sobrinho@gmail.com>
    Author:    José Gomes Júnior <zegomesjf@gmail.com>

--- app/assets/javascripts/simple_form.decimal_input.js
+++ app/assets/javascripts/simple_form.decimal_input.js
@@ -1,9 +1,10 @@
-(function ($) {
-  $('input[data-decimal]').live('focus', function () {
-    $(this).priceFormat({
-      prefix: '',
-      thousandsSeparator: '.',
-      centsSeparator: ','
-    });
+$(document).on("focus", "input[data-decimal]", function () {
+  var input = $(this);
+
+  input.priceFormat({
+    prefix: false,
+    thousandsSeparator: ".",
+    centsSeparator: ",",
+    centsLimit: input.data("precision")
   });
-})(jQuery);
+});
--- lib/compras/inputs/decimal_input.rb
+++ lib/compras/inputs/decimal_input.rb
@@ -16,9 +16,35 @@ module Compras
       def input_html_options
         super.tap do |options|
           options[:type]              ||= :string
+          options[:maxlength]         ||= maxlength
           options[:data]              ||= {}
           options[:data][:decimal]    ||= true
+          options[:data][:precision]  ||= precision
         end
+      end
+
+      def maxlength
+        return unless column_with_precision_and_scale?
+
+        points, missing = (column.precision - column.scale).divmod(3)
+
+        if missing.zero?
+          column.precision + points
+        else
+          column.precision + points + 1
+        end
+      end
+
+      def precision
+        return unless column_with_precision_and_scale?
+
+        column.scale
+      end
+
+      private
+
+      def column_with_precision_and_scale?
+        column && column.precision && column.scale
       end
     end
   end
