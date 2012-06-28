5704fb41334812e251be0a13d5c0632d8a23729e
Dmitry Nix <dmitry.nix@gmail.com>
2012-06-28 10:51:24 -0300

    Add tests to set_code callback at budget_revenue model

--- spec/models/budget_revenue_spec.rb
+++ spec/models/budget_revenue_spec.rb
@@ -26,4 +26,17 @@ describe BudgetRevenue do
     subject.stub(:divide?).and_return(false)
     subject.should_not validate_presence_of :value
   end
+
+  context 'auto set code' do
+    it 'should set 1 as code when have not other' do
+      subject.run_callbacks(:create)
+      subject.code.should eq 1
+    end
+
+    it 'should set 2 as code when have other' do
+      subject.stub(:last_code).and_return(1)
+      subject.run_callbacks(:create)
+      subject.code.should eq 2
+    end
+  end
 end
