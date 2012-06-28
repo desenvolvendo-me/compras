7726c3dc03236511cbbaa85d6f206ba396f88d77
Tiago Andre Geraldi <virgulla@gmail.com>
2012-06-28 11:49:45 -0300

    Ocurrence Contractual Historic: added callback test

--- spec/models/occurrence_contractual_historic_spec.rb
+++ spec/models/occurrence_contractual_historic_spec.rb
@@ -14,4 +14,33 @@ describe OccurrenceContractualHistoric do
     subject.sequence = 1
     subject.to_s.should eq '1'
   end
+
+  describe '#next_code' do
+    context 'when the code of last object is 3' do
+      before do
+        subject.stub(:last_code).and_return(3)
+      end
+
+      it 'should be 4' do
+        subject.next_code.should eq 4
+      end
+    end
+  end
+
+  describe 'generate the code' do
+    before do
+      subject.stub(:last_code).and_return 1
+    end
+
+    it 'assigns the next_code' do
+      subject.run_callbacks(:create)
+      subject.sequence.should eq 2
+    end
+
+    it 'overrides the number' do
+      subject.sequence = 30
+      subject.run_callbacks(:create)
+      subject.sequence.should eq 2
+    end
+  end
 end
