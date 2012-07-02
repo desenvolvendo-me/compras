67b66431b3db33c4db992f0c05a27265b3cbdf90
Dmitry Nix <dmitry.nix@gmail.com>
2012-07-02 09:46:14 -0300

    Refactory licitation_process_appeal model specs to validate appeal_date.

--- spec/models/licitation_process_appeal_spec.rb
+++ spec/models/licitation_process_appeal_spec.rb
@@ -20,17 +20,26 @@ describe LicitationProcessAppeal do
   it { should validate_presence_of :licitation_process }
   it { should validate_presence_of :appeal_date }
 
-  context "validating appeal_date" do
-    context "should be equal or greater than process_date" do
-      let(:licitation_process) do
-        double('licitation_process', :id => 1, :process_date => Date.current)
-      end
+  context 'validating appeal_date' do
+    before(:each) do
+      subject.stub(:licitation_process).and_return(licitation_process)
+    end
 
-      it 'should not be valid' do
-        subject.stub(:appeal_date => Date.yesterday, :licitation_process => licitation_process)
-        subject.should_not be_valid
-        subject.errors[:appeal_date].should include "deve ser maior ou igual a data do processo (#{I18n.l Date.current})"
-      end
+    let :licitation_process do
+      double('licitation_process', :process_date => Date.new(2012, 12, 13))
+    end
+
+    it 'be valid when appeal_date is after process_date' do
+      subject.should allow_value(Date.new(2012, 12, 20)).for(:appeal_date)
+    end
+
+    it 'be valid when appeal_date is equals to process_date' do
+      subject.should allow_value(Date.new(2012, 12, 13)).for(:appeal_date)
+    end
+
+    it 'be invalid when appeal_date is before process_date' do
+      subject.should_not allow_value(Date.new(2012, 1, 1)).for(:appeal_date).
+                                                           with_message('deve ser maior ou igual a data do processo (13/12/2012)')
     end
   end
 end
