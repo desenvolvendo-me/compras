4bfac9f70343e240a0bf47b74df5c46165460fc6
José Gomes Júnior <zegomesjf@gmail.com>
2012-07-03 03:29:04 -0300

    Change the totals sums to read directly from pledge on pledge_cancellations.
    [Finishes #32054403]

--- spec/requests/pledge_cancellations_spec.rb
+++ spec/requests/pledge_cancellations_spec.rb
@@ -31,27 +31,12 @@ feature "PledgeCancellations" do
     page.should have_disabled_field 'Data de emissão'
     page.should have_field 'Data de emissão', :with => I18n.l(Date.current)
 
-    within '#parcel_1' do
-      page.should have_content '1'
-      page.should have_content I18n.l(Date.current + 1.day)
-      find('.value').should have_content 'R$ 100,00'
-      find('.canceled_value').should have_content 'R$ 100,00'
-      find('.balance').should have_content 'R$ 0,00'
-      find('.liquidations_value').should have_content 'R$ 0,00'
+    within '.pledge_totals' do
+      page.find('#pledge_value').should have_content 'R$ 200,00'
+      page.find('#pledge_cancellations_sum').should have_content 'R$ 150,00'
+      page.find('#pledge_liquidations_sum').should have_content 'R$ 0,00'
+      page.find('#pledge_balance').should have_content 'R$ 50,00'
     end
-
-    within '#parcel_2' do
-      page.should have_content '2'
-      page.should have_content I18n.l(Date.current + 2.day)
-      find('.value').should have_content 'R$ 100,00'
-      find('.canceled_value').should have_content 'R$ 50,00'
-      find('.balance').should have_content 'R$ 50,00'
-      find('.liquidations_value').should have_content 'R$ 0,00'
-    end
-
-    page.find('#pledge_value').should have_content 'R$ 200,00'
-    page.find('#pledge_cancellations_sum').should have_content 'R$ 150,00'
-    page.find('#pledge_balance').should have_content 'R$ 50,00'
 
     page.should have_field 'Valor a ser anulado', :with => '150,00'
     page.should have_field 'Data *', :with => I18n.l(Date.current + 1.day)
@@ -71,34 +56,20 @@ feature "PledgeCancellations" do
     page.should have_disabled_field 'Data de emissão'
     page.should have_field 'Data de emissão', :with => I18n.l(Date.current)
 
-    within '#parcel_1' do
-      page.should have_content '1'
-      page.should have_content I18n.l(Date.current + 1.day)
-      find('.value').should have_content 'R$ 9,99'
-      find('.canceled_value').should have_content 'R$ 0,00'
-      find('.balance').should have_content 'R$ 9,99'
-      find('.liquidations_value').should have_content 'R$ 0,00'
+    within '.pledge_totals' do
+      page.find('#pledge_value').should have_content 'R$ 9,99'
+      page.find('#pledge_cancellations_sum').should have_content 'R$ 0,00'
+      page.find('#pledge_liquidations_sum').should have_content 'R$ 0,00'
+      page.find('#pledge_balance').should have_content 'R$ 9,99'
     end
 
-    page.find('#pledge_value').should have_content 'R$ 9,99'
-    page.find('#pledge_cancellations_sum').should have_content 'R$ 0,00'
-    page.find('#pledge_balance').should have_content 'R$ 9,99'
-
     clear_modal 'Empenho'
+
     page.should have_disabled_field 'Data de emissão'
     page.should have_field 'Data de emissão', :with => ''
 
-    within '#pledge_parcels' do
-      page.should_not have_content '1'
-      page.should_not have_content I18n.l(Date.current + 1.day)
-      page.should_not have_content 'R$ 9,99'
-      page.should_not have_content 'R$ 0,00'
-      page.should_not have_content 'R$ 9,99'
-    end
-
-    page.find('#pledge_value').should_not have_content 'R$ 9,99'
-    page.find('#pledge_cancellations_sum').should_not have_content 'R$ 0,00'
-    page.find('#pledge_balance').should_not have_content 'R$ 9,99'
+    page.should_not have_content('R$ 9,99')
+    page.should_not have_content('R$ 0,00')
   end
 
   scenario 'should have all fields disabled when editing an existent pledge' do
