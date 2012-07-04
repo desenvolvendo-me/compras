74ec69484ef7c484b30dd04e1abcc4a34c65b2c3
Mateus Lorandi dos Santos <mcomogo@gmail.com>
2012-07-04 16:48:54 -0300

    Fix include require 'app/models/unico/address' on street_spec model

--- spec/models/street_spec.rb
+++ spec/models/street_spec.rb
@@ -1,6 +1,7 @@
 require 'model_helper'
 require 'app/models/unico/street'
 require 'app/models/street'
+require 'app/models/unico/address'
 require 'app/models/address'
 require 'app/models/unico/neighborhood'
 
