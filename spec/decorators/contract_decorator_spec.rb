95f92d6ee3a01335a828c4bc2f3e4df4e6353b53
Tiago Andre Geraldi <virgulla@gmail.com>
2012-07-04 16:21:13 -0300

    Fixed Contract decorator spec

--- spec/decorators/contract_decorator_spec.rb
+++ spec/decorators/contract_decorator_spec.rb
@@ -4,9 +4,9 @@ require 'app/decorators/contract_decorator'
 
 describe ContractDecorator do
   it 'should return formatted total pledges value as currency' do
-    component.stub(:pledges_total_value => 100.0)
+    component.stub(:all_pledges_total_value => 100.0)
     helpers.stub(:number_to_currency).with(100.0).and_return('R$ 100,00')
 
-    subject.pledges_total_value.should eq 'R$ 100,00'
+    subject.all_pledges_total_value.should eq 'R$ 100,00'
   end
 end
