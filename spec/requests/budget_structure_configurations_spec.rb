1a994e4f18f9293d0e45db66a76acfaf3c7f3350
José Gomes Júnior <zegomesjf@gmail.com>
2012-07-04 10:43:19 -0300

    changes the order and size of fields in the forms.
    [Finishes #31397285]

--- spec/requests/budget_structure_configurations_spec.rb
+++ spec/requests/budget_structure_configurations_spec.rb
@@ -7,7 +7,6 @@ feature "BudgetStructureConfigurations" do
   end
 
   scenario 'create a new budget_structure_configuration' do
-    pending 'awaiting a response from the issue https://github.com/jonleighton/poltergeist/issues/88'
 
     Entity.make!(:detran)
     RegulatoryAct.make!(:sopa)
