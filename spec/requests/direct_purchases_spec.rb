db9d46fd69aff573956ed10fb6bdfa5213f70da3
Mateus Lorandi dos Santos <mcomogo@gmail.com>
2012-07-05 10:48:48 -0300

    DirectPurchase: add whitelines in spec request

--- spec/requests/direct_purchases_spec.rb
+++ spec/requests/direct_purchases_spec.rb
@@ -133,11 +133,13 @@ feature "DirectPurchases" do
       fill_modal 'Referência legal', :with => 'Referencia legal', :field => 'Descrição'
       select 'Material ou serviços', :from => 'Modalidade'
       select 'Global', :from => 'Tipo do empenho'
+
       within_modal 'Fornecedor' do
         fill_modal 'Pessoa', :with => 'Wenderson Malheiros', :field => 'Nome'
         click_button 'Pesquisar'
         click_record 'Wenderson Malheiros'
       end
+
       fill_modal 'Estrutura orçamentaria', :with => 'Secretaria de Educação', :field => 'Descrição'
       fill_modal 'Objeto da licitação', :with => 'Ponte', :field => 'Descrição'
       fill_modal 'Local de entrega', :with => 'Secretaria da Educação', :field => 'Descrição'
