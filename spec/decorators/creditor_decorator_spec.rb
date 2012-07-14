d8671032aa956ba2fe7fa75b5bf4c832904a6860
Dmitry Nix <dmitry.nix@gmail.com>
2012-07-14 10:32:01 -0300

    Use 2012 as sample year of person and creditor decorator.

--- spec/decorators/creditor_decorator_spec.rb
+++ spec/decorators/creditor_decorator_spec.rb
@@ -4,13 +4,13 @@ require 'app/decorators/creditor_decorator'
 
 describe CreditorDecorator do
   let :date do
-    Date.new(2142, 12, 14)
+    Date.new(2012, 12, 14)
   end
 
   it 'should return commercial_registration_date' do
     component.stub(:commercial_registration_date).and_return(date)
-    helpers.stub(:l).with(date).and_return('14/12/2142')
+    helpers.stub(:l).with(date).and_return('14/12/2012')
 
-    subject.commercial_registration_date.should eq '14/12/2142'
+    subject.commercial_registration_date.should eq '14/12/2012'
   end
 end
