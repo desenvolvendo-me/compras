089db4af25f6fee60a8a2cb30e1328cc53d7e900
Dmitry Nix <dmitry.nix@gmail.com>
2012-07-16 10:53:52 -0300

    Refactory PledgeHistoric descriptor validation.
    Use EnumerateIt helper.

--- app/models/pledge_historic.rb
+++ app/models/pledge_historic.rb
@@ -1,14 +1,14 @@
 class PledgeHistoric < Compras::Model
   attr_accessible :descriptor_id, :description, :source
 
-  has_enumeration_for :source
+  has_enumeration_for :source, :create_helpers => true
 
   belongs_to :descriptor
 
   has_many :pledges, :dependent => :restrict
 
   validates :description, :presence => true
-  validates :descriptor, :presence => true, :unless => Proc.new { |ph| ph.source == Source::DEFAULT }
+  validates :descriptor, :presence => true, :unless => :default?
 
   orderize :description
   filterize
