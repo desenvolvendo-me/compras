01e3b112f7225b654943a6412c50b5f761e888a6
Dmitry Nix <dmitry.nix@gmail.com>
2012-07-17 15:32:22 -0300

    Reorder revenue_nature modal finder fields.
    [Finishes #32861603]

--- app/decorators/revenue_nature_decorator.rb
+++ app/decorators/revenue_nature_decorator.rb
@@ -1,4 +1,9 @@
 class RevenueNatureDecorator < Decorator
+  attr_modal :revenue_nature, :specification, :descriptor_id,
+             :regulatory_act_id, :kind, :revenue_category_id,
+             :revenue_subcategory_id, :revenue_source_id, :revenue_rubric_id,
+             :classification
+
   def publication_date
     helpers.l super if super
   end
