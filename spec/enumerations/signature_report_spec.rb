c63ffbf97e474de21da90ebb43fc00299984f412
José Gomes Júnior <zegomesjf@gmail.com>
2012-07-19 14:11:43 -0300

    Add registration cadastral certificates to signature spec test.
    [Fix #32936937]

--- spec/enumerations/signature_report_spec.rb
+++ spec/enumerations/signature_report_spec.rb
@@ -5,12 +5,12 @@ require 'app/enumerations/signature_report'
 describe SignatureReport do
   it 'should return availables' do
     described_class.stub(:unavailables_keys).and_return(['supply_authorizations'])
-    described_class.availables.should eq [['Processos Administrativos', 'administrative_processes']]
+    described_class.availables.should eq [["Certificados de Registro Cadastral", "registration_cadastral_certificates"], ["Processos Administrativos", "administrative_processes"]]
   end
 
   it 'should return availables_keys' do
     described_class.stub(:unavailables_keys).and_return(['supply_authorizations'])
-    described_class.availables_keys.should eq ['administrative_processes']
+    described_class.availables_keys.should eq ['administrative_processes', 'registration_cadastral_certificates']
   end
 
   it 'should return formatted reports' do
