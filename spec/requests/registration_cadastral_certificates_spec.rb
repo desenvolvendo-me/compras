2cc72a6a24f8241c0d29392bcb692a528088b5b8
José Gomes Júnior <zegomesjf@gmail.com>
2012-07-20 13:36:24 -0300

    add #id to table to improve test way and replace fix date with ruby code on request test.

--- spec/requests/registration_cadastral_certificates_spec.rb
+++ spec/requests/registration_cadastral_certificates_spec.rb
@@ -15,37 +15,49 @@ feature "RegistrationCadastralCertificates" do
     click_link 'Nohup'
 
     click_link 'CRC'
-    
+
     click_link '1/2012'
 
     click_link 'Imprimir certificado de registro cadastral'
 
     page.should have_content 'CERTIFICADO DE REGISTRO CADASTRAL'
-    page.should have_content '18/07/2012'
-    page.should have_content '19/07/2012'
-    page.should have_content 'Nohup'
-    page.should have_content '1'
-    page.should have_content 'Rua Girassol, 9874 - São Francisco'
-    page.should have_content 'Curitiba'
-    page.should have_content 'Parana'
-    page.should have_content 'Brasil'
-    page.should have_content '33400-500'
-    page.should have_content '(11) 7070-9999'
-    page.should have_content '00.000.000/9999-62'
-    page.should have_content 'SP'
-    page.should have_content 'Wenderson Malheiros'
-    page.should have_content '003.149.513-34'
-    page.should have_content 'Especificação do certificado do registro cadastral'
+    page.should have_content I18n.l(Date.yesterday)
+    page.should have_content I18n.l(Date.current)
+
+    within '#dados-gerais' do
+      page.should have_content 'Nohup'
+      page.should have_content '1'
+      page.should have_content 'Rua Girassol, 9874 - São Francisco'
+      page.should have_content 'Curitiba'
+      page.should have_content 'Parana'
+      page.should have_content 'Brasil'
+      page.should have_content '33400-500'
+      page.should have_content '(11) 7070-9999'
+      page.should have_content '00.000.000/9999-62'
+      page.should have_content 'SP'
+      page.should have_content 'Wenderson Malheiros'
+      page.should have_content '003.149.513-34'
+      page.should have_content 'Especificação do certificado do registro cadastral'
+    end
+
     page.should have_content 'RAMO DE ATIVIDADE'
-    page.should have_content '4712100'
-    page.should have_content 'Comércio varejista de mercadorias em geral'
-    page.should have_content '7739099'
-    page.should have_content 'Aluguel de outras máquinas'
+
+    within '#ramo-de-atividade' do
+      page.should have_content '4712100'
+      page.should have_content 'Comércio varejista de mercadorias em geral'
+      page.should have_content '7739099'
+      page.should have_content 'Aluguel de outras máquinas'
+    end
+
     page.should have_content 'DOCUMENTAÇÃO'
-    page.should have_content 'Fiscal'
-    page.should have_content '123456'
-    page.should have_content '19/07/2012'
-    page.should have_content '20/07/2012'
+
+    within '#documentacao' do
+      page.should have_content 'Fiscal'
+      page.should have_content '123456'
+      page.should have_content I18n.l(Date.current)
+      page.should have_content I18n.l(Date.tomorrow)
+    end
+
     page.should have_content 'Este certificado obedece o estipulado na lei 8.666 de 21.06.1993 e suas atualizações.'
     page.should have_content "Belo Horizonte, #{I18n.l(Date.current, :format => :long)}"
   end
