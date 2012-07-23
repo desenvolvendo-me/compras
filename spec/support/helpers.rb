9d265c809f99ae39fec55cb3a975edd06a42f072
Tiago Andre Geraldi <virgulla@gmail.com>
2012-07-23 16:23:10 -0300

    Revert "Added helper to close dialog"
    This reverts commit a75cf3cd7f2f21f2055c406c1466f94677f91a9d.

--- spec/support/helpers.rb
+++ spec/support/helpers.rb
@@ -69,10 +69,6 @@ module Helpers
     end
   end
 
-  def close_dialog
-    page.find(:xpath, '(//a[contains(@class, "ui-dialog-titlebar-close")])[last()]').click
-  end
-
   def within_links
     within '.links' do
       yield
