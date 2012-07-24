60b2e7790bebfc5d6bb7f3d6a8c56b6ed5c4c58e
Dmitry Nix <dmitry.nix@gmail.com>
2012-07-24 14:12:05 -0300

    Insert MovimentType model into migrate to migrate MovimentTypeOperation enumeration value

--- db/migrate/20120723184857_migrate_moviment_type_operation.rb
+++ db/migrate/20120723184857_migrate_moviment_type_operation.rb
@@ -1,4 +1,6 @@
 class MigrateMovimentTypeOperation < ActiveRecord::Migration
+  class MovimentType < Compras::Model; end
+
   def change
     MovimentType.where(:operation => 'sum').update_all(:operation => 'add')
     MovimentType.where(:operation => 'subtraction').update_all(:operation => 'subtract')
