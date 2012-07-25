e6a5549ed87e020a0e1efd04a9897aaded86c278
Tiago Andre Geraldi <virgulla@gmail.com>
2012-07-25 09:56:47 -0300

    Signature Configuration: applied validator of duplicated items

--- app/models/signature_configuration.rb
+++ app/models/signature_configuration.rb
@@ -9,8 +9,8 @@ class SignatureConfiguration < Compras::Model
 
   validates :report, :presence => true
   validates :report, :uniqueness => true, :allow_blank => true
-  validate :cannot_have_duplicated_order
-  validate :cannot_have_duplicated_signature
+  validates :signature_configuration_items, :no_duplication => :order
+  validates :signature_configuration_items, :no_duplication => :signature_id
 
   def self.unavailables_reports
     select('distinct report').pluck(:report)
@@ -22,29 +22,4 @@ class SignatureConfiguration < Compras::Model
   def to_s
     report_humanize
   end
-
-  def cannot_have_duplicated_order
-   single_orders = []
-
-   signature_configuration_items.reject(&:marked_for_destruction?).each do |item|
-     if single_orders.include?(item.order)
-       errors.add(:signature_configuration_items)
-       item.errors.add(:order, :taken)
-     end
-     single_orders << item.order
-   end
-  end
-
-  def cannot_have_duplicated_signature
-   single_signatures = []
-
-   signature_configuration_items.reject(&:marked_for_destruction?).each do |item|
-     if single_signatures.include?(item.signature)
-       errors.add(:signature_configuration_items)
-       item.errors.add(:signature, :taken)
-     end
-     single_signatures << item.signature
-   end
-  end
-
 end
--- spec/models/signature_configuration_spec.rb
+++ spec/models/signature_configuration_spec.rb
@@ -2,6 +2,7 @@
 require 'model_helper'
 require 'app/models/signature_configuration'
 require 'app/models/signature_configuration_item'
+require 'app/models/signature'
 
 describe SignatureConfiguration do
   it 'should return report_humanize as to_s' do
@@ -13,25 +14,6 @@ describe SignatureConfiguration do
 
   it { should validate_presence_of :report }
 
-  context 'check duplicated items' do
-    let :signature do
-      double('Signature', :id => 1)
-    end
-
-  it "the duplicated orders and signatures should be invalid except the first" do
-      item_one = subject.signature_configuration_items.build(:order => '10')
-      item_two = subject.signature_configuration_items.build(:order => '10')
-
-      item_one.stub(:signature => signature)
-      item_two.stub(:signature => signature)
-
-      subject.valid?
-
-      item_one.errors.messages[:order].should be_nil
-      item_two.errors.messages[:order].should include "já está em uso"
-
-      item_one.errors.messages[:signature].should be_nil
-      item_two.errors.messages[:signature].should include "já está em uso"
-    end
-  end
+  it { should validate_duplication_of(:order).on(:signature_configuration_items) }
+  it { should validate_duplication_of(:signature_id).on(:signature_configuration_items) }
 end
