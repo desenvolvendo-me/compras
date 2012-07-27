eb2d33c872ae1699ff05f98addb9e66739d4e241
Dmitry Nix <dmitry.nix@gmail.com>
2012-07-27 12:00:32 -0300

    Fix price_collection_lot_item_decorator_spec to test PriceCollectionLotItemDecorator instead of PriceCollectionClassificationDecorator

--- spec/decorators/price_collection_lot_item_decorator_spec.rb
+++ spec/decorators/price_collection_lot_item_decorator_spec.rb
@@ -1,8 +1,8 @@
 # encoding: utf-8
 require 'decorator_helper'
-require 'app/decorators/price_collection_classification_decorator'
+require 'app/decorators/price_collection_lot_item_decorator'
 
-describe PriceCollectionClassificationDecorator do
+describe PriceCollectionLotItemDecorator do
   context '#unit_price_by_price_collection_and_creditor' do
     before do
       component.stub(:unit_price_by_price_collection_and_creditor).and_return(330.0)
