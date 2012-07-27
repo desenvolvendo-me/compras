6d11789c1913808deae93b2cd4776de20ecd2762
Mateus Lorandi dos Santos <mcomogo@gmail.com>
2012-07-27 11:12:48 -0300

    Remove mustache from licitation_process/_budget_allocations
    And remove decorators not used
    
    [Finishes #31702671]

--- app/decorators/administrative_process_budget_allocation_decorator.rb
+++ app/decorators/administrative_process_budget_allocation_decorator.rb
@@ -3,28 +3,12 @@ class AdministrativeProcessBudgetAllocationDecorator
   include Decore::Proxy
   include ActionView::Helpers::NumberHelper
 
-  def id_or_mustache_variable
-    component.id || "{{id}}"
+  def budget_allocation_amount
+    number_with_precision(super)
   end
 
-  def budget_allocation_id_or_mustache_variable
-    component.budget_allocation_id || "{{budget_allocation_id}}"
-  end
-
-  def budget_allocation_or_mustache_variable
-    component.budget_allocation || "{{description}}"
-  end
-
-  def budget_allocation_expense_nature_or_mustache_variable
-    component.budget_allocation_expense_nature || "{{expenseNature}}"
-  end
-
-  def budget_allocation_amount_or_mustache_variable
-    number_with_precision(component.budget_allocation_amount) || "{{amount}}"
-  end
-
-  def value_or_mustache_variable
-    number_with_precision(component.value) || "{{value}}"
+  def value
+    number_with_precision(super)
   end
 
   def total_items_value
--- spec/decorators/administrative_process_budget_allocation_decorator_spec.rb
+++ spec/decorators/administrative_process_budget_allocation_decorator_spec.rb
@@ -3,94 +3,16 @@ require 'decorator_helper'
 require 'app/decorators/administrative_process_budget_allocation_decorator'
 
 describe AdministrativeProcessBudgetAllocationDecorator do
-  context '#id_or_mustache_variable' do
-    context 'when do not have id' do
-      before do
-        component.stub(:id).and_return(nil)
-      end
+  it 'should return budget_allocation_amount' do
+    component.stub(:budget_allocation_amount).and_return(10)
 
-      it 'should return mustache variable' do
-        subject.id_or_mustache_variable.should eq "{{id}}"
-      end
-    end
-
-    context 'when do not have id' do
-      before do
-        component.stub(:id).and_return(1)
-      end
-
-      it 'should return id' do
-        subject.id_or_mustache_variable.should eq 1
-      end
-    end
+    subject.budget_allocation_amount.should eq "10,00"
   end
 
-  context '#budget_allocation_id_or_mustache_variable' do
-    it 'should return mustache variable' do
-      component.stub(:budget_allocation_id).and_return(nil)
-      subject.budget_allocation_id_or_mustache_variable.should eq "{{budget_allocation_id}}"
-    end
+  it 'should return value' do
+    component.stub(:value).and_return(15)
 
-    it 'should return budget_allocation_id' do
-      component.stub(:budget_allocation_id).and_return(1)
-      subject.budget_allocation_id_or_mustache_variable.should eq 1
-    end
-  end
-
-  context '#budget_allocation_or_mustache_variable' do
-    it 'should return mustache variable' do
-      component.stub(:budget_allocation).and_return(nil)
-
-      subject.budget_allocation_or_mustache_variable.should eq "{{description}}"
-    end
-
-    it 'should return budget_allocation' do
-      component.stub(:budget_allocation).and_return(1)
-
-      subject.budget_allocation_or_mustache_variable.should eq 1
-    end
-  end
-
-  context '#budget_allocation_expense_nature_or_mustache_variable' do
-    it 'should return mustache variable' do
-      component.stub(:budget_allocation_expense_nature).and_return(nil)
-
-      subject.budget_allocation_expense_nature_or_mustache_variable.should eq "{{expenseNature}}"
-    end
-
-    it 'should return expense_nature' do
-      component.stub(:budget_allocation_expense_nature).and_return(1)
-
-      subject.budget_allocation_expense_nature_or_mustache_variable.should eq 1
-    end
-  end
-
-  context '#budget_allocation_amount_or_mustache_variable' do
-    it 'should return mustache variable' do
-      component.stub(:budget_allocation_amount).and_return(nil)
-
-      subject.budget_allocation_amount_or_mustache_variable.should eq "{{amount}}"
-    end
-
-    it 'should return budget_allocation_amount' do
-      component.stub(:budget_allocation_amount).and_return("1,00")
-
-      subject.budget_allocation_amount_or_mustache_variable.should eq "1,00"
-    end
-  end
-
-  context '#value_or_mustache_variable' do
-    it 'should return mustache variable' do
-      component.stub(:value).and_return(nil)
-
-      subject.value_or_mustache_variable.should eq "{{value}}"
-    end
-
-    it 'should return value' do
-      component.stub(:value).and_return("1,00")
-
-      subject.value_or_mustache_variable.should eq "1,00"
-    end
+    subject.value.should eq "15,00"
   end
 
   context '#total_items_value' do
