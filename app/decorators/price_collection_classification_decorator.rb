2fbc803702f65bcd4ec1508da70459cab011b930
Dmitry Nix <dmitry.nix@gmail.com>
2012-07-27 11:52:51 -0300

    Remove unused decorators from PriceCollectionClassificationDecorator

--- app/decorators/price_collection_classification_decorator.rb
+++ app/decorators/price_collection_classification_decorator.rb
@@ -12,22 +12,6 @@ class PriceCollectionClassificationDecorator
     number_with_precision super if super
   end
 
-  def unit_price_by_proposal(proposal)
-    number_with_precision super if super
-  end
-
-  def total_value_by_proposal(proposal)
-    number_with_precision super if super
-  end
-
-  def unit_price_by_price_collection_and_creditor(price_collection, creditor)
-    number_with_precision super if super
-  end
-
-  def total_value_by_price_collection_and_creditor(price_collection, creditor)
-    number_with_precision super if super
-  end
-
   def classification
     I18n.translate((component.classification == 1).to_s)
   end
--- spec/decorators/price_collection_classification_decorator_spec.rb
+++ spec/decorators/price_collection_classification_decorator_spec.rb
@@ -23,46 +23,6 @@ describe PriceCollectionClassificationDecorator do
     end
   end
 
-  context '#unit_price_by_proposal' do
-    before do
-      component.stub(:unit_price_by_proposal).and_return(500.0)
-    end
-
-    it 'should applies currency' do
-      subject.unit_price_by_proposal(nil).should eq '500,00'
-    end
-  end
-
-  context '#total_value_by_proposal' do
-    before do
-      component.stub(:total_value_by_proposal).and_return(780.0)
-    end
-
-    it 'should applies precision' do
-      subject.total_value_by_proposal(nil).should eq '780,00'
-    end
-  end
-
-  context '#unit_price_by_price_collection_and_creditor' do
-    before do
-      component.stub(:unit_price_by_price_collection_and_creditor).and_return(330.0)
-    end
-
-    it 'should applies precision' do
-      subject.unit_price_by_price_collection_and_creditor(nil, nil).should eq '330,00'
-    end
-  end
-
-  context '#total_value_by_price_collection_and_creditor' do
-    before do
-      component.stub(:total_value_by_price_collection_and_creditor).and_return(220.0)
-    end
-
-    it 'should applies precision' do
-      subject.total_value_by_price_collection_and_creditor(nil, nil).should eq '220,00'
-    end
-  end
-
   context '#classification' do
     before do
       I18n.backend.store_translations 'pt-BR', :true => 'Sim'
