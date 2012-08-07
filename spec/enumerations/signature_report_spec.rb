360059b38ef10f76edfc0b2c365e98e6e32766f0
Mateus Lorandi dos Santos <mcomogo@gmail.com>
2012-08-07 10:18:46 -0300

    Fix SignatureReport enumeration test

--- spec/enumerations/signature_report_spec.rb
+++ spec/enumerations/signature_report_spec.rb
@@ -5,12 +5,12 @@ require 'app/enumerations/signature_report'
 describe SignatureReport do
   it 'should return availables' do
     described_class.stub(:unavailables_keys).and_return(['supply_authorizations'])
-    described_class.availables.should eq [["Certificados de Registro Cadastral", "registration_cadastral_certificates"], ["Processos Administrativos", "administrative_processes"]]
+    described_class.availables.should eq [["Certificados de Registro Cadastral", "registration_cadastral_certificates"], ["Licitation Process Ratifications", "licitation_process_ratifications"], ["Processos Administrativos", "administrative_processes"]]
   end
 
   it 'should return availables_keys' do
     described_class.stub(:unavailables_keys).and_return(['supply_authorizations'])
-    described_class.availables_keys.should eq ['administrative_processes', 'registration_cadastral_certificates']
+    described_class.availables_keys.should eq ['administrative_processes', 'licitation_process_ratifications', 'registration_cadastral_certificates']
   end
 
   it 'should return formatted reports' do
