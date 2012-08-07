af7b026f872f0037639fb4c65f2d6fd360384b5d
Dmitry Nix <dmitry.nix@gmail.com>
2012-08-07 09:33:11 -0300

    Fix syntax at licitation_process_bidder_proposal model

--- app/models/licitation_process_bidder_proposal.rb
+++ app/models/licitation_process_bidder_proposal.rb
@@ -2,7 +2,7 @@ class LicitationProcessBidderProposal < Compras::Model
   attr_accessible :licitation_process_bidder, :ratificated, :brand, :unit_price,
                   :administrative_process_budget_allocation_item,
                   :licitation_process_bidder_id,
-                  :administrative_process_budget_allocation_item_id,
+                  :administrative_process_budget_allocation_item_id
 
   has_enumeration_for :situation, :with => SituationOfProposal
 
