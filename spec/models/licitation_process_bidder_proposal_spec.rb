d83a9c65e1802a72bb72af256b0d7c57eb34cc6a
Dmitry Nix <dmitry.nix@gmail.com>
2012-08-07 09:23:42 -0300

    Avoid `.rb` to required filename at licitation_process_bidder_proposal model spec

--- spec/models/licitation_process_bidder_proposal_spec.rb
+++ spec/models/licitation_process_bidder_proposal_spec.rb
@@ -1,7 +1,7 @@
 # encoding: utf-8
 require 'model_helper'
 require 'app/models/licitation_process_bidder_proposal'
-require 'app/enumerations/situation_of_proposal.rb'
+require 'app/enumerations/situation_of_proposal'
 
 describe LicitationProcessBidderProposal do
 
