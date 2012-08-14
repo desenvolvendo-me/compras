f7a7e68fbf76ae61b01efc97e4fffc6b6399baec
Tiago Andre Geraldi <virgulla@gmail.com>
2012-08-14 15:06:07 -0300

    Supply Authorization Mailer: Removed unecessary attr_accessor

--- app/mailers/supply_authorization_mailer.rb
+++ app/mailers/supply_authorization_mailer.rb
@@ -2,17 +2,14 @@
 class SupplyAuthorizationMailer < ActionMailer::Base
   add_template_helper ApplicationHelper
 
-  attr_accessor :prefecture
-
   def authorization_to_creditor(direct_purchase, prefecture)
     @direct_purchase = direct_purchase
     @prefecture = prefecture
-    self.prefecture = prefecture
 
     mail :to => @direct_purchase.creditor_person_email, :subject => 'Autorização de Fornecimento', :from => email
   end
 
   def email
-    prefecture.email.blank? ? ActionMailer::Base.default[:from] : prefecture.email
+    @prefecture.email.blank? ? ActionMailer::Base.default[:from] : @prefecture.email
   end
 end
