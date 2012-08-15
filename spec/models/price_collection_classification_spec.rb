e77c6e5df68bb7a02ad5ef3f7291f4e16ee5f986
Tiago Andre Geraldi <virgulla@gmail.com>
2012-08-15 15:20:40 -0300

    Fixed identation

--- spec/models/price_collection_classification_spec.rb
+++ spec/models/price_collection_classification_spec.rb
@@ -3,20 +3,20 @@ require 'model_helper'
 require 'app/models/price_collection_classification'
 
 describe PriceCollectionClassification do
-    it { should belong_to :price_collection_proposal }
-    it { should belong_to :classifiable }
+  it { should belong_to :price_collection_proposal }
+  it { should belong_to :classifiable }
 
-    context '#disqualified' do
-      it 'should be false' do
-        subject.classification = 1
+  context '#disqualified' do
+    it 'should be false' do
+      subject.classification = 1
 
-        expect(subject.disqualified?).to be false
-      end
-
-      it 'should be true' do
-        subject.classification = -1
-
-        expect(subject.disqualified?).to be true
-      end
+      expect(subject.disqualified?).to be false
     end
+
+    it 'should be true' do
+      subject.classification = -1
+
+      expect(subject.disqualified?).to be true
+    end
+  end
 end
