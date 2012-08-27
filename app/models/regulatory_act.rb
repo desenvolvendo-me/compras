5b01c594777fcbcf1de09b127c10527a3827f5fe
Dmitry Nix <dmitry.nix@gmail.com>
2012-08-27 16:46:27 -0300

    Add presence validate to validates at regulatory_act

--- app/models/regulatory_act.rb
+++ app/models/regulatory_act.rb
@@ -23,7 +23,8 @@ class RegulatoryAct < Compras::Model
   has_one :extra_credit
 
   validates :regulatory_act_type, :creation_date, :publication_date, :content,
-            :signature_date, :vigor_date, :legal_text_nature, :act_number
+            :signature_date, :vigor_date, :legal_text_nature, :act_number,
+            :presence => true
 
   with_options :allow_blank => true do |allowing_blank|
     allowing_blank.validates :revenue_antecipation_percent, :numericality => { :less_than_or_equal_to => 100 }
