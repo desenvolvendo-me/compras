dfc1d62d6808fb7fd01818583e6177345f0d2261
Mateus Lorandi dos Santos <mcomogo@gmail.com>
2012-08-28 18:29:11 -0300

    PurchaseSolicitationItemGroup: group decorator test

--- spec/decorators/purchase_solicitation_item_group_decorator_spec.rb
+++ spec/decorators/purchase_solicitation_item_group_decorator_spec.rb
@@ -3,31 +3,39 @@ require 'decorator_helper'
 require 'app/decorators/purchase_solicitation_item_group_decorator'
 
 describe PurchaseSolicitationItemGroupDecorator do
-  it 'should allow_submit_button when it is not annulled and is editable' do
-    component.stub(:annulled?).and_return(false)
-    component.stub(:editable?).and_return(true)
+  context 'when not annulled' do
+    before do
+      component.stub(:annulled?).and_return(false)
+    end
 
-    expect(subject).to be_allow_submit_button
+    it 'should allow_submit_button when it is not annulled and is editable' do
+      component.stub(:editable?).and_return(true)
+
+      expect(subject).to be_allow_submit_button
+    end
+
+    it 'should not allow_submit_button when it is not annulled and is not editable' do
+      component.stub(:editable?).and_return(false)
+
+      expect(subject).not_to be_allow_submit_button
+    end
   end
 
-  it 'should not allow_submit_button when it is annulled and is editable' do
-    component.stub(:annulled?).and_return(true)
-    component.stub(:editable?).and_return(true)
+  context 'when is annulled' do
+    before do
+      component.stub(:annulled?).and_return(true)
+    end
 
-    expect(subject).not_to be_allow_submit_button
-  end
+    it 'should not allow_submit_button when it is annulled and is editable' do
+      component.stub(:editable?).and_return(true)
 
-  it 'should not allow_submit_button when it is not annulled and is not editable' do
-    component.stub(:annulled?).and_return(false)
-    component.stub(:editable?).and_return(false)
+      expect(subject).not_to be_allow_submit_button
+    end
 
-    expect(subject).not_to be_allow_submit_button
-  end
+    it 'should not allow_submit_button when it is annulled and is not editable' do
+      component.stub(:editable?).and_return(false)
 
-  it 'should not allow_submit_button when it is annulled and is not editable' do
-    component.stub(:annulled?).and_return(true)
-    component.stub(:editable?).and_return(false)
-
-    expect(subject).not_to be_allow_submit_button
+      expect(subject).not_to be_allow_submit_button
+    end
   end
 end
