c030b621fb988c914c2efb3ac47b193a9dd1b365
Tiago Andre Geraldi <virgulla@gmail.com>
2012-08-28 09:22:55 -0300

    Renamed test description

--- spec/enumerations/administrative_process_modality_spec.rb
+++ spec/enumerations/administrative_process_modality_spec.rb
@@ -4,11 +4,11 @@ require 'app/enumerations/administrative_process_modality'
 
 describe AdministrativeProcessModality do
   context 'available for licitation process classification' do
-    it 'should return true' do
+    it 'should be available for licitation process classification' do
       expect(described_class.available_for_licitation_process_classification?('competition_for_purchases_and_services')).to be true
     end
 
-    it 'should return false' do
+    it 'should not be available for licitation process classification' do
       expect(described_class.available_for_licitation_process_classification?('auction')).to be false
     end
   end
