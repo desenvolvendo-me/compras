192ca99cb032632fa3c037ca506e3c886940b064
Marcelo Cajueiro <marcelocajueiro@gmail.com>
2012-09-10 08:48:50 -0300

    Prevent initializing application and connecting to the database while precompiling assets
    Source: https://github.com/nohupbrasil/tributario/commit/d0ecec0f74d657cb11314926246fdaa2e913fdb2
    
    Credits: @sobrinho
    
    Finishes [#35759213]

--- config/application.rb
+++ config/application.rb
@@ -64,6 +64,10 @@ module Compras
     # Enable the asset pipeline
     config.assets.enabled = true
 
+    # Prevent initializing application and connecting to the database while precompiling assets
+    # See https://devcenter.heroku.com/articles/rails3x-asset-pipeline-cedar
+    config.assets.initialize_on_precompile = false
+
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
 
