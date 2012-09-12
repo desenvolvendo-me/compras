3b08e1b19b5df2f095de421edc33da8d1bdbd43a
Tiago Andre Geraldi <virgulla@gmail.com>
2012-09-12 19:59:35 -0300

    fixing test for new sintax

--- spec/integrations/tce_specification_capability_spec.rb
+++ spec/integrations/tce_specification_capability_spec.rb
@@ -8,14 +8,14 @@ describe TceSpecificationCapability do
 
     subject.tce_capability_agreements.build(:agreement_id => agreement.id)
     subject.valid?
-    subject.errors[:agreements].should include("não deve haver nenhum convênio inativo")
+    expect(subject.errors[:agreements]).to include("não deve haver nenhum convênio inativo")
   end
 
   it "should be valid when have a persisted inactive tce capability agreement" do
     subject = TceSpecificationCapability.make!(:turismo)
 
     subject.valid?
-    subject.errors[:agreements].should_not include("não deve haver nenhum convênio inativo")
+    expect(subject.errors[:agreements]).to_not include("não deve haver nenhum convênio inativo")
   end
 
   it "should be valid when try to add a new active tce capability agreement" do
@@ -23,6 +23,6 @@ describe TceSpecificationCapability do
 
     subject.tce_capability_agreements.build(:agreement_id => agreement.id)
     subject.valid?
-    subject.errors[:agreements].should_not include("não deve haver nenhum convênio inativo")
+    expect(subject.errors[:agreements]).to_not include("não deve haver nenhum convênio inativo")
   end
 end
