c794fa1529cb5ad1ad8dfde866c9872e307f2d4a
Cairo Noleto <caironoleto@gmail.com>
2012-09-14 16:03:11 -0300

    Adding carrierwave initializer
    Signed-off-by: Cairo Noleto <caironoleto@gmail.com>

---
+++ config/initializers/carrierwave.rb
@@ -0,0 +1,14 @@
+if Rails.env.production? || Rails.env.staging?
+  CarrierWave.configure do |config|
+    config.storage = :fog
+
+    config.fog_credentials = {
+      :provider              => 'AWS',
+      :aws_access_key_id     => ENV['S3_KEY'],
+      :aws_secret_access_key => ENV['S3_SECRET']
+    }
+
+    config.fog_directory = "frotas-#{Rails.env}"
+    config.fog_public = false
+  end
+end
