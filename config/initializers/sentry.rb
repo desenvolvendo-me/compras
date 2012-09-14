9cba83aee36c70248615d910f474cbbab726971f
Cairo Noleto <caironoleto@gmail.com>
2012-09-14 16:03:39 -0300

    Adding sentry initilizer
    Signed-off-by: Cairo Noleto <caironoleto@gmail.com>

---
+++ config/initializers/sentry.rb
@@ -0,0 +1,8 @@
+require 'raven'
+
+if Rails.env.production? || Rails.env.staging?
+  Raven.configure do |config|
+    config.dsn = ENV['SENTRY_DSN']
+    config.environments = %w[ staging production ]
+  end
+end
