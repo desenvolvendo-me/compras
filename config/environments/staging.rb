bde4550976c61dc6e84a61e1a5ab9808631c7173
José Gomes Júnior <zegomesjf@gmail.com>
2012-09-17 15:19:02 -0300

    remove config asset_host from environments.

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -43,7 +43,7 @@ Compras::Application.configure do
   # config.cache_store = :mem_cache_store
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server
-  config.action_controller.asset_host = "https://staging.compras.nobesistemas.com.br"
+  # config.action_controller.asset_host = "http://assets.example.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   config.assets.precompile += %w( devise.css report.css )
