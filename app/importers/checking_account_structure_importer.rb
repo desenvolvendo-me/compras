4548ef4d09fd87176ea61436f0f9fb8358edafbb
Tiago Andre Geraldi <virgulla@gmail.com>
2012-09-18 18:25:30 -0300

    Checking Account Structure Importer: allowing blank fields, doesn't validate
    [Finishes #35802297]

--- app/importers/checking_account_structure_importer.rb
+++ app/importers/checking_account_structure_importer.rb
@@ -12,6 +12,15 @@ class CheckingAccountStructureImporter < Importer
     self.checking_account_of_fiscal_account_repository = checking_account_of_fiscal_account_repository
   end
 
+  def import!
+    transaction do
+      parser.foreach(file, options) do |row|
+        checking_account_structure = repository.new(normalize_attributes(row.to_hash))
+        checking_account_structure.save(:validate => false)
+      end
+    end
+  end
+
   protected
 
   def normalize_attributes(attributes)
@@ -22,8 +31,8 @@ class CheckingAccountStructureImporter < Importer
     attributes.delete 'checking_account_of_fiscal_account'
     attributes.delete 'checking_account_structure_information'
 
-    attributes.merge('checking_account_structure_information_id' => structure_information.id,
-                     'checking_account_of_fiscal_account_id' => fiscal_account.id)
+    attributes.merge('checking_account_structure_information_id' => structure_information.try(:id),
+                     'checking_account_of_fiscal_account_id' => fiscal_account.try(:id))
   end
 
   def file
@@ -31,22 +40,26 @@ class CheckingAccountStructureImporter < Importer
   end
 
   def fiscal_account_by_attributes(attributes)
+    return unless attributes['checking_account_of_fiscal_account']
+
     fiscal_account_name = attributes['checking_account_of_fiscal_account']
     fiscal_account = checking_account_of_fiscal_account_repository.find_by_name(fiscal_account_name)
 
     unless fiscal_account
-      raise "O checking_account_fiscal_account #{fiscal_account_name ? fiscal_account_name : '<vazio>'} não existe na base de dados"
+      raise "O checking_account_fiscal_account #{fiscal_account_name} não existe na base de dados"
     end
 
     fiscal_account
   end
 
   def structure_information_by_attributes(attributes)
+    return unless attributes['checking_account_structure_information']
+
     structure_information_name = attributes['checking_account_structure_information']
     structure_information = checking_account_structure_information_repository.find_by_name(structure_information_name)
 
     unless structure_information
-      raise "O checking_account_structure_information #{structure_information_name ? structure_information_name : '<vazio>'} não existe na base de dados"
+      raise "O checking_account_structure_information #{structure_information_name} não existe na base de dados"
     end
 
     structure_information
--- spec/importers/checking_account_structure_importer_spec.rb
+++ spec/importers/checking_account_structure_importer_spec.rb
@@ -1,6 +1,7 @@
 # encoding: utf-8
 require 'importer_helper'
 require 'app/importers/checking_account_structure_importer'
+require 'active_support/core_ext/object/try'
 
 describe CheckingAccountStructureImporter do
   subject do
@@ -15,17 +16,22 @@ describe CheckingAccountStructureImporter do
     double("CheckingAccountOfFiscalAccount")
   end
 
+  let :checking_account_structure_object do
+    double(:save => true)
+  end
+
   let :fiscal_account do
-    double(:id => 2)
+    double(:id => 1)
   end
 
   let :structure_information do
     double(:id => 1)
   end
 
-  let :null_repository do repository = double.as_null_object
+  let :null_repository do
+    repository = double.as_null_object
 
-    repository.stub(:transaction) do |&block|
+    repository.should_receive(:transaction) do |&block|
       block.call
     end
 
@@ -33,24 +39,29 @@ describe CheckingAccountStructureImporter do
   end
 
   it 'should create two checking_account_structure and raise not found checking_account_structure_information' do
-    fiscal_account_repository.should_receive(:find_by_name).with("DISPONIBILIDADE FINANCEIRA").exactly(3).times.and_return(fiscal_account)
+    fiscal_account_repository.stub(:find_by_name => fiscal_account)
+    structure_information_repository.stub(:find_by_name => structure_information)
 
-    structure_information_repository.should_receive(:find_by_name).with("FONTE DE RECURSOS").and_return(structure_information)
+    null_repository.should_receive(:new).with({ "checking_account_structure_information_id" => 1,
+                                                 "checking_account_of_fiscal_account_id" => 1,
+                                                 "name" => "FonteRecursos", "description" => nil,
+                                                 "tag" => "FonteRecursos", "fill" => nil, "reference" => nil }).
+                                          and_return(checking_account_structure_object)
 
-    null_repository.should_receive(:create!).with("checking_account_structure_information_id" => 1,
-                                                  "checking_account_of_fiscal_account_id" => 2,
-                                                  "name" => "FonteRecursos", "description" => nil,
-                                                  "tag" => "FonteRecursos", "fill" => nil, "reference" => nil)
+    null_repository.should_receive(:new).with({ "checking_account_structure_information_id" => 1,
+                                                 "checking_account_of_fiscal_account_id" => 1,
+                                                 "name" => "CodigoAplicacao", "description" => nil,
+                                                 "tag" => "CodigoAplicacao", "fill" => nil, "reference" => nil }).
+                                          and_return(checking_account_structure_object)
 
-    structure_information_repository.should_receive(:find_by_name).with("CÓDIGO DE APLICAÇÃO").and_return(structure_information)
+    null_repository.should_receive(:new).with({ "checking_account_structure_information_id" => 1,
+                                                 "checking_account_of_fiscal_account_id" => 1,
+                                                 "name" => "FonteRecursos", "description" => nil,
+                                                 "tag" => "FonteRecursos", "fill" => nil, "reference" => nil }).
+                                          and_return(checking_account_structure_object)
 
-    null_repository.should_receive(:create!).with("checking_account_structure_information_id" => 1,
-                                                  "checking_account_of_fiscal_account_id" => 2,
-                                                  "name" => "CodigoAplicacao", "description" => nil,
-                                                  "tag" => "CodigoAplicacao", "fill" => nil, "reference" => nil)
+    null_repository.should_receive(:new).any_number_of_times.and_return(checking_account_structure_object)
 
-    structure_information_repository.should_receive(:find_by_name).with(nil)
-
-    expect { subject.import! }.to raise_error "O checking_account_structure_information <vazio> não existe na base de dados"
+    subject.import!
   end
 end
