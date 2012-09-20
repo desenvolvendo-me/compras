a333e5d4669e93de2b9cc52f49a35990d8114abc
Tiago Andre Geraldi <virgulla@gmail.com>
2012-09-20 16:52:04 -0300

    Agreement Additive: should validate presence of value when kind is value
    [Finishes #36193835]

--- app/models/agreement_additive.rb
+++ app/models/agreement_additive.rb
@@ -1,12 +1,13 @@
 class AgreementAdditive < Compras::Model
   attr_accessible :kind, :regulatory_act_id, :value, :description, :number
 
-  has_enumeration_for :kind, :with => AgreementAdditiveKind
+  has_enumeration_for :kind, :with => AgreementAdditiveKind, :create_helpers => { :prefix => true }
 
   delegate :year, :to => :agreement, :allow_nil => true
 
   belongs_to :agreement
   belongs_to :regulatory_act
 
-  validates :description, :kind, :regulatory_act, :value, :presence => true
+  validates :description, :kind, :regulatory_act, :presence => true
+  validates :value, :presence => true, :if => :kind_value?
 end
--- spec/models/agreement_additive_spec.rb
+++ spec/models/agreement_additive_spec.rb
@@ -6,7 +6,6 @@ describe AgreementAdditive do
   it { should validate_presence_of :description }
   it { should validate_presence_of :kind }
   it { should validate_presence_of :regulatory_act }
-  it { should validate_presence_of :value }
 
   it { should belong_to(:agreement) }
   it { should belong_to(:regulatory_act) }
@@ -22,6 +21,20 @@ describe AgreementAdditive do
 
     it 'should delegate year to agreement' do
       expect(subject.year).to eq '2009'
+    end
+  end
+
+  describe 'validating value' do
+    it 'should be required when the kind is value' do
+      subject.stub(:kind_value?).and_return true
+
+      expect(subject).to validate_presence_of :value
+    end
+
+    it 'should not be required when the kind is not value' do
+      subject.stub(:kind_value?).and_return false
+
+      expect(subject).not_to validate_presence_of :value
     end
   end
 end
