8ebde0334e5fd30fda83fd6aeaabcc2526bbb45d
Dmitry Nix <dmitry.nix@gmail.com>
2012-09-25 08:42:07 -0300

    Remove commented code from agreement bank account decorator

--- app/decorators/agreement_bank_account_decorator.rb
+++ app/decorators/agreement_bank_account_decorator.rb
@@ -4,13 +4,6 @@ class AgreementBankAccountDecorator
   include ActionView::Helpers::TranslationHelper
 
   def creation_date(date_repository = ::Date)
-    #if component.creation_date?
-      #current_date_or_creation_date = component.creation_date
-    #else
-      #current_date_or_creation_date = date_repository.current
-    #end
-
-    #localize current_date_or_creation_date
     localize component.creation_date || date_repository.current
   end
 end
