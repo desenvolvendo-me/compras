fd51143a803803a00b37ca43ac9f2b3f66c04c0d
Dmitry Nix <dmitry.nix@gmail.com>
2012-09-26 08:25:20 -0300

    Add missing agreement_file has_many spec to agreement

--- spec/models/agreement_spec.rb
+++ spec/models/agreement_spec.rb
@@ -7,6 +7,7 @@ require 'app/models/agreement'
 require 'app/models/bank_account_capability'
 require 'app/models/bank_account'
 require 'app/models/agreement_additive'
+require 'app/models/agreement_file'
 require 'app/models/tce_capability_agreement'
 
 describe Agreement do
@@ -23,6 +24,7 @@ describe Agreement do
   it { should have_many(:tce_capability_agreements).dependent(:restrict) }
   it { should have_many(:agreement_occurrences).dependent(:destroy) }
   it { should have_many(:agreement_additives).dependent(:destroy).order(:number) }
+  it { should have_many(:agreement_files).dependent(:destroy) }
   it { should have_many(:agreement_participants).dependent(:destroy).order(:id) }
 
   it { should validate_presence_of :number_year }
