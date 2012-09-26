956178d2d9a7fffe0a50e548caef97826e7d6947
Dmitry Nix <dmitry.nix@gmail.com>
2012-09-26 15:18:26 -0300

    Fix order of agreement files.
    [Finishes #36551049]

--- spec/models/agreement_spec.rb
+++ spec/models/agreement_spec.rb
@@ -24,7 +24,7 @@ describe Agreement do
   it { should have_many(:tce_capability_agreements).dependent(:restrict) }
   it { should have_many(:agreement_occurrences).dependent(:destroy) }
   it { should have_many(:agreement_additives).dependent(:destroy).order(:number) }
-  it { should have_many(:agreement_files).dependent(:destroy) }
+  it { should have_many(:agreement_files).dependent(:destroy).order(:id) }
   it { should have_many(:agreement_participants).dependent(:destroy).order(:id) }
 
   it { should validate_presence_of :number_year }
