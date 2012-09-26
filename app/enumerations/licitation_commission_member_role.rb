0cb8c4dbd44ebda7aa6309e66117f73ebd2fdb94
Andre Bernardes <abernardes@gmail.com>
2012-09-26 15:20:30 -0300

    Added support team role to commission member roles.
    [Finishes #36703243]

--- app/enumerations/licitation_commission_member_role.rb
+++ app/enumerations/licitation_commission_member_role.rb
@@ -1,4 +1,4 @@
 class LicitationCommissionMemberRole < EnumerateIt::Base
   associate_values :auctioneer, :substitute_auctioneer, :support, :president, :vice_president, :secretary,
-                   :vice_secretary, :member, :alternate
+                   :vice_secretary, :member, :alternate, :support_team
 end
