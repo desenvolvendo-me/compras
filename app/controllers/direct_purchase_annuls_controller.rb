8ff2ca9e133ff78ba3c39da2cd6994580aa32c73
Mateus Lorandi dos Santos <mcomogo@gmail.com>
2012-10-09 15:57:51 -0300

    DirectPurchaseAnnuls: Fix save resource_annul before proceed with annulment process
    Prevent send authorization email instead annulment email

--- app/controllers/direct_purchase_annuls_controller.rb
+++ app/controllers/direct_purchase_annuls_controller.rb
@@ -1,11 +1,19 @@
 class DirectPurchaseAnnulsController < ResourceAnnulsController
   protected
 
+  def create_resource(object)
+    validate_parent!(object)
+
+    object.transaction do
+      object.save
+
+      annul(object)
+    end
+  end
+
   def annul(object)
     direct_purchase = object.annullable
 
-    object.transaction do
-      DirectPurchaseAnnulment.new(direct_purchase, object, self).annul
-    end
+    DirectPurchaseAnnulment.new(direct_purchase, object, self).annul
   end
 end
--- spec/controllers/direct_purchase_annuls_controller_spec.rb
+++ spec/controllers/direct_purchase_annuls_controller_spec.rb
@@ -16,5 +16,21 @@ describe DirectPurchaseAnnulsController do
 
       post :create, :annullable => annullable
     end
+
+    it 'should send the annulment email' do
+      Prefecture.make!(:belo_horizonte)
+      employee = Employee.make!(:sobrinho)
+      supply_authorization = SupplyAuthorization.make!(:compra_2012)
+      direct_purchase = supply_authorization.direct_purchase
+
+      post :create, :direct_purchase_annul => {
+        :annullable_id => direct_purchase.id,
+        :annullable_type => 'DirectPurchase',
+        :date => '09/10/2012',
+        :employee_id => employee.id
+      }
+
+      expect(ActionMailer::Base.deliveries.first.subject).to eq 'Anulação da autorização de fornecimento'
+    end
   end
 end
