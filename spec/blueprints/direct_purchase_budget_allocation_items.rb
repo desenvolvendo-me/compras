39de3a42a9a486c9aeb5a64cafe46cca215fe479
Mateus Lorandi dos Santos <mcomogo@gmail.com>
2012-10-11 11:04:06 -0300

    Remove duplicated direct_purchase_budget_allocation_items blueprints
    [Finishes #37531467]

--- spec/blueprints/direct_purchase_budget_allocation_items.rb
+++ spec/blueprints/direct_purchase_budget_allocation_items.rb
@@ -5,20 +5,6 @@ DirectPurchaseBudgetAllocationItem.blueprint(:compra_direta_item) do
   unit_price { 200.0 }
 end
 
-DirectPurchaseBudgetAllocationItem.blueprint(:compra_direta_item_extra) do
-  material { Material.make!(:antivirus) }
-  brand { "Norton" }
-  quantity { 3 }
-  unit_price { 200.0 }
-end
-
-DirectPurchaseBudgetAllocationItem.blueprint(:compra_direta_item_engenharia) do
-  material { Material.make!(:antivirus) }
-  brand { "Norton" }
-  quantity { 3 }
-  unit_price { 200.0 }
-end
-
 DirectPurchaseBudgetAllocationItem.blueprint(:kaspersky) do
   material { Material.make!(:antivirus) }
   brand { 'Kaspersky' }
--- spec/blueprints/direct_purchase_budget_allocations.rb
+++ spec/blueprints/direct_purchase_budget_allocations.rb
@@ -5,12 +5,12 @@ end
 
 DirectPurchaseBudgetAllocation.blueprint(:alocacao_compra_extra) do
   budget_allocation { BudgetAllocation.make!(:alocacao) }
-  items { [DirectPurchaseBudgetAllocationItem.make!(:compra_direta_item_extra)] }
+  items { [DirectPurchaseBudgetAllocationItem.make!(:compra_direta_item)] }
 end
 
 DirectPurchaseBudgetAllocation.blueprint(:alocacao_compra_engenharia) do
   budget_allocation { BudgetAllocation.make!(:alocacao) }
-  items { [DirectPurchaseBudgetAllocationItem.make!(:compra_direta_item_engenharia)] }
+  items { [DirectPurchaseBudgetAllocationItem.make!(:compra_direta_item)] }
 end
 
 DirectPurchaseBudgetAllocation.blueprint(:valores_proximo_ao_limite) do
