902c19fd106b4d1c085e04aa01cf55810c2c4d4a
Mateus Lorandi dos Santos <mcomogo@gmail.com>
2012-10-17 11:16:32 -0300

    DirectPurchaseBudgetAllocationCleaner: remove private method

--- app/business/direct_purchase_budget_allocation_cleaner.rb
+++ app/business/direct_purchase_budget_allocation_cleaner.rb
@@ -1,14 +1,8 @@
 class DirectPurchaseBudgetAllocationCleaner
   def self.clear_old_records(direct_purchase, purchase_solicitation, item_group)
-    return unless changed?(direct_purchase, purchase_solicitation, item_group)
+    return unless direct_purchase.purchase_solicitation != purchase_solicitation ||
+                  direct_purchase.purchase_solicitation_item_group != item_group
 
     direct_purchase.direct_purchase_budget_allocations.each(&:mark_for_destruction)
-  end
-
-  private
-
-  def self.changed?(direct_purchase, purchase_solicitation, item_group)
-    direct_purchase.purchase_solicitation != purchase_solicitation ||
-    direct_purchase.purchase_solicitation_item_group != item_group
   end
 end
