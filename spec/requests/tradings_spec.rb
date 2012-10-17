62c7fb340baf1aab423df019014163c022719099
Andre Bernardes <abernardes@gmail.com>
2012-10-17 11:28:21 -0300

    Fixed link in Tradings#index action, it should say "Criar Pregão Presencial".

--- spec/requests/tradings_spec.rb
+++ spec/requests/tradings_spec.rb
@@ -16,7 +16,7 @@ feature "Tradings" do
 
     navigate "Pregão > Pregões Presenciais"
 
-    click_link "Pregão Presencial"
+    click_link "Criar Pregão Presencial"
     fill_in "Ano", :with => "2012"
     fill_modal "Órgão/Entidade", :with => "Detran"
     fill_modal "Unidade licitante", :with => "Secretaria de Educação"
