d064ab31741e91372d52ad21d0cd131095163cb5
Mateus Lorandi dos Santos <mcomogo@gmail.com>
2012-10-26 15:54:37 -0200

    Change modal_info_link.js to use "on" instead "live"

--- app/assets/javascripts/modal_info_link.js
+++ app/assets/javascripts/modal_info_link.js
@@ -1,11 +1,11 @@
 jQuery(document).ready(function($){
-  $("input.modal").live('change', function (event, record) {
+  $("input.modal").on('change', function (event, record) {
     if (record) {
       $(this).parent().find(".modal_info a").attr("href", record.modal_info_url);
     }
   });
 
-  $(".modal_info a").live('click', function(event){
+  $(".modal_info a").on('click', function(event){
     event.preventDefault();
     var url = this.href;
     $.getScript(url);
