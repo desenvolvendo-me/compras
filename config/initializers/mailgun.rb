9d14590e5d27d8d0ebcf808271b5f063de5fc51d
Mateus Lorandi dos Santos <mcomogo@gmail.com>
2012-10-29 16:39:28 -0200

    Put all mailgun configuration inside if to prevent config overwriting on initializers

--- config/initializers/mailgun.rb
+++ config/initializers/mailgun.rb
@@ -1,12 +1,12 @@
-ActionMailer::Base.smtp_settings = {
-  :port           => ENV['MAILGUN_SMTP_PORT'],
-  :address        => ENV['MAILGUN_SMTP_SERVER'],
-  :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
-  :password       => ENV['MAILGUN_SMTP_PASSWORD'],
-  :domain         => ENV['MAILGUN_SMTP_DOMAIN'],
-  :authentication => :plain,
-}
-
 if Rails.env.production? || Rails.env.staging?
+  ActionMailer::Base.smtp_settings = {
+    :port           => ENV['MAILGUN_SMTP_PORT'],
+    :address        => ENV['MAILGUN_SMTP_SERVER'],
+    :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
+    :password       => ENV['MAILGUN_SMTP_PASSWORD'],
+    :domain         => ENV['MAILGUN_SMTP_DOMAIN'],
+    :authentication => :plain,
+  }
+
   ActionMailer::Base.delivery_method = :smtp
 end
