970de65de06392c36b582dabe163b5291f949d37
Andre Bernardes <abernardes@gmail.com>
2012-10-30 14:43:10 -0200

    Changed precision of TradingItem#minimum_reduction_percent to 5 with a scale of 2 since it's a percentage.
    [Finishes #38574349]

---
+++ db/migrate/20121030132855_change_precision_of_percent_trading_items.rb
@@ -0,0 +1,8 @@
+class ChangePrecisionOfPercentTradingItems < ActiveRecord::Migration
+  def change
+    remove_column :compras_trading_items, :minimum_reduction_percent 
+
+    add_column :compras_trading_items, :minimum_reduction_percent, :decimal,
+               :precision => 5, :scale => 2, :default => 0.0
+  end
+end
