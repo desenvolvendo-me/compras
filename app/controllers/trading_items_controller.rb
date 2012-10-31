907e1736da12030e76f615f98526382f0a03acf4
Andre Bernardes <abernardes@gmail.com>
2012-10-31 14:30:56 -0200

    Replace Inherited Resources belongs to method in Trading Items Controller by a definition of a begin_of_association_chain.
    This keeps our dependency on Inherited Resources at
    a lower level.

--- app/controllers/trading_items_controller.rb
+++ app/controllers/trading_items_controller.rb
@@ -1,3 +1,7 @@
 class TradingItemsController < CrudController
-  nested_belongs_to :trading
+  def begin_of_association_chain
+    if params[:trading_id]
+      @parent = Trading.find(params[:trading_id])
+    end
+  end
 end
