34cbc0e7fdb7730433cdcdd13021f60cac3619a8
Mateus Lorandi dos Santos <mcomogo@gmail.com>
2012-11-06 08:33:15 -0200

    Change add_status_to_trading_item_bids migration
    Use raw SQL to update status to default value because execute only one hit to the database.

--- db/migrate/20121105162156_add_status_to_compras_trading_item_bids.rb
+++ db/migrate/20121105162156_add_status_to_compras_trading_item_bids.rb
@@ -2,8 +2,6 @@ class AddStatusToComprasTradingItemBids < ActiveRecord::Migration
   def change
     add_column :compras_trading_item_bids, :status, :string
 
-    TradingItemBid.find_each do |item|
-      item.update_status(TradingItemBidStatus::WITH_PROPOSAL)
-    end
+    execute "UPDATE compras_trading_item_bids SET status = 'with_proposal'"
   end
 end
