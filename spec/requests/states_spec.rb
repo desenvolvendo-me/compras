9f13fad15b0669517cbbdcf6af696aba0ba37553
José Gomes Júnior <zegomesjf@gmail.com>
2012-11-15 10:19:38 -0200

    Move states item on menu.
    the idea is to improve the menu, and leave the menu more like the accounting system menu to facilitate use by the user.

--- spec/requests/states_spec.rb
+++ spec/requests/states_spec.rb
@@ -9,7 +9,7 @@ feature "States" do
   scenario 'create a new state' do
     Country.make!(:brasil)
 
-    navigate 'Cadastros Gerais > Endereços > Estados'
+    navigate 'Geral > Parâmetros > Endereços > Estados'
 
     click_link 'Criar Estado'
 
@@ -31,7 +31,7 @@ feature "States" do
   scenario 'update a state' do
     rs = State.make!(:rs)
 
-    navigate 'Cadastros Gerais > Endereços > Estados'
+    navigate 'Geral > Parâmetros > Endereços > Estados'
 
     click_link 'Rio Grande do Sul'
 
@@ -49,7 +49,7 @@ feature "States" do
   scenario 'destroy a state' do
     rs = State.make!(:rs)
 
-    navigate 'Cadastros Gerais > Endereços > Estados'
+    navigate 'Geral > Parâmetros > Endereços > Estados'
 
     click_link 'Rio Grande do Sul'
 
