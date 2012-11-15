709f0a5ce18f9f867a72aac23e5426a898650af0
José Gomes Júnior <zegomesjf@gmail.com>
2012-11-15 10:32:11 -0200

    Move condominia item on menu.
    the idea is to improve the menu, and leave the menu more like the accounting system menu to facilitate use by the user.

--- spec/requests/condominia_spec.rb
+++ spec/requests/condominia_spec.rb
@@ -10,7 +10,7 @@ feature "Condominia" do
     Neighborhood.make!(:sao_francisco)
     Street.make!(:girassol)
 
-    navigate 'Cadastros Gerais > Endereços > Condomínios'
+    navigate 'Geral > Parâmetros > Endereços > Condomínios'
 
     click_link 'Criar Condomínio'
 
@@ -30,7 +30,7 @@ feature "Condominia" do
   scenario 'update a condominium' do
     Condominium.make!(:tambuata)
 
-    navigate 'Cadastros Gerais > Endereços > Condomínios'
+    navigate 'Geral > Parâmetros > Endereços > Condomínios'
 
     click_link 'Tambuata'
 
@@ -48,7 +48,7 @@ feature "Condominia" do
   scenario 'destroy a condominium' do
     Condominium.make!(:tambuata)
 
-    navigate 'Cadastros Gerais > Endereços > Condomínios'
+    navigate 'Geral > Parâmetros > Endereços > Condomínios'
 
     click_link 'Tambuata'
 
