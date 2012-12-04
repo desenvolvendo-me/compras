c00aac1a9f0c6a989cb2c6fb38b75e1226243634
Mateus Lorandi dos Santos <mcomogo@gmail.com>
2012-12-04 17:45:57 -0200

    Refactor ResourceAnnulsController spec
    I removed pending statement from the tests and I
    made them to  work as expected =D

--- spec/controllers/resource_annuls_controller_spec.rb
+++ spec/controllers/resource_annuls_controller_spec.rb
@@ -1,9 +1,24 @@
 # encoding: utf-8
 require 'spec_helper'
 
-describe ResourceAnnulsController do
+class TestAnnulsController < ResourceAnnulsController
+end
+
+describe TestAnnulsController do
+  let(:resource_annul) do
+    double(:resource_annul, :date= => true, :employee= => true)
+  end
+
   before do
     sign_in User.make!(:sobrinho_as_admin_and_employee)
+
+    Rails.application.routes.draw do
+      resources :test_annuls
+    end
+  end
+
+  after do
+    Rails.application.reload_routes!
   end
 
   context "with annullable" do
@@ -27,34 +42,62 @@ describe ResourceAnnulsController do
 
   describe 'GET #new' do
     it "should return 401 if parent is already annulled" do
-      pending 'Waiting solution for rails update' do
-        resource_annul = ResourceAnnul.make!(:rescisao_de_contrato_anulada)
+      subject.stub(:build_resource).and_return(resource_annul)
 
-        subject.stub(:controller_name => 'contract_termination_annuls')
-        subject.should_receive(:validate_parent!).
-          with(instance_of(ResourceAnnul)).
-          and_raise(Exceptions::Unauthorized)
+      resource_annul.should_receive(:annulled?).and_return(true)
 
-        get :new, :annullable_id => resource_annul.annullable_id
+      get :new, :annullable_id => 1
 
-        expect(response.code).to eq '401'
-      end
+      expect(response.code).to eq '401'
+    end
+
+    it "should not return 401 if parent is not annulled" do
+      subject.stub(:build_resource).and_return(resource_annul)
+
+      resource_annul.should_receive(:annulled?).and_return(false)
+
+      get :new, :annullable_id => 1
+
+      expect(response.code).to eq '200'
     end
   end
 
   describe 'POST #create' do
     it "should return 401 if parent is already annulled" do
-      pending 'Waiting solution for rails update' do
-        resource_annul = ResourceAnnul.make!(:rescisao_de_contrato_anulada)
+      subject.stub(:build_resource).and_return(resource_annul)
 
-        subject.should_receive(:validate_parent!).
-          with(instance_of(ResourceAnnul)).
-          and_raise(Exceptions::Unauthorized)
+      resource_annul.should_receive(:annulled?).and_return(true)
 
-        post :create, :resource_annul => resource_annul.attributes
+      post :create
 
-        expect(response.code).to eq '401'
-      end
+      expect(response.code).to eq '401'
+    end
+
+    it "should not return 401 if parent is not annulled" do
+      subject.stub(:build_resource).and_return(resource_annul)
+      subject.stub(:edit_parent_path).and_return('/test/edit/1')
+      resource_annul.stub(:annullable).and_return('Test')
+
+      resource_annul.should_receive(:annulled?).and_return(false)
+      resource_annul.should_receive(:save).and_return(true)
+      resource_annul.should_receive(:transaction).and_yield
+      ObjectAnnulment.should_receive(:annul!).with('Test')
+
+      post :create
+
+      expect(response).to redirect_to('/test/edit/1')
+    end
+  end
+
+  describe 'PUT #update' do
+    it 'should not have update action' do
+      expect { put :update }.to raise_exception ActionController::RoutingError
+    end
+  end
+
+  describe 'DELETE #destroy' do
+    it 'should not have index action' do
+      expect { delete :destroy }.to raise_exception ActionController::RoutingError
     end
   end
 end
