89b2dc08f66e95728ddfc03a56e556b81671c6d0
Joao Mello <joao.mello@dtmtec.com.br>
2012-12-18 10:16:16 -0200

    Authorize nested cruds for Licitation Process Publications.
    - Set authorization to the nested controller as 
    parent controller.
        
    [#39495983]

--- app/controllers/licitation_process_publications_controller.rb
+++ app/controllers/licitation_process_publications_controller.rb
@@ -23,4 +23,10 @@ class LicitationProcessPublicationsController < CrudController
       @parent = LicitationProcess.find(params[:licitation_process_id])
     end
   end
+
+  protected
+
+  def main_controller_name
+    'administrative_processes'
+  end
 end
