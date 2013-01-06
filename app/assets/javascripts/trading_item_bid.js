0fa2ce26d03e40d962ae70477e4b62ffe103d87a
José Gomes Júnior <zegomesjf@gmail.com>
2013-01-06 15:05:01 -0200

    Refactor confirmation message on trading item bid

---
+++ app/assets/javascripts/trading_item_bid.js
@@ -0,0 +1,31 @@
+function confirmationMessage(){
+  var statusChecked = $('input[name="trading_item_bid[status]"]:checked').val();
+  var bidderName = $('#trading_item_bid_bidder').val();
+  var bidAmount = $('#trading_item_bid_amount').val();
+
+  switch(statusChecked)
+  {
+    case "declined":
+      return 'Confirma que o licitante ' + bidderName +  ' declinou ?';
+    case "with_proposal":
+      return 'Confirma a ' + stageName() + ' no valor de R$ '+ bidAmount + ' para o licitante ' + bidderName + '?';
+    case "disqualified":
+      return 'Confirma que o licitante ' + bidderName +  ' foi desclassificado ?';
+    case "without_proposal":
+      return 'Confirma que o licitante ' + bidderName +  ' não tem proposta para o item ?';
+  }
+}
+
+function stageName(){
+   var stage = $('#trading_item_bid_stage').val();
+
+  switch(stage)
+  {
+    case "Rodada de lances":
+      return 'oferta';
+    case "Propostas":
+      return 'proposta';
+    case "Negociação":
+      return 'negociação';
+  }
+}
