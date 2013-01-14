0dcbdfa7be5faa8481951530a55559a9a23510bb
Mateus Lorandi dos Santos <mcomogo@gmail.com>
2013-01-14 16:31:41 -0200

    Fix migration, only close when have a closing_date

--- db/migrate/20130114160235_remove_closing_date_from_compras_trading_items.rb
+++ db/migrate/20130114160235_remove_closing_date_from_compras_trading_items.rb
@@ -1,9 +1,11 @@
 class RemoveClosingDateFromComprasTradingItems < ActiveRecord::Migration
   def change
     TradingItem.find_each do |item|
-      TradingItemClosing.create!(
-        :trading_item_id => item.id,
-        :status => TradingItemClosingStatus::WINNER)
+      if item.closing_date
+        TradingItemClosing.create!(
+          :trading_item_id => item.id,
+          :status => TradingItemClosingStatus::WINNER)
+      end
     end
 
     remove_column :compras_trading_items, :closing_date
