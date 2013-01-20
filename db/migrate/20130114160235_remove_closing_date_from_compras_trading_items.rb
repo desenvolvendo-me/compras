200ae0b09082a1fd510a49afa590691e345598a1
Mateus Lorandi dos Santos <mcomogo@gmail.com>
2013-01-20 11:25:13 -0200

    Add model to migration

--- db/migrate/20130114160235_remove_closing_date_from_compras_trading_items.rb
+++ db/migrate/20130114160235_remove_closing_date_from_compras_trading_items.rb
@@ -1,4 +1,7 @@
 class RemoveClosingDateFromComprasTradingItems < ActiveRecord::Migration
+  class TradingItem < Compras::Model;end
+  class TradingItemClosing < Compras::Model;end
+
   def change
     TradingItem.find_each do |item|
       if item.closing_date
