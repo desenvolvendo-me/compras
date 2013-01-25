da444d245656494a818c32fd2e09cbd8cfaa00d4
Mateus Lorandi dos Santos <mcomogo@gmail.com>
2013-01-25 08:29:59 -0200

    Remove needless variable user
    Use creditor instead

--- app/mailers/price_collection_mailer.rb
+++ app/mailers/price_collection_mailer.rb
@@ -11,12 +11,11 @@ class PriceCollectionMailer < ActionMailer::Base
   end
 
   def invite_registered_creditor(creditor, price_collection, current_prefecture, customer)
-    @user = creditor.user
     @creditor = creditor
     @price_collection = price_collection
     @prefecture = current_prefecture
     @customer = customer
 
-    mail :to => @user.email, :subject => "Convite para o Registro de Preço #{ @price_collection }"
+    mail :to => @creditor.email, :subject => "Convite para o Registro de Preço #{ @price_collection }"
   end
 end
