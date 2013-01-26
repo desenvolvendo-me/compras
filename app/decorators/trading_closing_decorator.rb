b97b66d7a5900f72ea4828f49e8f8b791f5eba0c
Mateus Lorandi dos Santos <mcomogo@gmail.com>
2013-01-26 14:05:51 -0200

    Remove unused attr_header at TradingClosing

--- app/decorators/trading_closing_decorator.rb
+++ app/decorators/trading_closing_decorator.rb
@@ -1,10 +1,7 @@
 class TradingClosingDecorator
   include Decore
   include Decore::Proxy
-  include Decore::Header
   include ActionView::Helpers::TranslationHelper
-
-  attr_header :created_at_date, :status, :observation
 
   def created_at_date
     I18n.l created_at.to_date
