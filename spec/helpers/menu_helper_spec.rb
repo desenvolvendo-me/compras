650757d9c3d17c499022bf00d897f74cb169dcc5
Mateus Lorandi dos Santos <mcomogo@gmail.com>
2013-02-12 08:52:20 -0200

    Add cache to menu
    [Finishes #44110069]

--- spec/helpers/menu_helper_spec.rb
+++ spec/helpers/menu_helper_spec.rb
@@ -2,8 +2,13 @@ require 'spec_helper'
 
 describe MenuHelper do
   describe "#render_menu" do
+    before do
+      helper.stub_chain(:current_user, :administrator?).and_return(false)
+    end
+
     it "should render the menu" do
       helper.should_receive(:can?).any_number_of_times.with(any_args).and_return(true)
+
       helper.should_receive(:item1_path).and_return('/item_um')
       helper.should_receive(:item2_path).and_return('/item_dois')
 
@@ -14,11 +19,11 @@ describe MenuHelper do
 
       expect(helper.render_menu(:path => 'spec/fixtures/menu.yml')).to eq '<li><a href="#">Menu Principal</a><ul><li><a href="#">Submenu</a><ul><li><a href="/item_um">Item 1</a></li><li><a href="/item_dois">Item 2</a></li></ul></li></ul></li>'
     end
-  end
 
-  it 'should not render the menu' do
-    helper.should_receive(:can?).any_number_of_times.with(any_args).and_return(false)
+    it 'should not render the menu' do
+      helper.should_receive(:can?).any_number_of_times.with(any_args).and_return(false)
 
-    expect(helper.render_menu(:path => 'spec/fixtures/menu.yml')).to eq ''
+      expect(helper.render_menu(:path => 'spec/fixtures/menu.yml')).to eq ''
+    end
   end
 end
