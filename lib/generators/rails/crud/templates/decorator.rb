cfdfa37f6f77093e916df2a1636bf8f06cfb2933
José Gomes Júnior <zegomesjf@gmail.com>
2013-02-26 07:26:49 -0300

    Refactor decorator template.

--- lib/generators/rails/crud/templates/decorator.rb
+++ lib/generators/rails/crud/templates/decorator.rb
@@ -1,7 +1,9 @@
 class <%= class_name %>Decorator
   include Decore
   include Decore::Proxy
-  <%= "include Decore::Header" if index_fields.any? %>
+<% if index_fields.any? -%>
+  include Decore::Header
 
   attr_header <%= index_fields.map { |field| ":#{field}" }.join(", ") %>
+<% end -%>
 end
