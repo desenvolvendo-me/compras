e1a4288fc11822631bba3194b64b711915e76b77
José Gomes Júnior <zegomesjf@gmail.com>
2013-02-26 18:11:45 -0300

    Corrige teste de integração na compra direta
    devido a alteração no orderize no model

--- spec/integrations/direct_purchase_spec.rb
+++ spec/integrations/direct_purchase_spec.rb
@@ -2,12 +2,12 @@ require 'spec_helper'
 
 describe DirectPurchase do
   describe ".ordered" do
-    it "should order by year and budget_structure" do
+    it "should order by year DESC and budget_structure DESC" do
       direct_purchase_2012_1 = DirectPurchase.make!(:compra)
       direct_purchase_2012_2 = DirectPurchase.make!(:compra_nao_autorizada)
       direct_purchase_2011 = DirectPurchase.make!(:compra_2011)
 
-      expect(DirectPurchase.ordered.to_a).to eq [direct_purchase_2011, direct_purchase_2012_1, direct_purchase_2012_2]
+      expect(DirectPurchase.ordered.to_a).to eq [direct_purchase_2012_2, direct_purchase_2012_1, direct_purchase_2011]
     end
   end
 
