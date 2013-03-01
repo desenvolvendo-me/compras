3b102c42f12560bd304c25cfafb0956f3ffc58ae
Mateus Lorandi dos Santos <mcomogo@gmail.com>
2013-03-01 08:45:46 -0300

    Utiliza raw sql to migrate data
    Previne quebra de migration.
    
    Parte da task [#45016315]

--- db/migrate/20130117184937_add_delivery_location_id_to_compras_licitation_processes.rb
+++ db/migrate/20130117184937_add_delivery_location_id_to_compras_licitation_processes.rb
@@ -5,8 +5,13 @@ class AddDeliveryLocationIdToComprasLicitationProcesses < ActiveRecord::Migratio
     add_index :compras_licitation_processes, :delivery_location_id
     add_foreign_key :compras_licitation_processes, :compras_delivery_locations, :column => :delivery_location_id
 
-    LicitationProcess.joins { purchase_solicitation }.find_each do |licitation_process|
-      licitation_process.update_column(:delivery_location_id, licitation_process.purchase_solicitation.delivery_location_id)
-    end
+    execute <<-SQL
+      UPDATE compras_licitation_processes a
+        SET delivery_location_id = b.delivery_location_id
+      FROM compras_purchase_solicitations b
+        JOIN compras_administrative_processes c
+        ON c.purchase_solicitation_id = b.id
+      WHERE a.administrative_process_id = c.id
+    SQL
   end
 end
