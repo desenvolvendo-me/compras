db2ff2043dee8daff97631d58c58809ef47108e7
José Gomes Júnior <zegomesjf@gmail.com>
2013-03-06 08:12:28 -0300

    Troca ServiceOrContractype por ContractType em um teste que faltou.
    Devido a mudança em 75ed2d7b4ccaff7f60ed95e882b19bbb19b4f8dd

--- spec/integrations/contract_type_spec.rb
+++ spec/integrations/contract_type_spec.rb
@@ -1,9 +1,9 @@
 # encoding: UTF-8
 require 'spec_helper'
 
-describe ServiceOrContractType do
+describe ContractType do
   context 'uniqueness validation' do
-    before { ServiceOrContractType.make!(:trainees) }
+    before { ContractType.make!(:trainees) }
 
     it { should validate_uniqueness_of(:description) }
   end
