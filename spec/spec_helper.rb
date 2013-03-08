46cc51723ea89ab57b230bbbd254000f276d9fc3
José Gomes Júnior <zegomesjf@gmail.com>
2013-03-08 16:07:45 -0300

    Adiciona condição para gerar o log de cobertura do simple cov.
    agora para gerar o log deverá ser executado.
    SIMPLECOV=true rspec spec/models
    
    Isso faz com que aumente a velocidade dos testes.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,6 @@
 require 'simplecov'
-SimpleCov.start
+SimpleCov.start if ENV["SIMPLECOV"]
+
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../../config/environment", __FILE__)
--- spec/unit_helper.rb
+++ spec/unit_helper.rb
@@ -1,6 +1,6 @@
 require 'rubygems'
 require 'bundler/setup'
 require 'simplecov'
-SimpleCov.start
+SimpleCov.start if ENV["SIMPLECOV"]
 
 $:.unshift File.expand_path('../../', __FILE__)
