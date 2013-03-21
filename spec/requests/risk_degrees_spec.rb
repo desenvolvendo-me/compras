b24ffaddfb2bd5b58f511547624fe20dd5607eed
Denys dos Santos <denysnando@gmail.com>
2013-03-21 10:56:52 -0300

    Unifica testes de risk_degrees_spec
    Foi unificado os cenários de create, update e destroy. 
    
    [#46133687]

--- spec/requests/risk_degrees_spec.rb
+++ spec/requests/risk_degrees_spec.rb
@@ -6,7 +6,7 @@ feature "RiskDegrees" do
     sign_in
   end
 
-  scenario 'create a new risk_degree' do
+  scenario 'create a new risk_degree, update and destroy an existing' do
     navigate 'Comum > Pessoas > Auxiliar > Grau de Riscos'
 
     click_link 'Criar Grau de Risco'
@@ -22,17 +22,9 @@ feature "RiskDegrees" do
 
     expect(page).to have_field 'Nível', :with => "1"
     expect(page).to have_field 'Nome', :with => "Médio"
-  end
-
-  scenario 'update an existent risk_degree' do
-    RiskDegree.make!(:leve)
-
-    navigate 'Comum > Pessoas > Auxiliar > Grau de Riscos'
-
-    click_link 'Leve'
-
+    
+    fill_in 'Nível', :with => '2'
     fill_in 'Nome', :with => 'Muito Grave'
-    fill_in 'Nível', :with => '4'
 
     click_button 'Salvar'
 
@@ -40,22 +32,14 @@ feature "RiskDegrees" do
 
     click_link 'Muito Grave'
 
-    expect(page).to have_field 'Nível', :with => "4"
+    expect(page).to have_field 'Nível', :with => "2"
     expect(page).to have_field 'Nome', :with => "Muito Grave"
-  end
-
-  scenario 'destroy an existent risk_degree' do
-    RiskDegree.make!(:grave)
-
-    navigate 'Comum > Pessoas > Auxiliar > Grau de Riscos'
-
-    click_link 'Grave'
 
     click_link 'Apagar'
 
     expect(page).to have_notice 'Grau de Risco apagado com sucesso.'
 
-    expect(page).to_not have_content 'Grave'
+    expect(page).to_not have_content 'Muito Grave'
   end
 
   scenario 'index with columns at the index' do
