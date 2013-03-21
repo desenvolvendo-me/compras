040293f3078741cb7104b3450617fba39a68ee37
Denys dos Santos <denysnando@gmail.com>
2013-03-21 11:04:19 -0300

    Unifica testes de trading_configurations_spec
    Foi unificado os cenários de create, update. 
    
    [#46133687]

--- spec/requests/trading_configurations_spec.rb
+++ spec/requests/trading_configurations_spec.rb
@@ -6,37 +6,25 @@ feature "TradingConfigurations" do
     sign_in
   end
 
-  scenario 'create a new trading_configuration' do
+  scenario 'create a new trading_configuration and update an existent' do
     navigate "Processos de Compra > Auxiliar > Configuração do Pregão Presencial"
 
     fill_in 'Porcentagem limite para participar das ofertas', :with => '10,00'
 
-    click_button 'Salvar'
-
     expect(page).not_to have_button 'Apagar'
+
+    click_button 'Salvar'
 
     expect(page).to have_notice 'Configuração do Registro de Preço criado com sucesso.'
 
-    navigate "Processos de Compra > Auxiliar > Configuração do Pregão Presencial"
-
-    expect(page).to have_field 'Porcentagem limite para participar das ofertas', :with => '10,00'
-  end
-
-  scenario 'update an existent trading_configuration' do
-    TradingConfiguration.make!(:pregao)
-
-    navigate "Processos de Compra > Auxiliar > Configuração do Pregão Presencial"
-
     expect(page).to have_field 'Porcentagem limite para participar das ofertas', :with => '10,00'
 
-    fill_in 'Porcentagem limite para participar das ofertas', :with => '20,00'
+    fill_in 'Porcentagem limite para participar das ofertas', :with => '15,00'
 
     click_button 'Salvar'
 
     expect(page).to have_notice 'Configuração do Registro de Preço editado com sucesso.'
 
-    navigate "Processos de Compra > Auxiliar > Configuração do Pregão Presencial"
-
-    expect(page).to have_field 'Porcentagem limite para participar das ofertas', :with => '20,00'
+    expect(page).to have_field 'Porcentagem limite para participar das ofertas', :with => '15,00'
   end
 end
