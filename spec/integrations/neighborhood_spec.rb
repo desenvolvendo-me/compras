3f1162e55266e8090beb57cc5cb529df6f2ba589
Carlos Silva <cefs1984@gmail.com>
2013-03-22 20:21:19 -0300

    Remove chamada de blueprint inexistente.

--- spec/integrations/neighborhood_spec.rb
+++ spec/integrations/neighborhood_spec.rb
@@ -3,8 +3,6 @@ require 'spec_helper'
 
 describe Neighborhood do
   context 'uniqueness validations' do
-    before { Neighborhood.make!(:portugal) }
-
     it { should validate_uniqueness_of(:name).scoped_to(:district_id, :city_id) }
   end
 end
