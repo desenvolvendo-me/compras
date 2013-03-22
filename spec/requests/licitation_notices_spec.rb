ed5c40bd453794daf7f56b5ca5bf2b611e45ea86
Carlos Silva <cefs1984@gmail.com>
2013-03-22 15:55:51 -0300

    Unifica testes de licitation_notices_spec.
    Foi unificado os cenários de create, update e destroy.
    
    [#46133687]

--- spec/requests/licitation_notices_spec.rb
+++ spec/requests/licitation_notices_spec.rb
@@ -8,7 +8,7 @@ feature "LicitationNotices" do
 
   let(:date_current) { I18n.l Date.current }
 
-  scenario 'create a new licitation_notice' do
+  scenario 'create, update and destroy destroy a new licitation_notice' do
     LicitationProcess.make!(:processo_licitatorio)
 
     navigate 'Processos de Compra > Avisos de Licitações'
@@ -42,38 +42,33 @@ feature "LicitationNotices" do
     expect(page).to have_field 'Data do processo', :with => '19/03/2012'
     expect(page).to have_field 'Objeto da licitação', :with => 'Licitação para compra de carteiras'
     expect(page).to have_field 'Observações gerais', :with => 'Aviso de processo'
-  end
 
-  scenario 'update an existent licitation_notice' do
-    LicitationNotice.make!(:aviso_de_licitacao)
-    LicitationProcess.make!(:processo_licitatorio_computador)
-
-    navigate 'Processos de Compra > Avisos de Licitações'
-
-    within_records do
-      click_link "1/2012 - Convite 1 - #{date_current}"
-    end
-
-    fill_modal 'Processo de compra', :with => '2013', :field => 'Ano'
-    fill_in 'Data do aviso', :with => '12/04/2012'
-    fill_in 'Observações gerais', :with => 'Aviso de processo, continuação.'
+    fill_in 'Data do aviso', :with => '06/04/2012'
+    fill_in 'Observações gerais', :with => 'Novo aviso de processo, continuação.'
 
     click_button 'Salvar'
 
     expect(page).to have_notice 'Aviso de Licitação editado com sucesso.'
 
     within_records do
-      click_link '2/2013 - Convite 1 - 12/04/2012'
+      click_link '1/2012 - Convite 1 - 06/04/2012'
     end
 
-    expect(page).to have_field 'Processo de compra', :with => '2/2013 - Convite 1'
+    expect(page).to have_field 'Processo de compra', :with => '1/2012 - Convite 1'
     expect(page).to have_field 'Número do aviso', :with => '1'
-    expect(page).to have_field 'Data do aviso', :with => '12/04/2012'
+    expect(page).to have_field 'Data do aviso', :with => '06/04/2012'
     expect(page).to have_field 'Modalidade', :with => 'Convite'
     expect(page).to have_field 'Número da licitação', :with => '1'
-    expect(page).to have_field 'Data do processo', :with => '20/03/2013'
+    expect(page).to have_field 'Data do processo', :with => '19/03/2012'
     expect(page).to have_field 'Objeto da licitação', :with => 'Licitação para compra de carteiras'
-    expect(page).to have_field 'Observações gerais', :with => 'Aviso de processo, continuação.'
+    expect(page).to have_field 'Observações gerais', :with => 'Novo aviso de processo, continuação.'
+
+    click_link 'Apagar'
+
+    expect(page).to have_notice 'Aviso de Licitação apagado com sucesso.'
+
+    expect(page).to_not have_content '1/2012 - Convite 1 - 06/04/2012'
+    expect(page).to_not have_content 'A licitação começou.'
   end
 
   scenario 'create a new licitation_notice when already exists a licitation_notice with the same licitation process year' do
@@ -133,23 +128,6 @@ feature "LicitationNotices" do
     expect(page).to have_field 'Data do processo', :with => '19/03/2012'
     expect(page).to have_field 'Objeto da licitação', :with => 'Licitação para compra de carteiras'
     expect(page).to have_field 'Observações gerais', :with => 'Aviso de processo 2'
-  end
-
-  scenario 'destroy an existent licitation_notice' do
-    LicitationNotice.make!(:aviso_de_licitacao)
-
-    navigate 'Processos de Compra > Avisos de Licitações'
-
-    within_records do
-      click_link "1/2012 - Convite 1 - #{date_current}"
-    end
-
-    click_link 'Apagar'
-
-    expect(page).to have_notice 'Aviso de Licitação apagado com sucesso.'
-
-    expect(page).to_not have_content I18n.l(Date.current)
-    expect(page).to_not have_content "A licitação começou."
   end
 
   scenario 'delegate fields should be empty when clear licitaion process' do
