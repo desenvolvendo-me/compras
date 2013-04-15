24e5a404113a344c652614f89d6626ba35862572
Mateus Lorandi dos Santos <mcomogo@gmail.com>
2013-04-15 20:27:22 -0300

    Usa o novo formato do NestedGrid na Solicitação

--- app/decorators/purchase_solicitation_item_decorator.rb
+++ app/decorators/purchase_solicitation_item_decorator.rb
@@ -8,6 +8,14 @@ class PurchaseSolicitationItemDecorator
     number_with_precision super if super
   end
 
+  def quantity
+    number_with_precision super if super
+  end
+
+  def unit_price
+    number_with_precision super if super
+  end
+
   def material_label
     services? ? "Serviço" : "Material"
   end
