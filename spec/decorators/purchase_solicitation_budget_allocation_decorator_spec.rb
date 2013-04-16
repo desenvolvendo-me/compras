40b7f22d28cbcadcb05abdb54533c008954a7545
Mateus Lorandi dos Santos <mcomogo@gmail.com>
2013-04-16 17:51:10 -0300

    Aplica o novo modelo do NestedGrid na Solicitação
    Foi aplicado o novo modelo de NestedGrid nas
    Dotações da Solicitação, assim o edit já está
    incluso.
    
    [Finishes #48175493]

---
+++ spec/decorators/purchase_solicitation_budget_allocation_decorator_spec.rb
@@ -0,0 +1,21 @@
+# encoding: utf-8
+require 'decorator_helper'
+require 'app/decorators/purchase_solicitation_budget_allocation_decorator'
+
+describe PurchaseSolicitationBudgetAllocationDecorator do
+  describe '#estimated_value' do
+    it 'should return the number with precision' do
+      component.stub(:estimated_value => 18.2)
+
+      expect(subject.estimated_value).to eq '18,20'
+    end
+  end
+
+  describe '#budget_allocation_amount' do
+    it 'should return the number with precision' do
+      component.stub(:budget_allocation_amount => 10)
+
+      expect(subject.budget_allocation_amount).to eq '10,00'
+    end
+  end
+end
