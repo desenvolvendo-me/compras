a421c18296f4993545ac6c1b3bccb99f473a8517
Rodrigo Ulisses <rodrigo.ulisses7@gmail.com>
2013-05-09 10:46:18 -0300

    Removendo blueprint e scenario desncessários.

--- spec/blueprints/bank_accounts.rb
+++ spec/blueprints/bank_accounts.rb
@@ -8,14 +8,3 @@ BankAccount.blueprint(:itau_tributos) do
   digit { 2 }
   capabilities { [BankAccountCapability.make!(:reforma)] }
 end
-
-# encoding: utf-8
-BankAccount.blueprint(:santander_tributos) do
-  description { "Santander Tributos" }
-  agency { ::FactoryGirl::Preload.factories['Agency'][:santander] }
-  account_number { "1111" }
-  status { Status::ACTIVE }
-  kind { BankAccountKind::APPLICATION }
-  digit { 2 }
-  capabilities { [BankAccountCapability.make!(:reforma)] }
-end
