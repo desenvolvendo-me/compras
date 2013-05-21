92b3b393a5c49d57cfa83b9bace2ff55389c46c1
Mateus Lorandi dos Santos <mcomogo@gmail.com>
2013-05-21 07:53:59 -0300

    Adiciona virgula que estava faltando no enumeration

--- app/enumerations/monthly_monitoring_files.rb
+++ app/enumerations/monthly_monitoring_files.rb
@@ -1,3 +1,4 @@
 class MonthlyMonitoringFiles < EnumerateIt::Base
-  associate_values :regulatory_act, :process_responsible, :price_registration_accession :legal_analysis_appraisal
+  associate_values :regulatory_act, :process_responsible, :price_registration_accession,
+    :legal_analysis_appraisal
 end
