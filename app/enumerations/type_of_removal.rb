e27925b277173bf20d6302c8b4c1bae9cd691ba6
Denys dos Santos <denysnando@gmail.com>
2013-05-23 21:55:31 -0300

    Inserir campos no enumeration type_of_removal
    foram inseridos os seguintes campos
    Inexigibilidade - Credenciamento
    Dispensa Justificada - Credenciamento
    Chamamento
    
    [Finishes #50470597]

--- app/enumerations/type_of_removal.rb
+++ app/enumerations/type_of_removal.rb
@@ -1,3 +1,9 @@
 class TypeOfRemoval < EnumerateIt::Base
-  associate_values :removal_by_limit, :removal_justified, :removal_by_ineligibility, :other_reasons_to_removal
+  associate_values :removal_by_limit,
+                   :removal_justified,
+                   :removal_by_ineligibility,
+                   :other_reasons_to_removal,
+                   :unenforceability_accreditation,
+                   :dispensation_justified_accreditation,
+                   :call
 end
