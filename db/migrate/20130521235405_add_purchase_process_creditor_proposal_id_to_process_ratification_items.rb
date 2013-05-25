ac986f8ddc1b3d083c48826a16a345f62ece7d78
José Gomes Júnior <zegomesjf@gmail.com>
2013-05-25 09:12:33 -0300

    Corrige migration que adiciona campo em adjudicação.
    Como não tem como buscar os valores do novo campo, não tem como
    colocar um valor default para o mesmo.

--- db/migrate/20130521235405_add_purchase_process_creditor_proposal_id_to_process_ratification_items.rb
+++ db/migrate/20130521235405_add_purchase_process_creditor_proposal_id_to_process_ratification_items.rb
@@ -3,7 +3,7 @@ class AddPurchaseProcessCreditorProposalIdToProcessRatificationItems < ActiveRec
     remove_column :compras_licitation_process_ratification_items, :bidder_proposal_id
 
     add_column :compras_licitation_process_ratification_items, :purchase_process_creditor_proposal_id,
-      :integer, null: false
+      :integer
 
     add_foreign_key :compras_licitation_process_ratification_items, :compras_purchase_process_creditor_proposals,
       name: :clprt_purchase_process_creditor_proposal_id_fk, column: :purchase_process_creditor_proposal_id
