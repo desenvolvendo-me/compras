c2630f3e0f984fbb6b5044844e9586589c875699
José Gomes Júnior <zegomesjf@gmail.com>
2013-05-26 01:09:21 -0300

    Customiza attr_header para responsáveis do processo
    antes
    http://cl.ly/image/3O1V1b3M2V0s
    
    depois
    http://cl.ly/image/2B451Q1y0O09

---
+++ app/decorators/process_responsible_decorator.rb
@@ -0,0 +1,10 @@
+#encoding: utf-8
+class ProcessResponsibleDecorator
+  include Decore
+  include Decore::Proxy
+  include Decore::Header
+  include ActionView::Helpers::NumberHelper
+  include ActionView::Helpers::TranslationHelper
+
+  attr_header :code_and_year, :modality_or_type_of_removal, :object_type, :status
+end
