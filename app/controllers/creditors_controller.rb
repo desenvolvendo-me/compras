4b71c67ba3fb9be17bb79ec7408a4296431a3a97
Rodrigo Ulisses E Silva <rodrigo.ulisses7@gmail.com>
2013-05-31 11:21:12 -0300

    Adicionando filtro de pesquisa no creditor na tela de Ajudicação.
    Ao pesquisar pelo participante vencedor, só deverá pesquisar pelos participantes habilitados.
    
    [Finishes #50817325]

--- app/controllers/creditors_controller.rb
+++ app/controllers/creditors_controller.rb
@@ -4,4 +4,5 @@ class CreditorsController < CrudController
   has_scope :won_calculation, allow_blank: true
   has_scope :without_direct_purchase_ratification, allow_blank: true
   has_scope :without_licitation_ratification, allow_blank: true
+  has_scope :enabled_by_licitation, allow_blank: true
 end
