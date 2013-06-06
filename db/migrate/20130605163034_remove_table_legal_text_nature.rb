6c0e1c982ee87b2d6a4a802da66bf1d3471319cd
José Gomes Júnior <zegomesjf@gmail.com>
2013-06-06 08:34:14 -0300

    Arruma migration que remove legal_text_natures

--- db/migrate/20130605163034_remove_table_legal_text_nature.rb
+++ db/migrate/20130605163034_remove_table_legal_text_nature.rb
@@ -1,9 +1,7 @@
 class RemoveTableLegalTextNature < ActiveRecord::Migration
-  def up
+  def change
     remove_column :compras_regulatory_acts, :legal_text_nature_id
-  end
 
-  def down
     drop_table :compras_legal_text_natures
   end
 end
