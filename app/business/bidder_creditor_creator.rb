cff7929730cebf4c83e20b9f4e333fff4e930bff
Mateus Lorandi dos Santos <mcomogo@gmail.com>
2013-06-07 16:19:10 -0300

    Cria credores da habilitação automaticamente quando pregão
    [Finishes #50810247]

--- app/business/bidder_creditor_creator.rb
+++ app/business/bidder_creditor_creator.rb
@@ -1,7 +1,7 @@
 class BidderCreditorCreator
-  def initialize(purchase_process,  purchase_process_repository = LicitationProcess)
+  def initialize(purchase_process, options = {})
     @purchase_process = purchase_process
-    @purchase_process_repository = purchase_process_repository
+    @trading_item_winner = options.fetch(:trading_item_winner) { TradingItemWinner }
   end
 
   def self.create!(*params)
@@ -9,14 +9,31 @@ class BidderCreditorCreator
   end
 
   def create_bidders!
-    return nil unless bidders.empty?
+    return unless bidders.empty?
+
+    create_bidders_for_direct_purchase
+    create_bidders_for_trading
+  end
+
+  private
+
+  attr_reader :purchase_process, :trading_item_winner
+
+  def create_bidders_for_trading
+    return unless purchase_process.trading?
+
+    trading_items.each do |item|
+      bidders.create!(licitation_process_id: purchase_process.id, creditor_id: trading_item_winner_creditor_id(item))
+    end
+  end
+
+  def create_bidders_for_direct_purchase
+    return unless purchase_process.direct_purchase?
+
     items.each  do |item|
       bidders.create!(licitation_process_id: purchase_process.id, creditor_id: item.creditor.id)
     end
   end
-
-  private
-  attr_reader :purchase_process
 
   def items
     purchase_process.items
@@ -24,5 +41,17 @@ class BidderCreditorCreator
 
   def bidders
     purchase_process.bidders
+  end
+
+  def trading
+    purchase_process.trading
+  end
+
+  def trading_items
+    trading.items.closed
+  end
+
+  def trading_item_winner_creditor_id(item)
+    trading_item_winner.new(item).creditor.id
   end
 end
