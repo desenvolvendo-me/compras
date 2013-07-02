6e11ab3cf34619d2741363f4edea2aef66e8e21e
José Gomes Júnior <zegomesjf@gmail.com>
2013-07-02 16:39:42 -0300

    Corrige migração do realinhamento de preço

--- db/migrate/20130625115718_migrate_realignment_price_data.rb
+++ db/migrate/20130625115718_migrate_realignment_price_data.rb
@@ -1,7 +1,7 @@
 class MigrateRealignmentPriceData < ActiveRecord::Migration
   def up
     RealignmentPriceItem.find_each do |item|
-      proposal = PurchaseProcessCreditorProposal.find item.proposal_id
+      proposal = PurchaseProcessCreditorProposal.find item.purchase_process_creditor_proposal_id
       realignment = RealignmentPrice.find_or_create_by_purchase_process_id_and_creditor_id_and_lot(
         purchase_process_id: proposal.licitation_process_id,
         creditor_id: proposal.creditor_id,
