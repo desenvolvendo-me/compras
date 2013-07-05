3b7a30cd8cc698ae4124842c68064135e12472b1
Mateus Lorandi dos Santos <mcomogo@gmail.com>
2013-07-05 14:31:02 -0300

    Fornece o método to_s na conversão de json
    Quando um model é convertido para json
    estou adicionando automaticamente o to_s
    para o json.
    
    Com isso não é mais necessário definir
    o to_s no UnicoAPI para os resources.
    
    E também acaba com o problema de quando
    o to_s depende de outro model.

--- lib/compras/model.rb
+++ lib/compras/model.rb
@@ -1,4 +1,10 @@
 class Compras::Model < ActiveRecord::Base
   self.abstract_class = true
   self.table_name_prefix = 'compras_'
+
+  def as_json(options = {})
+    options[:methods] = Array(options[:methods]) << :to_s
+
+    super
+  end
 end
