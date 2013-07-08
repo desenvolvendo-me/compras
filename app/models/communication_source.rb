5626e5a6d734677e73dcb8d48a1f53b9ab0a81c9
Romulo Storel <romulostorel@gmail.com>
2013-07-08 19:26:15 -0300

    Removendo DisseminationSource do sistema * Foi movido para o Unico
    [#53005963]

--- app/models/communication_source.rb
+++ app/models/communication_source.rb
@@ -1,8 +1,6 @@
 class CommunicationSource < Compras::Model
   attr_accessible :description
 
-  has_many :dissemination_sources, :dependent => :restrict
-
   validates :description, :presence => true, :uniqueness => { :allow_blank => true }
 
   filterize
--- spec/models/communication_source_spec.rb
+++ spec/models/communication_source_spec.rb
@@ -1,14 +1,11 @@
 require 'model_helper'
 require 'app/models/communication_source'
-require 'app/models/dissemination_source'
 
 describe CommunicationSource do
   it 'should return to_s as description' do
     subject.description = 'Jornal'
     expect(subject.to_s).to eq 'Jornal'
   end
-
-  it { should have_many(:dissemination_sources).dependent(:restrict) }
 
   it { should validate_presence_of :description }
 end
