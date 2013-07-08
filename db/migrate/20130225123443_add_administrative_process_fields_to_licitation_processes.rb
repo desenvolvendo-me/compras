b8a4aac1105a5d607bc32b71c241a1687129aba7
José Gomes Júnior <zegomesjf@gmail.com>
2013-07-08 15:35:47 -0300

    removendo migração não mais necessária.

--- db/migrate/20130225123443_add_administrative_process_fields_to_licitation_processes.rb
+++ db/migrate/20130225123443_add_administrative_process_fields_to_licitation_processes.rb
@@ -9,20 +9,5 @@ class AddAdministrativeProcessFieldsToLicitationProcesses < ActiveRecord::Migrat
     add_column :compras_licitation_processes, :responsible_id,                      :integer
     add_column :compras_licitation_processes, :purchase_solicitation_item_group_id, :integer
     add_column :compras_licitation_processes, :purchase_solicitation_id,            :integer
-
-    execute <<-SQL
-      UPDATE compras_licitation_processes a SET
-        protocol = b.protocol,
-        item = b.item,
-        object_type = b.object_type,
-        summarized_object = b.summarized_object,
-        description = b.description,
-        responsible_id = b.responsible_id,
-        purchase_solicitation_item_group_id = b.purchase_solicitation_item_group_id,
-        modality = b.modality,
-        purchase_solicitation_id = b.purchase_solicitation_id
-      FROM compras_administrative_processes b
-      WHERE a.administrative_process_id = b.id
-    SQL
   end
 end
