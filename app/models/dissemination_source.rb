0cf59de1977feab3e108b054b7d98000a7a68d91
Romulo Storel <romulostorel@gmail.com>
2013-07-09 17:39:03 -0300

    Remapeando relacionamentos, estourando erro sem os mesmos
    [#53005963]

--- app/models/dissemination_source.rb
+++ app/models/dissemination_source.rb
@@ -1,4 +1,8 @@
 class DisseminationSource < Unico::DisseminationSource
+  belongs_to :communication_source
+
+  has_and_belongs_to_many :regulatory_acts, join_table: :unico_dissemination_sources_unico_regulatory_acts
+
   filterize
   orderize :description
 
