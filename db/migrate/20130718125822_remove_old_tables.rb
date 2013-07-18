24248fd48ccfb8730ebfdf17cc1afe8f1f12d858
Romulo Storel <romulostorel@gmail.com>
2013-07-18 10:35:06 -0300

    Removendo tabelas não mais utilizadas no compras
    [#53422809]

---
+++ db/migrate/20130718125822_remove_old_tables.rb
@@ -0,0 +1,32 @@
+class RemoveOldTables < ActiveRecord::Migration
+  def change
+    remove_table :compras_bank_account_capabilities
+    remove_table :compras_agreement_bank_accounts
+    remove_table :compras_tax_revenue_reversings
+    remove_table :compras_tax_revenue_receipt_reversings
+    remove_table :compras_tax_revenue_receipts
+    remove_table :compras_tax_revenue_closings
+    remove_table :compras_tax_revenues
+    remove_table :compras_pledge_liquidation_parcel_payers
+    remove_table :compras_bank_reconciliation_items
+    remove_table :compras_extra_budget_pledge_payments
+    remove_table :compras_bank_transfer_closings
+    remove_table :compras_bank_transfers
+    remove_table :compras_voided_checks
+    remove_table :compras_checkbooks
+    remove_table :compras_bank_reconciliations
+    remove_table :compras_account_movements
+  end
+
+  protected
+
+  def remove_table(table_name)
+    if connection.table_exists? table_name
+      drop_table table_name
+    end
+  end
+
+  def connection
+    ActiveRecord::Base.connection
+  end
+end
