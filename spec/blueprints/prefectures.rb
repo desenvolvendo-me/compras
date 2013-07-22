a7e3f2eb68b29d64875b2211fa7c8078e1b5a6f9
Romulo Storel <romulostorel@gmail.com>
2013-07-22 19:16:33 -0300

    Corrigindo testes de models e integrations

--- spec/blueprints/prefectures.rb
+++ spec/blueprints/prefectures.rb
@@ -9,3 +9,14 @@ Prefecture.blueprint(:belo_horizonte) do
   address { Address.make!(:apto, :addressable => object) }
   setting { Setting.make!(:parametros_prefeitura, :prefecture => object) }
 end
+
+Prefecture.blueprint(:belo_horizonte_mg) do
+  name { "Belo Horizonte" }
+  cnpj { "48.021.328/0001-10" }
+  phone { "(31) 9999-0909" }
+  fax { "(31) 2343-8908" }
+  email { "belo_horizonte@belohorizonte.com.br" }
+  mayor_name { "Márcio Lacerda" }
+  address { Address.make!(:apto_bh, :addressable => object) }
+  setting { Setting.make!(:parametros_prefeitura, :prefecture => object) }
+end
