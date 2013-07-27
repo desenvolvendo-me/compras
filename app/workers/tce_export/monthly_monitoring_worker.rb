cc6a02132a1ca48ed1b22d7cf9873439d234c4d7
José Gomes Júnior <zegomesjf@gmail.com>
2013-07-27 09:45:08 -0300

    Seta o Customer no worker monthly monitoring.
    Até então estávamos gerando os arquivos SICOM utilizando integração via
    banco, mas agora que está utilizando via API é necessário setar o
    customer no worker.
    
    cc @nohupbrasil/coordenadores

--- app/workers/tce_export/monthly_monitoring_worker.rb
+++ app/workers/tce_export/monthly_monitoring_worker.rb
@@ -3,6 +3,7 @@ class TceExport::MonthlyMonitoringWorker
 
   def perform(customer_id, monthly_monitoring_id)
     using_connection_for(customer_id) do
+      UnicoAPI::Consumer.set_customer(Customer.find customer_id)
       monthly_monitoring = TceExport::MonthlyMonitoring.find(monthly_monitoring_id)
 
       monthly_monitoring.set_file File.open(TceExport::MG::MonthlyMonitoring.generate_zip_file(monthly_monitoring))
