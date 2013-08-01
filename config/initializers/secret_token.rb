32deb5bed7202b2c09532b461fdbd670def9ba58
José Gomes Júnior <zegomesjf@gmail.com>
2013-08-01 17:07:06 -0300

    Secret_token é setado através de uma ENV
    [Finishes #54471846]

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,10 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Compras::Application.config.secret_token = 'c31bfa8977942d83d4a3a2104a5e7b0b4b160c1c77c86dcf88dfdfed75145c4aced365b38c458807ea37675ca4c923c9e53b713636bcf835be7b9a9a7b3a8030'
+raise "You must set a secret token in ENV['SECRET_TOKEN']" if ENV['SECRET_TOKEN'].blank? && Rails.production_way?
+
+if Rails.production_way?
+  Compras::Application.config.secret_token = ENV['SECRET_TOKEN']
+else
+  Compras::Application.config.secret_token = 'c31bfa8977942d83d4a3a2104a5e7b0b4b160c1c77c86dcf88dfdfed75145c4aced365b38c458807ea37675ca4c923c9e53b713636bcf835be7b9a9a7b3a8030'
+end
