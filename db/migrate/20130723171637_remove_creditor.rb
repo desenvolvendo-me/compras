92dd04fcad6bdf2784ffd2e316dc8aaca56c8274
José Gomes Júnior <zegomesjf@gmail.com>
2013-08-01 13:26:03 -0300

    remove tabelas da contabilidade.

--- db/migrate/20130723171637_remove_creditor.rb
+++ db/migrate/20130723171637_remove_creditor.rb
@@ -1,5 +1,19 @@
 class RemoveCreditor < ActiveRecord::Migration
   def change
+    remove_table :compras_pledge_liquidation_parcel_retentions
+    remove_table :compras_pledge_liquidation_parcel_payers
+    remove_table :compras_pledge_liquidation_parcel_payment_closings
+    remove_table :compras_payment_reversal_closings
+    remove_table :compras_pledge_liquidation_parcel_payment_reversals
+    remove_table :compras_bank_reconciliation_items
+    remove_table :compras_pledge_liquidation_parcel_payments
+    remove_table :compras_pledge_liquidation_parcels
+    remove_table :contabilidade_fiscal_note_items
+    remove_table :contabilidade_fiscal_notes
+    remove_table :compras_pledge_liquidations
+    remove_table :compras_pledge_items
+    remove_table :compras_pledge_cancellations
+    remove_table :compras_pledges
     remove_table :compras_extra_budget_pledge_items
     remove_table :compras_extra_budget_pledges
     drop_table :compras_creditors
