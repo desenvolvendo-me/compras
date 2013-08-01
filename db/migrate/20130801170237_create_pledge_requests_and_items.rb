c284362a27adf01040c65bdc30c8fcb87f7e29a6
José Gomes Júnior <zegomesjf@gmail.com>
2013-08-01 14:12:18 -0300

    recria as tabelas pledge_request e items
    porém com a associação correta do credor com o único

---
+++ db/migrate/20130801170237_create_pledge_requests_and_items.rb
@@ -0,0 +1,50 @@
+class CreatePledgeRequestsAndItems < ActiveRecord::Migration
+  def change
+    create_table :compras_pledge_requests do |t|
+      t.integer :descriptor_id
+      t.integer :budget_allocation_id
+      t.integer :expense_nature_id
+      t.integer :accounting_account_id
+      t.integer :contract_id
+      t.integer :reserve_fund_id
+      t.integer :purchase_process_id
+      t.integer :creditor_id
+      t.decimal :amount, precision: 10, scale: 2
+      t.text    :historic_complement
+      t.date    :emission_date
+
+      t.timestamps
+    end
+
+    create_table :compras_pledge_request_items do |t|
+      t.integer :pledge_request_id
+      t.integer :purchase_process_item_id
+      t.integer :accounting_cost_center_id
+      t.integer :quantity
+
+      t.timestamps
+    end
+
+    add_index :compras_pledge_requests, :descriptor_id
+    add_index :compras_pledge_requests, :budget_allocation_id
+    add_index :compras_pledge_requests, :expense_nature_id
+    add_index :compras_pledge_requests, :accounting_account_id
+    add_index :compras_pledge_requests, :reserve_fund_id
+    add_index :compras_pledge_requests, :contract_id
+    add_index :compras_pledge_requests, :purchase_process_id
+
+    add_foreign_key :compras_pledge_requests, :compras_licitation_processes, column: :purchase_process_id
+    add_foreign_key :compras_pledge_requests, :compras_contracts, column: :contract_id
+    add_foreign_key :compras_pledge_requests, :unico_creditors, column: :creditor_id
+
+    add_index :compras_pledge_request_items, :pledge_request_id
+    add_index :compras_pledge_request_items, :purchase_process_item_id
+    add_index :compras_pledge_request_items, :accounting_cost_center_id
+
+    add_foreign_key :compras_pledge_request_items, :compras_pledge_requests,
+      column: :pledge_request_id
+    add_foreign_key :compras_pledge_request_items, :compras_purchase_process_items,
+      column: :purchase_process_item_id
+  end
+end
+
