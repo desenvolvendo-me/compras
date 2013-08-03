d5e27f577250f45bc3c7ef9f86863bce8ebef1d6
José Gomes Júnior <zegomesjf@gmail.com>
2013-08-02 23:14:07 -0300

    Substitui o deprecado fakeweb pelo webmock.
    além disso, estava tendo problemas com o fakeweb em um cenário
    que fazia duas requisições pra API.

--- spec/support/vcr_setup.rb
+++ spec/support/vcr_setup.rb
@@ -2,7 +2,7 @@ require 'vcr'
 
 VCR.configure do |c|
   c.cassette_library_dir = File.expand_path("../../fixtures/vcr_cassettes", __FILE__)
-  c.hook_into :fakeweb
+  c.hook_into :webmock
   c.debug_logger = File.open("#{Rails.root}/log/vcr.log", "w")
 
   c.ignore_request do |request|
