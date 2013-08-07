a937524a156580e664e452641a666df0331cfbfe
Mateus Lorandi dos Santos <mcomogo@gmail.com>
2013-08-07 16:24:29 -0300

    Corrige problema em relatório quando compra direta
    O código estava assumindo que o item do processo
    de compra e as propostas possuíam alguns metodos
    em comum, mas isso não era verdade.
    
    Adicionei o teste pra quando for compra direta.
    
    [Finishes #54785552]

--- spec/features/reports/minute_purchase_processes_spec.rb
+++ spec/features/reports/minute_purchase_processes_spec.rb
@@ -23,55 +23,101 @@ feature 'Report::MinutePurchaseProcesses' do
     BudgetAllocation.stub(:find)
   end
 
-  scenario 'should minute purchase process' do
-    make_dependencies!
+  context 'with a purchase_process as licitation_process' do
+    scenario 'should minute purchase process' do
+      creditor_sobrinho = Creditor.make!(:sobrinho)
+      creditor_wenderson = Creditor.make!(:wenderson_sa)
 
-    navigate 'Processos de Compra > Processos de Compras'
+      item = PurchaseProcessItem.make!(:item)
+      item_arame = PurchaseProcessItem.make!(:item_arame)
 
-    click_link 'Limpar Filtro'
-    click_link '1/2012'
+      purchase_process = LicitationProcess.make!(:processo_licitatorio,
+        bidders: [Bidder.make!(:licitante, creditor: creditor_wenderson), Bidder.make!(:licitante_sobrinho, creditor: creditor_sobrinho)],
+        items: [item, item_arame])
 
-    click_link 'Imprimir ATA'
+      PurchaseProcessCreditorProposal.make!(:proposta_arame_farpado,
+        licitation_process: purchase_process,
+        creditor: creditor_wenderson,
+        item: item)
 
-    expect(page).to have_content I18n.l(Date.tomorrow)
-    expect(page).to have_content '14:00'
-    expect(page).to have_content 'Wenderson Malheiros'
-    expect(page).to have_content 'Gabriel Sobrinho'
+      PurchaseProcessCreditorProposal.make!(:proposta_arame,
+        licitation_process: purchase_process,
+        creditor: creditor_sobrinho,
+        item: item)
+
+      PurchaseProcessCreditorProposal.make!(:proposta_arame_farpado,
+        licitation_process: purchase_process,
+        creditor: creditor_wenderson,
+        item: item_arame)
+
+      PurchaseProcessCreditorProposal.make!(:proposta_arame,
+        licitation_process: purchase_process,
+        creditor: creditor_sobrinho,
+        item: item_arame,
+        unit_price: 4.99)
+
+      JudgmentCommissionAdvice.make!(:parecer, licitation_process: purchase_process)
+
+      navigate 'Processos de Compra > Processos de Compras'
+
+      click_link 'Limpar Filtro'
+      click_link '1/2012'
+
+      click_link 'Imprimir ATA'
+
+      expect(page).to have_content I18n.l(Date.tomorrow)
+      expect(page).to have_content '14:00'
+      expect(page).to have_content 'Wenderson Malheiros'
+      expect(page).to have_content 'Gabriel Sobrinho'
+    end
   end
 
+  context 'with a purchase_process as direct_purchase' do
+    scenario 'should minute purchase process' do
+      creditor_sobrinho = Creditor.make!(:sobrinho)
+      creditor_wenderson = Creditor.make!(:wenderson_sa)
 
-  def make_dependencies!
-    creditor_sobrinho = Creditor.make!(:sobrinho)
-    creditor_wenderson = Creditor.make!(:wenderson_sa)
+      item = PurchaseProcessItem.make!(:item, creditor: creditor_sobrinho)
+      item_arame = PurchaseProcessItem.make!(:item_arame, creditor: creditor_wenderson)
 
-    item = PurchaseProcessItem.make!(:item)
-    item_arame = PurchaseProcessItem.make!(:item_arame)
+      purchase_process = LicitationProcess.make!(:compra_direta,
+        bidders: [Bidder.make!(:licitante, creditor: creditor_wenderson), Bidder.make!(:licitante_sobrinho, creditor: creditor_sobrinho)],
+        items: [item, item_arame])
 
-    purchase_process = LicitationProcess.make!(:processo_licitatorio,
-      bidders: [Bidder.make!(:licitante, creditor: creditor_wenderson), Bidder.make!(:licitante_sobrinho, creditor: creditor_sobrinho)],
-      items: [item, item_arame])
+      PurchaseProcessCreditorProposal.make!(:proposta_arame_farpado,
+        licitation_process: purchase_process,
+        creditor: creditor_wenderson,
+        item: item)
 
-    PurchaseProcessCreditorProposal.make!(:proposta_arame_farpado,
-      licitation_process: purchase_process,
-      creditor: creditor_wenderson,
-      item: item)
+      PurchaseProcessCreditorProposal.make!(:proposta_arame,
+        licitation_process: purchase_process,
+        creditor: creditor_sobrinho,
+        item: item)
 
-    PurchaseProcessCreditorProposal.make!(:proposta_arame,
-      licitation_process: purchase_process,
-      creditor: creditor_sobrinho,
-      item: item)
+      PurchaseProcessCreditorProposal.make!(:proposta_arame_farpado,
+        licitation_process: purchase_process,
+        creditor: creditor_wenderson,
+        item: item_arame)
 
-    PurchaseProcessCreditorProposal.make!(:proposta_arame_farpado,
-      licitation_process: purchase_process,
-      creditor: creditor_wenderson,
-      item: item_arame)
+      PurchaseProcessCreditorProposal.make!(:proposta_arame,
+        licitation_process: purchase_process,
+        creditor: creditor_sobrinho,
+        item: item_arame,
+        unit_price: 4.99)
 
-    PurchaseProcessCreditorProposal.make!(:proposta_arame,
-      licitation_process: purchase_process,
-      creditor: creditor_sobrinho,
-      item: item_arame,
-      unit_price: 4.99)
+      JudgmentCommissionAdvice.make!(:parecer, licitation_process: purchase_process)
 
-    JudgmentCommissionAdvice.make!(:parecer, licitation_process: purchase_process)
+      navigate 'Processos de Compra > Processos de Compras'
+
+      click_link 'Limpar Filtro'
+      click_link '2/2013'
+
+      click_link 'Imprimir ATA'
+
+      expect(page).to have_content I18n.l(Date.current)
+      expect(page).to have_content '14:00'
+      expect(page).to have_content 'Wenderson Malheiros'
+      expect(page).to have_content 'Gabriel Sobrinho'
+    end
   end
 end
