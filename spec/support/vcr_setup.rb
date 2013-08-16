5b904f2525923950e1f27059f389a4cabbd0e050
Mateus Lorandi dos Santos <mcomogo@gmail.com>
2013-08-16 18:17:29 -0300

    Corrige problema de codificação na geração da cassette

--- spec/support/vcr_setup.rb
+++ spec/support/vcr_setup.rb
@@ -11,6 +11,11 @@ VCR.configure do |c|
     serialize_with: :json
   }
 
+  c.preserve_exact_body_bytes do |http_message|
+    http_message.body.encoding.name == 'ASCII-8BIT' ||
+    !http_message.body.valid_encoding?
+  end
+
   c.ignore_request do |request|
     uri = URI(request.uri)
     (uri.port != 8081 && uri.host == "localhost") || (uri.host == "127.0.0.1")
