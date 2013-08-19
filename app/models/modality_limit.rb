208fec8c130435a6ed5b2197477780d93731dcad
Mateus Lorandi dos Santos <mcomogo@gmail.com>
2013-08-19 15:24:46 -0300

    Organiza model de ModalityLimit

--- app/models/modality_limit.rb
+++ app/models/modality_limit.rb
@@ -1,13 +1,11 @@
 class ModalityLimit < Compras::Model
-  attr_accessible :without_bidding, :invitation_letter, :taken_price
-  attr_accessible :public_competition, :work_without_bidding
-  attr_accessible :work_invitation_letter, :work_taken_price
-  attr_accessible :work_public_competition
+  attr_accessible :without_bidding, :invitation_letter, :taken_price,
+    :public_competition, :work_without_bidding, :work_invitation_letter,
+    :work_taken_price, :work_public_competition
 
-  validates :without_bidding, :invitation_letter, :taken_price, :presence => true
-  validates :public_competition, :work_without_bidding, :presence => true
-  validates :work_invitation_letter, :work_taken_price, :presence => true
-  validates :work_public_competition, :presence => true
+  validates :without_bidding, :invitation_letter, :taken_price, :public_competition,
+    :work_without_bidding, :work_invitation_letter, :work_taken_price,
+    :work_public_competition, :presence => true
 
   orderize "id DESC"
   filterize
