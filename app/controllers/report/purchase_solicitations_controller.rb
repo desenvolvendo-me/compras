8fe17a0b99fa51faf844cf4a5c3881e9a7175fb2
Rodrigo Ulisses <rodrigo.ulisses7@gmail.com>
2013-08-29 17:16:59 -0300

    Setando o valor default do Tipo de relatório.
    Ao tenderizer o form, já aparecia a mensagem de erro de validação do formulário.
    Com esse commit essa mensagem não será mais exibida.

--- app/controllers/report/purchase_solicitations_controller.rb
+++ app/controllers/report/purchase_solicitations_controller.rb
@@ -1,3 +1,13 @@
 class Report::PurchaseSolicitationsController < Report::BaseController
   report_class PurchaseSolicitationReport, :repository => PurchaseSolicitationSearcher
+
+  def new
+    @report = report_instance
+
+    unless params[:purchase_solicitation_report] && params[:purchase_solicitation_report][:report_type]
+      @report.report_type = ReportType::ANALYTICAL
+    end
+
+    @report.valid? if @report.searched?
+  end
 end
