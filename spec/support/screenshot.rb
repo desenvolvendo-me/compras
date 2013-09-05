42fc8565e52c1e0d64b7b5778e8f59ea66ce49c2
Mateus Lorandi dos Santos <mcomogo@gmail.com>
2013-09-05 17:29:44 -0300

    Improve the screenshot on errors
    Removed monkey patch and use only rspec after hook
    with some simple logic.

--- spec/support/screenshot.rb
+++ spec/support/screenshot.rb
@@ -1,71 +1,18 @@
-module RSpec
-  module Core
-    class Example
-      def set_exception(exception, context=nil)
-        if @exception
-          # An error has already been set; we don't want to override it,
-          # but we also don't want silence the error, so let's print it.
-          msg = <<-EOS
+RSpec.configure do |config|
+  config.add_setting :screenshot_on_errors, default: ENV['SCREENSHOT']
 
-  An error occurred #{context}
-    #{exception.class}: #{exception.message}
-    occurred at #{exception.backtrace.first}
-
-          EOS
-          RSpec.configuration.reporter.message(msg)
-        end
-
-        @exception ||= exception
-
-        screenshot if screenshot_on_errors? && is_metadata_type_feature?
-      end
-
-      private
-
-      def is_metadata_type_feature?
-        @metadata[:type] == :feature
-      end
-
-      def screenshot_full_path
-        p = Pathname.new(screenshot_folder).join(screenshot_name)
-        p.to_path
-      end
-
-      def screenshot_name
-        "error_#{next_error_number}.png"
-      end
-
-      def next_error_number
-        images_path = Pathname.new(screenshot_folder).join('*.png').to_path
-
-        Dir.glob(images_path).size + 1
-      end
-
-      def screenshot_folder
-        RSpec.configuration.screenshot_folder
-      end
-
-      def screenshot_on_errors?
-        RSpec.configuration.screenshot_on_errors
-      end
-
-      def screenshot
-        Capybara.save_screenshot screenshot_full_path, full: true
-      end
+  config.before(:suite, type: :feature) do
+    if config.screenshot_on_errors
+      FileUtils.rm_rf "tmp/errors"
+      FileUtils.mkdir_p "tmp/errors"
     end
   end
-end
 
-RSpec.configure do |config|
-  config.add_setting :screenshot_folder, :default => 'tmp/errors'
-  config.add_setting :screenshot_on_errors, :default => ENV['SCREENSHOT']
-  config.add_setting :screenshot_full, :default => true
-  config.add_setting :clear_screenshots_before_run, :default => true
+  config.after(:each, type: :feature) do
+    if example.exception && config.screenshot_on_errors
+      file_name = example.description.parameterize.underscore
 
-  config.before(:suite) do
-    if RSpec.configuration.screenshot_on_errors && RSpec.configuration.clear_screenshots_before_run
-      FileUtils.rm_rf RSpec.configuration.screenshot_folder
-      FileUtils.mkdir_p RSpec.configuration.screenshot_folder
+      page.save_screenshot "tmp/errors/#{file_name}.png", full: true
     end
   end
 end
