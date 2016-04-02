748e7e54495ea9aac9ee772a25fc2a1ee9ed6dc3
Talles Coelho <talles.coelho@integragrp.com.br>
2016-04-02 19:22:51 -0300

    Atualiza configuração da região do carrierwave para variável de ambiente

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,11 +1,12 @@
-if Rails.production_way?
+if Rails.env.production? || Rails.env.staging? || Rails.env.training?
   CarrierWave.configure do |config|
     config.storage = :fog
 
     config.fog_credentials = {
-      :provider              => 'AWS',
-      :aws_access_key_id     => ENV['S3_KEY'],
-      :aws_secret_access_key => ENV['S3_SECRET']
+      provider: 'AWS',
+      aws_access_key_id: ENV['S3_KEY'],
+      aws_secret_access_key: ENV['S3_SECRET'],
+      region: ENV['AWS_REGION']
     }
 
     config.fog_directory = "integra-#{Rails.env}"
