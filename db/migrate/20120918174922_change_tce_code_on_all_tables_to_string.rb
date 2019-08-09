baf5959d88979f5b8c72fc7113028537f299b1b2
Hugo Silva <hugocardososilva@gmail.com>
2019-08-09 18:23:17 -0300

    comentando migracao problematica

--- db/migrate/20120918174922_change_tce_code_on_all_tables_to_string.rb
+++ db/migrate/20120918174922_change_tce_code_on_all_tables_to_string.rb
@@ -3,6 +3,6 @@ class ChangeTceCodeOnAllTablesToString < ActiveRecord::Migration
     change_column :compras_agreement_kinds, :tce_code, :string
     change_column :compras_checking_account_of_fiscal_accounts, :tce_code, :string
     change_column :compras_checking_account_structure_informations, :tce_code, :string
-    change_column :compras_service_or_contract_types, :tce_code, :string
+    #change_column :compras_service_or_contract_types, :tce_code, :string
   end
 end
