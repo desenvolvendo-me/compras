322369cf8371c888b4b9cdadcb0e551fda45a682
Hugo Silva <hugocardososilva@gmail.com>
2019-08-11 06:26:26 -0300

    removendo add constraint

--- db/migrate/20130708200149_add_column_pledge_id_in_supply_orders.rb
+++ db/migrate/20130708200149_add_column_pledge_id_in_supply_orders.rb
@@ -3,7 +3,7 @@ class AddColumnPledgeIdInSupplyOrders < ActiveRecord::Migration
     add_column :compras_supply_orders, :pledge_id, :integer
 
     add_index :compras_supply_orders, :pledge_id
-    add_foreign_key :compras_supply_orders, :accounting_pledges, column: :pledge_id
+    #add_foreign_key :compras_supply_orders, :accounting_pledges, column: :pledge_id
   end
 
   def down
