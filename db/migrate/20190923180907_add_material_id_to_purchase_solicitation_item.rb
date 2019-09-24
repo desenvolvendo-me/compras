b9b56f50f764b6a2c3a6f2b85366916e51d4e1a1
andbri <andbri312@gmail.com>
2019-09-24 09:30:52 -0300

    remove migrate pegaram todo o banco de produção e colocaram em treino,assim sendo essa migrate não é necessária já que foi feito com base em outra versão do banco.

--- db/migrate/20190923180907_add_material_id_to_purchase_solicitation_item.rb
+++
@@ -1,8 +0,0 @@
-class AddMaterialIdToPurchaseSolicitationItem < ActiveRecord::Migration
-  def change
-    add_column :compras_purchase_solicitation_items, :material_id, :integer
-    add_index :compras_purchase_solicitation_items, :material_id
-
-    add_foreign_key :compras_purchase_solicitation_items, :unico_materials, :column => :material_id
-  end
-end
