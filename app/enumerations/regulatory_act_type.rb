60736fd4f00ae157ffc51f6a5bc35a3888e48217
andbri <andbri312@gmail.com>
2019-09-27 11:23:29 -0300

    remove uncomment lines

--- app/enumerations/regulatory_act_type.rb
+++ app/enumerations/regulatory_act_type.rb
@@ -1,12 +1,4 @@
 class RegulatoryActType < EnumerateIt::Base
-  # associate_values budget_change:  3,
-  #                  establishes_licitation_comission: 7,
-  #                  ldo:  5,
-  #                  loa:  4,
-  #                  ppa:  6,
-  #                  regulamentation_of_price_registration:  1,
-  #                  regulamentation_of_trading: 2
-  #
   associate_values :budget_change,
                    :establishes_licitation_comission,
                    :ldo,:loa,:ppa,
