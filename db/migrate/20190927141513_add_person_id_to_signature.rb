04fa713339e184467b6033418024cad4c9741fc2
andbri <andbri312@gmail.com>
2019-10-02 09:54:53 -0300

    atualizar migration para resolver erro 500 na base

--- db/migrate/20190927141513_add_person_id_to_signature.rb
+++
@@ -1,9 +0,0 @@
-class AddPersonIdToSignature < ActiveRecord::Migration
-  def change
-    add_column :compras_signatures, :person_id,
-               :integer
-
-    add_index :compras_signatures, :person_id
-
-  end
-end
