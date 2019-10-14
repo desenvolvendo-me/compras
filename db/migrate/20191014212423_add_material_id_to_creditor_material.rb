135090c3497b371060819ae754ff77419386aa55
andbri <andbri312@gmail.com>
2019-10-14 18:27:27 -0300

    migration para tabela compras_creditor_materials ter material_id da tabela unico_materials

---
+++ db/migrate/20191014212423_add_material_id_to_creditor_material.rb
@@ -0,0 +1,11 @@
+class AddMaterialIdToCreditorMaterial < ActiveRecord::Migration
+  def change
+    add_column :compras_creditor_materials, :material_id, :integer
+
+    add_index :compras_creditor_materials, :material_id
+
+    add_foreign_key :compras_creditor_materials,
+                    :unico_materials,
+                    column: :material_id
+  end
+end
