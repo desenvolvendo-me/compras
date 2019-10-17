b9603a3d6138dc0b820da06827027aa673ed9ab4
andbri <andbri312@gmail.com>
2019-10-17 17:51:09 -0300

    vinculando compras_price_collection_items ao unico_materials

---
+++ db/migrate/20191017183525_add_material_id_to_price_collection_items.rb
@@ -0,0 +1,10 @@
+class AddMaterialIdToPriceCollectionItems < ActiveRecord::Migration
+  def change
+    add_column :compras_price_collection_items,
+               :material_id, :integer
+    add_index :compras_price_collection_items, :material_id
+    add_foreign_key :compras_price_collection_items,
+                    :unico_materials,
+                    column: :material_id
+  end
+end
