3742321eb5c5785af5ce307598b69ee98c235d59
andbri <andbri312@gmail.com>
2019-10-17 17:50:42 -0300

    vinculando compras_purchase_process_items ao unico_materials

---
+++ db/migrate/20191017180846_add_material_id_to_purchase_process_items.rb
@@ -0,0 +1,9 @@
+class AddMaterialIdToPurchaseProcessItems < ActiveRecord::Migration
+  def change
+    add_column :compras_purchase_process_items,
+               :material_id, :integer
+    add_index :compras_purchase_process_items, :material_id
+    add_foreign_key :compras_purchase_process_items, :unico_materials,
+                    column: :material_id
+  end
+end
