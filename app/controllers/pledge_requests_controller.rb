9ea00eb62991b2e181be4c4f2a19478694716cc4
andbri <andbri312@gmail.com>
2019-11-10 11:30:40 -0300

    [u] pledge_request

--- app/controllers/pledge_requests_controller.rb
+++ app/controllers/pledge_requests_controller.rb
@@ -1,4 +1,16 @@
 class PledgeRequestsController < CrudController
   has_scope :purchase_process_id
 
+  def create
+    create! do |success, failure|
+      success.html { redirect_to edit_pledge_request_path(resource) }
+    end
+  end
+
+  def update
+    update! do |success, failure|
+      success.html { redirect_to edit_pledge_request_path(resource) }
+    end
+  end
+
 end
