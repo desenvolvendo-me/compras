8903e2ec05ca109f0cb52d08c039c2c16ac1cf86
andbri <andbri312@gmail.com>
2019-11-11 15:13:14 -0300

    [destroy] pledge_request_purchase_solicitation

--- spec/factories/pledge_request_purchase_solicitations.rb
+++
@@ -1,8 +0,0 @@
-# Read about factories at https://github.com/thoughtbot/factory_girl
-
-FactoryGirl.define do
-  factory :pledge_request_purchase_solicitation do
-    pledge_request nil
-    purchase_solicitation nil
-  end
-end
--- spec/models/pledge_request_purchase_solicitation_spec.rb
+++
@@ -1,6 +0,0 @@
-require 'model_helper'
-require 'app/models/pledge_request_purchase_solicitation'
-
-describe PledgeRequestPurchaseSolicitation do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
