c56a99322f55799e4ec6b1a0c4132986ffa966c6
andbri <andbri312@gmail.com>
2019-11-18 11:06:34 -0300

    [c] contract_report

---
+++ app/reports/contract_report.rb
@@ -0,0 +1,8 @@
+class ContractReport < Report
+
+  attr_accessor :contract_number,
+                :year,:creditor,:creditor_id,:publication_date,
+                :content,:contract_value,:modality_humanize,
+                :start_date,:end_date,:content
+
+end
