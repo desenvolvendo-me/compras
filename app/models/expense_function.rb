d72677dadb05b17cc821253b1094e20c9ca94d33
andbri <andbri312@gmail.com>
2019-11-19 12:53:21 -0300

    [u] model expense_function, add unique to code

--- app/models/expense_function.rb
+++ app/models/expense_function.rb
@@ -2,6 +2,7 @@ class ExpenseFunction < Compras::Model
   attr_accessible :code, :name
 
   validates :code, :name, presence: true
+  validates :code, uniqueness: true
 
   orderize "created_at"
   filterize
