3f3606186a5fe3ac2e2dba0200659cfa38b298d5
andbri <andbri312@gmail.com>
2019-11-22 12:22:14 -0300

    [u] mode organ, callback adicionado

--- app/models/organ.rb
+++ app/models/organ.rb
@@ -4,10 +4,20 @@ class Organ < Compras::Model
   has_enumeration_for :category, :with => OrganCategory, :create_helpers => true
 
   validates :name, :code, presence: true, uniqueness:true
+  validates :category, presence: true
   validates :year, :mask => "9999", :allow_blank => true
+  before_save :code_category
 
   orderize "created_at"
   filterize
+
+  def code_category
+    if self.category == 'analytical'
+      errors.add(:code, :wrong_length, :count=> 5) unless self.code.length == 6
+    else
+      errors.add(:code, :wrong_length, :count=> 2) unless self.code.length == 2
+    end
+  end
 
   def to_s
     code
