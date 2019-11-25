092dfb835e1d4af2e347f0cfd27ba77ca62235b3
andbri <andbri312@gmail.com>
2019-11-25 10:29:18 -0300

    puchasing_unit

--- app/models/purchasing_unit.rb
+++ app/models/purchasing_unit.rb
@@ -7,7 +7,7 @@ class PurchasingUnit < Compras::Model
   has_enumeration_for :situation, :with => PurchasingUnitSituation, :create_helpers => true
 
   validates :code, :name, presence: true
-  validates :code, :name,uniqueness: true, if: :unique_situation_active?
+  validates :code,uniqueness: true, if: :unique_situation_active?
   validates :code, :mask => "9999"
 
   def to_s
