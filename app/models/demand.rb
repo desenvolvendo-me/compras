c97b6341500bf66513b4f9040334fb4423b4b2a4
andbri <andbri312@gmail.com>
2019-12-06 16:55:00 -0300

    [u] model Demand

--- app/models/demand.rb
+++ app/models/demand.rb
@@ -1,10 +1,16 @@
 class Demand < Compras::Model
   attr_accessible  :name,:final_date, :initial_date,
-    :observation, :status, :year,:purchasing_unit_id,:purchase_solicitation_id
+    :observation, :status, :year,:purchasing_unit_id,
+                   :purchase_solicitation_id,
+                   :department_ids
 
   belongs_to :purchase_solicitation
   belongs_to :purchasing_unit
   has_many :demand_batches, class_name: 'DemandBatch', :order => :id,dependent: :destroy
+
+  has_many :demand_departments, :dependent => :destroy
+  has_many :departments, :through => :demand_departments, :order => :id
+
 
   has_enumeration_for :status, :with => DemandStatus, :create_helpers => true
 
