9974f533317ae8dc5216e0a4928ff2044072524b
andbri <andbri312@gmail.com>
2019-12-13 09:36:13 -0300

    [u] supply_ordes _show, modelo do relatório concluído

--- app/reports/supply_order_report.rb
+++ app/reports/supply_order_report.rb
@@ -13,6 +13,14 @@ class SupplyOrderReport < Report
     supply_order.creditor.person.name
   end
 
+  def get_delivery_location
+    if supply_order.licitation_process.purchase_solicitations.blank? || supply_order.licitation_process.purchase_solicitations.first.purchase_solicitation.delivery_location.nil?
+      "Não Informado"
+    else
+      supply_order.licitation_process.purchase_solicitations.first.purchase_solicitation.delivery_location
+    end
+  end
+
   def licitation_process
     supply_order.licitation_process
   end
