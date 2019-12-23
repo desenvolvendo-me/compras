8b0ed6b7a1ca57da2b6c9d0f61503adb2b231bc7
Marco Castro <marcodotcastro@gmail.com>
2019-12-23 09:55:16 -0300

    [f] lista

--- app/decorators/supply_order_decorator.rb
+++ app/decorators/supply_order_decorator.rb
@@ -5,10 +5,6 @@ class SupplyOrderDecorator
 
   attr_header :creditor, :authorization_date, :licitation_process
 
-  def creditor
-    self.contract.nil? || self.contract.creditors.blank? || self.contract.creditors.first.person.nil? ? '' : self.contract.creditors.first.person.name
-  end
-
   def modality_or_type_of_removal
     "#{component.modality_number} - #{component.modality_humanize || component.type_of_removal_humanize}"
   end
