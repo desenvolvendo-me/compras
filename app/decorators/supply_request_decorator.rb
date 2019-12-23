6516e0b1385bb3d19f1b8138b4d3932546434afb
Marco Castro <marcodotcastro@gmail.com>
2019-12-23 11:35:50 -0300

    [c] view supply_request e supply_request_item

---
+++ app/decorators/supply_request_decorator.rb
@@ -0,0 +1,11 @@
+class SupplyRequestDecorator
+  include Decore
+  include Decore::Proxy
+  include Decore::Header
+
+  attr_header :creditor, :authorization_date, :licitation_process
+
+  def modality_or_type_of_removal
+    "#{component.modality_number} - #{component.modality_humanize || component.type_of_removal_humanize}"
+  end
+end
