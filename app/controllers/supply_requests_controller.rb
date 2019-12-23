4b12d5f5a03af9d2deb37c90ca02c57ca9174b57
Marco Castro <marcodotcastro@gmail.com>
2019-12-23 15:33:27 -0300

    [a] adicionado filtro por departamento

--- app/controllers/supply_requests_controller.rb
+++ app/controllers/supply_requests_controller.rb
@@ -1,2 +1,13 @@
 class SupplyRequestsController < CrudController
+
+  def index
+    @supply_requests = filter_by_department(collection)
+  end
+
+  protected
+
+  def filter_by_department(collection)
+    departments = DepartmentPerson.where(user_id: current_user.id).pluck(:department_id)
+    collection.joins(:purchase_solicitation).where("compras_purchase_solicitations.department_id IN (?) ", departments)
+  end
 end
