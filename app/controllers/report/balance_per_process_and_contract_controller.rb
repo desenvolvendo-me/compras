aa8b2f50629569dc2bcc388127ebc0997475e5cd
Marco Castro <marcodotcastro@gmail.com>
2020-01-03 13:45:22 -0300

    [a] balance_per_process_and_contract_report

--- app/controllers/report/balance_per_process_and_contract_controller.rb
+++ app/controllers/report/balance_per_process_and_contract_controller.rb
@@ -18,10 +18,10 @@ class Report::BalancePerProcessAndContractController < Report::BaseController
   def get_balance_per_process_and_contract
 
     @licitation_processes = LicitationProcess.
-        where(balance_per_process_and_contract_report_params.except!(:creditor_id))
+        where(id: balance_per_process_and_contract_report_params["licitation_process_id"])
   end
 
   def balance_per_process_and_contract_report_params
-    @params = params.require(:balance_per_process_and_contract_report).permit(:process, :creditor_id)
+    @params = params.require(:balance_per_process_and_contract_report).permit(:licitation_process_id, :creditor_id, :contract_id)
   end
 end
--- app/reports/balance_per_process_and_contract_report.rb
+++ app/reports/balance_per_process_and_contract_report.rb
@@ -1,6 +1,6 @@
 class BalancePerProcessAndContractReport < Report
 
-  attr_accessor :licitation_process, :licitation_process_id, :creditor, :creditor_id, :department, :department_id
+  attr_accessor :licitation_process, :licitation_process_id, :creditor, :creditor_id, :contract, :contract_id
 
 
 end
