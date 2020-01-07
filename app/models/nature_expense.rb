e8e58a4de4617a9adf55b6f2729cfd4b10ede6b0
Marco Castro <marcodotcastro@gmail.com>
2020-01-07 11:34:05 -0300

    [f] modal

--- app/models/nature_expense.rb
+++ app/models/nature_expense.rb
@@ -1,18 +1,20 @@
 class NatureExpense < Compras::Model
-  attr_accessible :description, :nature,:split_expenses_attributes
+  attr_accessible :description, :nature, :split_expenses_attributes
 
+  attr_modal :description, :nature
+  
   has_many :split_expenses, class_name: 'SplitExpense',
            :order => :id,dependent: :destroy
   accepts_nested_attributes_for :split_expenses, allow_destroy: true
 
-  validates :description, :nature, presence: true, uniqueness:true
+  validates :description, :nature, presence: true, uniqueness: true
   validates :year, :mask => "9999", :allow_blank => true
 
   orderize "created_at"
   filterize
 
-  scope :term, lambda { |q|
-    where { description.like("#{q}%") }
+  scope :term, lambda {|q|
+    where {description.like("#{q}%")}
   }
 
   def to_s
