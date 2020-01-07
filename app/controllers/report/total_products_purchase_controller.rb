33b82f1710c420528d2abeef1a8084b577560637
Marco Castro <marcodotcastro@gmail.com>
2020-01-07 09:20:29 -0300

    [f] filter creditor

--- app/controllers/report/total_products_purchase_controller.rb
+++ app/controllers/report/total_products_purchase_controller.rb
@@ -19,7 +19,7 @@ class Report::TotalProductsPurchaseController < Report::BaseController
     @creditor = total_products_purchase_report_params["creditor_id"]
     @material = total_products_purchase_report_params["material_id"]
 
-    @creditors = Creditor.joins(licitation_process_ratification_items: :purchase_process_item)
+    @creditors = Creditor.joins(:contracts, licitation_process_ratification_items: :purchase_process_item)
     @creditors = @creditors.where(id: @creditor) if @creditor.present?
     @creditors = @creditors.where("compras_purchase_process_items.material_id = #{@material}") if @material.present?
     @creditors
