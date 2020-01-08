78936e187ae97caf5ba892132eadc92a4968d5df
Marco Castro <marcodotcastro@gmail.com>
2020-01-08 16:21:43 -0300

    [a] calc margin

--- app/controllers/additive_solicitations_controller.rb
+++ app/controllers/additive_solicitations_controller.rb
@@ -6,14 +6,9 @@ class AdditiveSolicitationsController < CrudController
     material_id = params["material_id"]
     licitation_process_id = params["licitation_process_id"]
 
-    additived_margen = AdditiveSolicitationItem.joins(:additive_solicitation).where("compras_additive_solicitations.licitation_process_id = #{licitation_process_id}").where("compras_additive_solicitation_items.material_id = #{material_id}").group("compras_additive_solicitation_items.material_id").select("SUM(compras_additive_solicitation_items.quantity * compras_additive_solicitation_items.value) AS total").first.total.to_i
-    item = PurchaseProcessItem.where(licitation_process_id: licitation_process_id).where(material_id: material_id).first
+    response = AdditiveSolicitation.calc_items_margin(licitation_process_id, material_id, quantity, value)
 
-    solicited_margen = quantity * value
-    max_margen = item.quantity * item.unit_price.to_f
-    margen = (((solicited_margen + additived_margen) / max_margen) * 100).round(2)
-
-    render :json => {balance: margen}
+    render :json => {balance: response["margen"]}
   end
 
 end
--- app/models/additive_solicitation.rb
+++ app/models/additive_solicitation.rb
@@ -16,4 +16,29 @@ class AdditiveSolicitation < Compras::Model
 
   orderize "id DESC"
   filterize
+
+  validate :items_margen_permitted
+
+  def items_margen_permitted
+    self.items.each do |item|
+      response = AdditiveSolicitation.calc_items_margin(self.licitation_process.id, item.material.id, item.quantity, item.value)
+      errors.add(:items, response["message"]) if response["message"].present?
+    end
+  end
+
+  def self.calc_items_margin(licitation_process_id, material_id, quantity, value)
+    response = {}
+    limit_marge = 25
+    additived_margen = AdditiveSolicitationItem.joins(:additive_solicitation).where("compras_additive_solicitations.licitation_process_id = #{licitation_process_id}").where("compras_additive_solicitation_items.material_id = #{material_id}").group("compras_additive_solicitation_items.material_id").select("SUM(compras_additive_solicitation_items.quantity * compras_additive_solicitation_items.value) AS total").first.total.to_i
+    item = PurchaseProcessItem.where(licitation_process_id: licitation_process_id).where(material_id: material_id).first
+    solicited_margen = quantity * value
+    max_margen = item.quantity * item.unit_price.to_f
+    margen = (limit_marge - (((solicited_margen + additived_margen) / max_margen) * 100)).round(2)
+    margen_available = (limit_marge - (((additived_margen) / max_margen) * 100)).round(2)
+
+    response["margen"] = margen
+    response["margen_available"] = margen_available
+    response["message"] = "Margem solicitada indisponível. Margem disponível: #{item.material} (#{margen_available}%)" if margen < 0
+    response
+  end
 end
