b06129f691d982bd8197d7df02d6576cc39d62db
Marco Castro <marcodotcastro@gmail.com>
2020-01-08 14:16:51 -0300

    [c] calcular margem

--- app/controllers/additive_solicitations_controller.rb
+++ app/controllers/additive_solicitations_controller.rb
@@ -1,6 +1,9 @@
 class AdditiveSolicitationsController < CrudController
 
   def margin
+    material = Material.find(params["material_id"])
+    licitation_process = LicitationProcess.find(params["licitation_process_id"])
+
     render :json => {balance: 100, value: 50}
   end
 
