54720d655a17c9f86b73be42b25d0a4170e4466b
Marco Castro <marcodotcastro@gmail.com>
2020-01-08 15:01:46 -0300

    [a] post value e quantity

--- app/assets/javascripts/additive_solicitations.js
+++ app/assets/javascripts/additive_solicitations.js
@@ -1,19 +1,22 @@
 function setMaterialMarginBalance() {
     var material_id = $('#additive_solicitation_material_id').val()
     var licitation_process_id = $('#additive_solicitation_licitation_process_id').val()
+    var quantity = $('#additive_solicitation_quantity').val()
+    var value = $('#additive_solicitation_value').val()
 
-    if (material_id) {
+    if (licitation_process_id && material_id && quantity && value) {
         $.ajax({
             url: Routes.additive_solicitation_material_margin,
             data: {
                 material_id: material_id,
                 licitation_process_id: licitation_process_id,
+                quantity: quantity,
+                value: value,
             },
             dataType: 'json',
             type: 'POST',
             success: function (data) {
                 $('#additive_solicitation_balance').val(data["balance"]);
-                $('#additive_solicitation_value').val(data["value"]);
             }
         });
     }
@@ -22,7 +25,7 @@ function setMaterialMarginBalance() {
 $(document).ready(function () {
     setMaterialMarginBalance();
 
-    $('form.additive_solicitation').on('change', '#additive_solicitation_material_id', function () {
+    $('form.additive_solicitation').on('change', '#additive_solicitation_licitation_process_id, #additive_solicitation_material_id, #additive_solicitation_quantity, #additive_solicitation_value', function () {
         setMaterialMarginBalance();
     });
 
--- app/controllers/additive_solicitations_controller.rb
+++ app/controllers/additive_solicitations_controller.rb
@@ -1,10 +1,12 @@
 class AdditiveSolicitationsController < CrudController
 
   def margin
+    quantity = params["quantity"]
+    value = params["value"]
     material = Material.find(params["material_id"])
     licitation_process = LicitationProcess.find(params["licitation_process_id"])
 
-    render :json => {balance: 100, value: 50}
+    render :json => {balance: 100}
   end
 
 end
