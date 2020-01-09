915be7dd3d1f5e76c44a91ecb6d57de52d9f3335
Marco Castro <marcodotcastro@gmail.com>
2020-01-09 13:39:49 -0300

    [a] Extrato de Consumo de Materiais por Processo estrutura

---
+++ app/controllers/report/extract_consumption_per_process_controller.rb
@@ -0,0 +1,36 @@
+class Report::ExtractConsumptionPerProcessController < Report::BaseController
+  include Report::BalanceHelper
+  include Report::ExtractConsumptionPerProcessHelper
+
+  report_class ExtractConsumptionPerProcessReport, :repository => ExtractConsumptionPerProcessSearcher
+
+  def show
+    @licitation_processes = get_extract_consumption_per_process
+
+    @report = report_instance
+
+    if @report.valid?
+      render layout: 'report'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def get_extract_consumption_per_process
+    @licitation_process = extract_consumption_per_process_report_params["licitation_process_id"]
+    @contract = extract_consumption_per_process_report_params["contract_id"]
+    @creditor = extract_consumption_per_process_report_params["creditor_id"]
+
+    @licitation_processes = LicitationProcess.joins(contracts: [:creditors])
+    @licitation_processes = @licitation_processes.where(id: @licitation_process) if @licitation_process.present?
+    @licitation_processes = @licitation_processes.where("compras_contracts.id = #{@contract}") if @contract.present?
+    @licitation_processes = @licitation_processes.where("compras_contracts_unico_creditors.creditor_id = #{@creditor}") if @creditor.present?
+    @licitation_processes
+  end
+
+  def extract_consumption_per_process_report_params
+    @params = params.require(:extract_consumption_per_process_report).permit(:licitation_process_id, :creditor_id, :contract_id)
+  end
+end
---
+++ app/helpers/report/extract_consumption_per_process_helper.rb
@@ -0,0 +1,9 @@
+module Report::ExtractConsumptionPerProcessHelper
+
+  def self.get_contracts(licitation_process, contract)
+    contracts = licitation_process.contracts
+    contracts = contracts.where(id: contract) if contract.present?
+    contracts
+  end
+
+end
