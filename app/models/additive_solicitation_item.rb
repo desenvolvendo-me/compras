fbf9b1a99175979ce003fe156a8c9721c0f70749
Marco Castro <marcodotcastro@gmail.com>
2020-01-10 09:49:29 -0300

    [f] material e js

--- app/models/additive_solicitation_item.rb
+++ app/models/additive_solicitation_item.rb
@@ -1,6 +1,6 @@
 class AdditiveSolicitationItem < Compras::Model
 
-  attr_accessible :quantity, :value, :material_id, :material
+  attr_accessible :quantity, :value, :material_id
 
   belongs_to :additive_solicitation
   belongs_to :material
