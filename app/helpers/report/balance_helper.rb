11b516d2e9a39b86a11e542638496d196b388315
Marco Castro <marcodotcastro@gmail.com>
2020-01-13 10:36:36 -0300

    [f] balance

--- app/helpers/report/balance_helper.rb
+++ app/helpers/report/balance_helper.rb
@@ -1,18 +1,14 @@
 module Report::BalanceHelper
 
   def self.get_balance(contract, item)
-    balance = {"total" => 0, "balance" => 0}
-
     supply_orders = SupplyOrder.joins(:contract, :items)
     supply_orders = supply_orders.where("compras_supply_order_items.material_id = #{item.material.id}")
     supply_orders = supply_orders.where(contract_id: contract.id)
 
+    total_provided = supply_orders.sum(:quantity) if supply_orders.any?
+    total_bid = contract.licitation_process.items.where(material_id: item.material.id).sum(:quantity)
 
-    total = supply_orders.sum(:quantity) if supply_orders.any?
-
-    balance["total"] = total.to_i
-    balance["balance"] = item.quantity - total.to_i
-
+    balance = total_bid.to_i - total_provided.to_i
     balance
   end
 
