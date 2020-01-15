6e0b169de15da9c7b5c7c731dd074829d6308044
Marco Castro <marcodotcastro@gmail.com>
2020-01-15 16:03:59 -0300

    [f] clean js

--- app/assets/javascripts/pledge_requests.js
+++ app/assets/javascripts/pledge_requests.js
@@ -1,78 +1,5 @@
 //= require modal_filter
 
-function sumTotalItems() {
-    var unit_price, quantity, total = 0;
-
-    $('.nested-items:visible').each(function () {
-        unit_price = new BigNumber(parsePtBrFloat($(this).find('.unit_price').val() || '0'));
-        quantity = new BigNumber($(this).find('.quantity').val() || '0');
-
-        total += unit_price * quantity;
-    });
-
-    $('#pledge_request_items_total_value').val(numberWithDelimiter(total));
-}
-
-function setItemModalUrl() {
-    var url = Routes.modal_purchase_processe_items,
-        creditor_id = $('#pledge_request_creditor_id').val(),
-        purchase_process_id = $('#pledge_request_purchase_process_id').val(),
-        params = {
-            ratification_creditor_id: creditor_id,
-            licitation_process_id: purchase_process_id
-        };
-
-    url += '?' + $.param(params);
-
-    $('input.purchase_process_item').data('modal-url', url);
-}
-
-function setCreditorModalUrl() {
-    var url = Routes.modal_creditors,
-        purchase_process_id = $('#pledge_request_purchase_process_id').val(),
-        params = {
-            by_ratification_and_licitation_process_id: purchase_process_id
-        };
-
-    url += '?' + $.param(params);
-
-    $('#pledge_request_creditor').data('modal-url', url);
-}
-
-function setContractModalUrl() {
-    var url = Routes.modal_contracts,
-        purchase_process_id = $('#pledge_request_purchase_process_id').val(),
-        params = {
-            purchase_process_id: purchase_process_id
-        };
-
-    url += '?' + $.param(params);
-
-    $('#pledge_request_contract').data('modal-url', url);
-}
-
-function setBudgetAllocationSource(budget_allocations_ids) {
-    var url = Routes.budget_allocations,
-        params = {
-            by_ids: budget_allocations_ids
-        };
-
-    url += '?' + $.param(params);
-
-    $('#pledge_request_budget_allocation').data('source', url);
-}
-
-function setExpenseNatureModalUrl(parent_id) {
-    var url = Routes.modal_expense_natures,
-        params = {
-            by_parent_id: parent_id
-        };
-
-    url += '?' + $.param(params);
-
-    $('#pledge_request_expense_nature').data('modal-url', url);
-}
-
 function setCreditor() {
     var contract_id = $('#pledge_request_contract_id').val()
     if (contract_id) {
@@ -92,132 +19,74 @@ function setCreditor() {
     }
 }
 
-function setPurchaseSolicitationBalance() {
-    var purchase_process_id = $('#pledge_request_purchase_process_id').val()
-    var purchase_solicitation_id = $('#pledge_request_purchase_solicitation_id').val()
+function setModalUrlToLicitationProcess() {
+    var selector_id = '#pledge_request_contract_id'
+    var selector_modal = '#pledge_request_purchase_process'
 
-    if (purchase_solicitation_id) {
-        $.ajax({
-            url: Routes.purchase_solicitation_balance,
-            data: {
-                purchase_solicitation_id: purchase_solicitation_id,
-                purchase_process_id: purchase_process_id
-            },
-            dataType: 'json',
-            type: 'GET',
-            success: function (data) {
-                $('#pledge_request_purchase_solicitation_balance').val(data["balance"]);
-
-            }
-        });
+    var id = $(selector_id).val();
+    if (id) {
+        setModalUrlToLicitationProcessByContract(selector_id, selector_modal)
     }
-}
-
-function setLicitationProcesso() {
-    var contract_id = $('#pledge_request_contract_id').val()
-    if (contract_id) {
-        $.ajax({
-            url: Routes.contracts_licitation_process,
-            data: {
-                contract_id: contract_id
-            },
-            dataType: 'json',
-            type: 'POST',
-            success: function (data) {
-                $('#pledge_request_purchase_process_id').val(data["licitation_process"]["id"]);
-                $('#pledge_request_purchase_process').val(data["licitation_process"]["description"]);
-            }
-        });
-    }
-
-
 }
 
 function setModalUrlToPurchaseSolicitation() {
     var selector_licitation_process_id = '#pledge_request_purchase_process_id'
     var selector_purchase_solicitation_modal = '#pledge_request_purchase_solicitation'
-    setModalUrlToPurchaseSolicitationByLicitationProcess(selector_licitation_process_id, selector_purchase_solicitation_modal)
+
+    var id = $(selector_licitation_process_id).val();
+    if (id) {
+        setModalUrlToPurchaseSolicitationByLicitationProcess(selector_licitation_process_id, selector_purchase_solicitation_modal)
+    }
+}
+
+function setModalUrlToPurchaseForm() {
+    var selector_purchase_solicitation_id = '#pledge_request_purchase_solicitation_id'
+    var selector_purchase_form_modal = '#pledge_request_purchase_form'
+
+    var id = $(selector_purchase_solicitation_id).val();
+    if (id) {
+        setModalUrlToPurchaseFormByPurchaseSolicitation(selector_purchase_solicitation_id, selector_purchase_form_modal)
+    }
+}
+
+function disableLicitationProcess() {
+    var purchase_contract_id = $("#pledge_request_contract_id").val()
+    $("#pledge_request_purchase_process").attr("disabled", purchase_contract_id ? false : true)
+}
+
+function disablePurchaseSolicitation() {
+    var purchase_process_id = $("#pledge_request_purchase_process_id").val()
+    $("#pledge_request_purchase_solicitation").attr("disabled", purchase_process_id ? false : true)
+}
+
+function disablePurchaseForm() {
+    var purchase_solicitation_id = $("#pledge_request_purchase_solicitation_id").val()
+    $("#pledge_request_purchase_form").attr("disabled", purchase_solicitation_id ? false : true)
 }
 
 $(document).ready(function () {
     setCreditor();
-    setPurchaseSolicitationBalance();
-    setLicitationProcesso();
+    disableLicitationProcess();
+    setModalUrlToLicitationProcess();
+    disablePurchaseSolicitation();
     setModalUrlToPurchaseSolicitation();
-
-    $('#pledge_request_purchase_solicitation_id').on('change', function () {
-        setPurchaseSolicitationBalance();
-    });
+    disablePurchaseForm();
+    setModalUrlToPurchaseForm();
 
     $('#pledge_request_contract_id').on('change', function () {
         setCreditor();
-        setLicitationProcesso();
+        disableLicitationProcess();
+        setModalUrlToLicitationProcess();
+    });
+
+    $('#pledge_request_purchase_process_id').on('change', function () {
+        disablePurchaseSolicitation();
         setModalUrlToPurchaseSolicitation();
     });
 
-    $('form#new_pledge_request').on('keyup', '.quantity', function () {
-        sumTotalItems();
+    $('#pledge_request_purchase_solicitation_id').on('change', function () {
+        disablePurchaseForm();
+        setModalUrlToPurchaseForm();
     });
 
-    $('form#new_pledge_request').on('click', '.remove-items', function () {
-        sumTotalItems();
-    });
-
-    $('#pledge_request_purchase_process_id').on('change', function (event, purchase_process) {
-        if (!purchase_process) {
-            purchase_process = {};
-        }
-
-        setCreditorModalUrl();
-        setContractModalUrl();
-        setItemModalUrl();
-
-        setBudgetAllocationSource(purchase_process.budget_allocations_ids);
-        // setReserveFunds();
-    });
-
-    $('#pledge_request_creditor_id').on('change', function () {
-        setItemModalUrl();
-    });
-
-    $('#pledge_request_reserve_fund_id').on('change', function () {
-        var option = $(this).find('option:selected');
-
-        // $('#pledge_request_reserve_fund_amount').val(option.data('amount'));
-    });
-
-    $('#pledge_request_budget_allocation_id').on('change', function (event, budget_allocation) {
-        if (!budget_allocation) {
-            budget_allocation = {};
-        }
-
-        $('#pledge_request_budget_allocation_balance').val(budget_allocation.balance);
-        $('#pledge_request_descriptor_id').val(budget_allocation.descriptor_id);
-
-        setExpenseNatureModalUrl(budget_allocation.expense_nature_id);
-    });
-
-    $('#items').on('nestedForm:afterAdd', function () {
-        setItemModalUrl();
-    });
-
-    $(document).on('change', 'input:hidden[name*="purchase_process_item_id"]', function (event, item) {
-        var $item_wrapper = $(this).closest('.nested-items');
-
-        if (!item) {
-            item = {};
-        }
-
-        $item_wrapper.find('.additional_information')
-            .val(item.additional_information);
-
-        $item_wrapper.find('.item_number')
-            .val(item.item_number);
-
-        $item_wrapper.find('.lot')
-            .val(item.lot);
-
-        $item_wrapper.find('.unit_price')
-            .val(item.ratification_item_unit_price);
-    });
 });
