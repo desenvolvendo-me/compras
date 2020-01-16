88a9b5e6bffc12bfb8a92b7a4a090ee123ecdffe
Marco Castro <marcodotcastro@gmail.com>
2020-01-16 13:21:44 -0300

    [c] balance_adjustment_item modal

--- app/models/balance_adjustment.rb
+++ app/models/balance_adjustment.rb
@@ -1,9 +1,13 @@
 class BalanceAdjustment < Compras::Model
-  attr_accessible :observation, :licitation_process_id, :contract_id, :purchase_solicitation_id
+  attr_accessible :observation, :licitation_process_id, :contract_id, :purchase_solicitation_id,
+                  :items_attributes
 
   belongs_to :licitation_process
   belongs_to :contract
   belongs_to :purchase_solicitation
+  has_many :items, class_name: 'BalanceAdjustmentItem', dependent: :destroy
+
+  accepts_nested_attributes_for :items, allow_destroy: true
 
   orderize "id DESC"
   filterize
