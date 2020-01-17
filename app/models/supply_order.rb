0c5dbdf3f2049fd4dcc18082f4b538beeb6e445e
Marco Castro <marcodotcastro@gmail.com>
2020-01-17 16:38:27 -0300

    [r]  supply_request

--- app/models/supply_order.rb
+++ app/models/supply_order.rb
@@ -2,22 +2,23 @@ class SupplyOrder < Compras::Model
   include MaterialBalance
   include NumberSupply
 
-  attr_accessible :licitation_process_id, :creditor_id, :authorization_date,
-                  :items_attributes, :invoices_attributes, :year, :pledge_id, :purchase_solicitation_id,
-                  :updatabled, :contract_id, :supply_request_id, :purchase_form_id, :observation
+  attr_accessible :licitation_process_id, :creditor_id, :authorization_date, :year, :observation, :updatabled,
+                  :items_attributes, :invoices_attributes, :supply_requests_attributes,
+                  :pledge_id, :purchase_solicitation_id, :contract_id, :purchase_form_id
 
   belongs_to :contract
   belongs_to :purchase_solicitation
   belongs_to :licitation_process
   belongs_to :creditor
-  belongs_to :supply_request
   belongs_to :purchase_form
 
   has_many :items, class_name: 'SupplyOrderItem', dependent: :destroy
   has_many :invoices, dependent: :destroy
+  has_many :supply_requests, class_name: "SupplyOrderRequests", dependent: :destroy
 
   accepts_nested_attributes_for :items, allow_destroy: true
   accepts_nested_attributes_for :invoices, allow_destroy: true
+  accepts_nested_attributes_for :supply_requests, allow_destroy: true
 
   delegate :modality_number, :modality_humanize, :type_of_removal_humanize,
            to: :licitation_process, allow_nil: true
@@ -28,7 +29,7 @@ class SupplyOrder < Compras::Model
   orderize "id DESC"
   filterize
 
-  before_update :change_status_in_service
+  # before_update :change_status_in_service
 
   def items_quantity_permitted
     message = calc_items_quantity(self.licitation_process, self.purchase_solicitation)
@@ -46,9 +47,11 @@ class SupplyOrder < Compras::Model
   private
 
   def change_status_in_service
-    if self.supply_request
+    if self.supply_request.any?
       self.updatabled? ? status = SupplyRequestStatus::DELIVERED : status = SupplyRequestStatus::IN_SERVICE
-      self.supply_request.update_attribute(:supply_request_status, status)
+      self.supply_requests.each do |supply_order_request|
+        supply_order_request.supply_request.update_attribute(:supply_request_status, status)
+      end
     end
   end
 
