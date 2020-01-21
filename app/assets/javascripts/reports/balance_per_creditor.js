2ed009ebb95779315229c725f56f385ef69d4ec3
Marco Castro <marcodotcastro@gmail.com>
2020-01-21 17:49:53 -0300

    [a] usabilidade

--- app/assets/javascripts/reports/balance_per_creditor.js
+++ app/assets/javascripts/reports/balance_per_creditor.js
@@ -1,5 +1,16 @@
 //= require modal_filter
 
+function setModalUrlToCreditor() {
+    var selector_id = '#current_user_id'
+    var selector_modal = '#balance_per_creditor_report_creditor'
+
+    var id = $(selector_id).val();
+
+    if (id) {
+        setModalUrlToCreditorByPurchasingUnit(selector_id, selector_modal)
+    }
+}
+
 function setModalUrlToLicitationProcess() {
     var selector_id = '#balance_per_creditor_report_creditor_id'
     var selector_modal = '#balance_per_creditor_report_licitation_process'
@@ -8,6 +19,11 @@ function setModalUrlToLicitationProcess() {
     if (id) {
         setModalUrlToLicitationProcessByCreditor(selector_id, selector_modal)
     }
+}
+
+function disableLicitationProcess() {
+    var creditor_id = $("#balance_per_creditor_report_creditor_id").val()
+    $("#balance_per_creditor_report_licitation_process").attr("disabled", creditor_id ? false : true)
 }
 
 function setModalUrlToDepartment() {
@@ -20,27 +36,29 @@ function setModalUrlToDepartment() {
     }
 }
 
-function disableLicitationProcess() {
-    var creditor_id = $("#balance_per_creditor_report_creditor_id").val()
-    $("#balance_per_creditor_report_licitation_process").attr("disabled", creditor_id ? false : true)
-}
-
 function disableDepartment() {
     var purchase_process_id = $("#balance_per_creditor_report_creditor_id").val()
     $("#balance_per_creditor_report_department").attr("disabled", purchase_process_id ? false : true)
 }
 
 $(document).ready(function () {
-    disableLicitationProcess();
-    disableDepartment();
+    setModalUrlToCreditor();
 
+    disableLicitationProcess();
     setModalUrlToLicitationProcess();
+
+    disableDepartment();
     setModalUrlToDepartment();
+
     $('#balance_per_creditor_report_creditor_id').on('change', function () {
         disableLicitationProcess();
-        disableDepartment();
 
         setModalUrlToLicitationProcess();
+    });
+
+    $('#balance_per_creditor_report_licitation_process_id').on('change', function () {
+        disableDepartment();
+
         setModalUrlToDepartment();
     });
 
