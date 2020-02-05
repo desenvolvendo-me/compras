5baccaca5fa79f36339847fccc896857c98a425e
Marco Castro <marcodotcastro@gmail.com>
2020-02-05 11:00:26 -0300

    [a] adicionado status na lista

--- app/enumerations/supply_order_status.rb
+++ app/enumerations/supply_order_status.rb
@@ -1,4 +1,3 @@
 class SupplyOrderStatus < EnumerateIt::Base
-  associate_values :sent, :partially_answered, :fully_serviced, :pending_delivery, :partially_answered
-
+  associate_values :sent, :partially_answered, :fully_serviced, :pending_delivery
 end
