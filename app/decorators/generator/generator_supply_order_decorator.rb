afec3ca7605b823b7e637d4cdf8a7d53cdb40e37
Marco Castro <marcodotcastro@gmail.com>
2020-02-10 11:57:08 -0300

    [a] adicionado status na view

--- app/decorators/generator/generator_supply_order_decorator.rb
+++ app/decorators/generator/generator_supply_order_decorator.rb
@@ -3,5 +3,5 @@ class Generator::GeneratorSupplyOrderDecorator
   include Decore::Proxy
   include Decore::Header
 
-  attr_header :control_code, :user
+  attr_header :control_code, :user, :status
 end
