3c25141eaadc51addbc2e00a3ca15f0a38fdd5c0
andbri <andbri321@gmail.com>
2020-03-16 18:23:37 -0300

    [u] supply_order_managements_controller, dashboard adicionado

--- app/controllers/supply_order_managements_controller.rb
+++ app/controllers/supply_order_managements_controller.rb
@@ -1,5 +1,6 @@
 class SupplyOrderManagementsController < ApplicationController
   def index
-
+    @groups = SupplyRequestAttendance.select(:service_status).
+            group(:service_status).count
   end
 end
