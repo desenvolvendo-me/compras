ab599390a5e7faac76f1becc2ce5a5a7af5c30e3
andbri <andbri321@gmail.com>
2020-03-26 14:50:21 -0300

    [u] model supply_order_spec

--- spec/models/supply_order_spec.rb
+++ spec/models/supply_order_spec.rb
@@ -1,14 +1,20 @@
+require 'minitest/autorun'
 require 'model_helper'
 require 'app/models/supply_order'
 require 'app/models/supply_order_item'
 
 describe SupplyOrder do
-  it { should belong_to :creditor }
-  it { should belong_to :licitation_process }
 
-  it { should have_many(:items).class_name('SupplyOrderItem').dependent(:destroy) }
+  context 'relationships' do
+    it { should belong_to :creditor }
+    it { should belong_to :licitation_process }
 
-  it { should validate_presence_of :authorization_date }
-  it { should validate_presence_of :creditor }
-  it { should validate_presence_of :licitation_process }
+    it { should have_many(:items).class_name('SupplyOrderItem').dependent(:destroy) }
+  end
+
+  context 'validations' do
+    it { should validate_presence_of :authorization_date }
+    it { should validate_presence_of :licitation_process }
+  end
+
 end
