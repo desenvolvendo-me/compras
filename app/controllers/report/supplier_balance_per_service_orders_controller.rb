2d4ede6d9c0bc8e4abe5feb97cc812675c460ff6
andbri <andbri321@gmail.com>
2020-04-06 12:23:40 -0300

    [u] report_supplier_balance_per_service_order, busca melhorada, mensagem adicionado caso a busca não retorne registros

--- app/controllers/report/supplier_balance_per_service_orders_controller.rb
+++ app/controllers/report/supplier_balance_per_service_orders_controller.rb
@@ -20,8 +20,16 @@ class Report::SupplierBalancePerServiceOrdersController < Report::BaseController
     @creditor = supplier_balance_per_service_order_report_params["creditor_id"]
     @licitation_process = supplier_balance_per_service_order_report_params["licitation_process_id"]
     @supply_order = supplier_balance_per_service_order_report_params["supply_order_id"]
-    @contracts = Contract.joins(:creditors).where(compras_contracts_unico_creditors:{creditor_id:@creditor})
+    @contracts = get_contracts @creditor
     @contracts = @contracts.where(licitation_process_id:@licitation_process) unless @licitation_process.nil?
+  end
+
+  def get_contracts(creditor)
+    if creditor.nil?
+      Contract.joins(:creditors).where(compras_contracts_unico_creditors:{creditor_id:creditor})
+    else
+      Contract.joins(:creditors)
+    end
   end
 
   def supplier_balance_per_service_order_report_params
@@ -31,6 +39,8 @@ class Report::SupplierBalancePerServiceOrdersController < Report::BaseController
 
   def normalize_attributes(params)
     params.delete(:supply_order_id) if params[:supply_order_id].blank?
+    params.delete(:licitation_process_id) if params[:licitation_process_id].blank?
     params
   end
+
 end
