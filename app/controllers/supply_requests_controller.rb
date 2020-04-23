7c1795e8f638378fe46d21a249b6d5765ef55f16
andbri <andbri321@gmail.com>
2020-04-23 20:13:00 -0300

    app/views/supply_requests, btn adicionados

--- app/controllers/supply_requests_controller.rb
+++ app/controllers/supply_requests_controller.rb
@@ -12,6 +12,7 @@ class SupplyRequestsController < CrudController
   def index
     @supply_requests = filters(collection) if params[:suplly_requests].nil?
     @supply_requests = SupplyRequest.where("id in (?)",params[:suplly_requests]) unless params[:suplly_requests].nil?
+    @te='sad'
     @gestor = gestor?
   end
 
@@ -23,7 +24,7 @@ class SupplyRequestsController < CrudController
   protected
 
   def gestor?
-    UserPurchasingUnit.where(user_id:current_user).count == 0 ? false:true
+    UserPurchasingUnit.where(user_id:current_user.id).count == 0 ? false:true
   end
 
   def filters(collection)
