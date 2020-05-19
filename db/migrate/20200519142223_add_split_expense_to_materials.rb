7c8fb2c15cdf8cc9b85d01a84844c7a470c613f9
andbri <andbri321@gmail.com>
2020-05-19 12:08:21 -0300

    [c] scaffold material, explit expense  adicionado

---
+++ db/migrate/20200519142223_add_split_expense_to_materials.rb
@@ -0,0 +1,8 @@
+class AddSplitExpenseToMaterials < ActiveRecord::Migration
+  def change
+    add_column :unico_materials, :split_expense_id, :integer
+    add_index :unico_materials, :split_expense_id
+    add_foreign_key :unico_materials, :compras_split_expenses,
+                    column: :split_expense_id
+  end
+end
