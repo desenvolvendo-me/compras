473ac6ed28ee50340b0525cba922d24def4cd7be
andbri <andbri321@gmail.com>
2020-05-19 17:18:14 -0300

    [c] scaffold report_creditor_material

---
+++ app/controllers/report/creditor_materials_controller.rb
@@ -0,0 +1,16 @@
+class Report::CreditorMaterialsController < Report::BaseController
+  report_class CreditorMaterialReport
+
+  def show
+    @report = report_instance
+    @report.supply_request_id = params[:supply_request_id]
+    @sup_req = SupplyRequest.find(params[:supply_request_id])
+    @report.creditor = @sup_req.contract.creditors.last if @sup_req.contract && @sup_req.contract.creditors.last
+
+    if @report.valid?
+      render layout: 'report'
+    else
+      render :new
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -977,6 +977,7 @@ Compras::Application.routes.draw do
 
     match 'supply_orders/:supply_order_id' => 'supply_orders#show', as: :supply_orders
     match 'supply_requests/:supply_request_id' => 'supply_requests#show', as: :supply_requests
+    match 'creditor_materials/:supply_request_id' => 'creditor_materials#show', as: :creditor_materials
 
   end
 
