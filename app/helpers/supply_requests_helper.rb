d564e3a08034578bf248fdd9f29490e9babbd1c6
Marco Castro <marcodotcastro@gmail.com>
2020-05-27 15:57:25 -0300

    fix(supply): user nil

--- app/helpers/supply_requests_helper.rb
+++ app/helpers/supply_requests_helper.rb
@@ -3,7 +3,7 @@ module SupplyRequestsHelper
   def disabled_material(object)
     disable = false
     disable = true if object.new_record?
-    disable = true unless current_user.login.eql?(object.user.login)
+    disable = true unless current_user.login.eql?(object.user.try(:login))
     disable
   end
 
