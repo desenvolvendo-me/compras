c4b7bad0fc7a138a3156880f80efdf4398f13abb
Wallison <wallisongoncaves105ibc@gmail.com>
2020-06-12 09:02:05 -0400

    correção de migrations

--- db/migrate/20200610144822_add_secretary_to_order_supply.rb
+++
@@ -1,9 +0,0 @@
-class AddSecretaryToOrderSupply < ActiveRecord::Migration
-  def change
-    add_column :compras_supply_orders, :secretary_id, :integer
-
-    add_index :compras_supply_orders, :secretary_id
-    add_foreign_key :compras_supply_orders, :compras_secretaries,
-                    column: :secretary_id
-  end
-end
--- db/migrate/20200610151717_add_budgetary_value_to_supply_orders.rb
+++
@@ -1,5 +0,0 @@
-class AddBudgetaryValueToSupplyOrders < ActiveRecord::Migration
-  def change
-    add_column :compras_supply_orders, :budgetary_value, :decimal, precision: 15, scale: 2
-  end
-end
