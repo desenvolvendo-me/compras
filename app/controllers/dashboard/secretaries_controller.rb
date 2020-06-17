67a9468aa8dc3562d0dec744c6bfc696fd25ff3f
Wallison <wallisongoncaves105ibc@gmail.com>
2020-06-17 14:27:33 -0400

    adicionado pesquisa de pedidos para aprovação

--- app/controllers/dashboard/secretaries_controller.rb
+++ app/controllers/dashboard/secretaries_controller.rb
@@ -4,12 +4,19 @@ class Dashboard::SecretariesController < CrudController
 
   def index
     @contracts = Contract.by_days_finish(30).page(params[:page]).per(5)
-
     @contracts_periods = Contract.count_contracts_finishing
+    employee = current_user.authenticable.id
+    @approval_requests = SupplyRequest.joins{ supply_request_attendances.outer }.joins { department.secretary.secretary_settings }
+                           .where{ supply_request_attendances.id.eq(nil) }.where { compras_secretary_settings.employee_id.eq(employee) }.page(params[:page_approval]).per(5)
   end
 
   def contracts
     @contracts = apply_scopes(Contract).page(params[:page]).per(5)
+  end
+
+  def approval_requests
+    @approval_requests = SupplyRequest.joins{ supply_request_attendances.outer }.joins { department.secretary.secretary_settings }
+                             .where{ supply_request_attendances.id.eq(nil) }.where { compras_secretary_settings.employee_id.eq(current_user.authenticable.id) }.page(params[:page_approval]).per(5)
   end
 
   private
