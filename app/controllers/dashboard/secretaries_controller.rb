7cdfb4fd5b2a3020509be81b544287c601e97a17
Wallison <wallisongoncaves105ibc@gmail.com>
2020-06-17 18:38:31 -0400

    correção paginação

--- app/controllers/dashboard/secretaries_controller.rb
+++ app/controllers/dashboard/secretaries_controller.rb
@@ -15,8 +15,9 @@ class Dashboard::SecretariesController < CrudController
   end
 
   def approval_requests
+    employee = current_user.authenticable.id
     @approval_requests = SupplyRequest.joins{ supply_request_attendances.outer }.joins { department.secretary.secretary_settings }
-                             .where{ supply_request_attendances.id.eq(nil) }.where { compras_secretary_settings.employee_id.eq(current_user.authenticable.id) }.page(params[:page_approval]).per(5)
+                             .where{ supply_request_attendances.id.eq(nil) }.where { compras_secretary_settings.employee_id.eq(employee) }.page(params[:page_approval]).per(5)
   end
 
   private
