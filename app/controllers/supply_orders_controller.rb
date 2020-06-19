7edf542969b39b4267e365cd1025ebcfde40c5e4
Wallison <wallisongoncaves105ibc@gmail.com>
2020-06-19 19:13:25 -0400

    preenchimento de dados em ordem de fornecimento

--- app/controllers/supply_orders_controller.rb
+++ app/controllers/supply_orders_controller.rb
@@ -2,6 +2,16 @@ class SupplyOrdersController < CrudController
   has_scope :by_purchasing_unit
 
   def new
-    super
+    if params[:supply_order].present?
+      object = build_resource
+      object.licitation_process_id = params[:supply_order][:licitation_process_id]
+      object.contract_id = params[:supply_order][:contract_id]
+      object.purchase_solicitation_id = params[:supply_order][:purchase_solicitation_id]
+      params[:supply_order][:item_ids]&.each_pair do |index, item|
+        object.items.new(material_id: item)
+      end
+    else
+      super
+    end
   end
 end
