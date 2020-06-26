85a30ca18690af4dd78abcdc3247c636b2f530dd
wallison <wallisongoncalves1105ibc@gmail.com>
2020-06-26 08:59:52 -0400

    Add helper para pegar valor total do pedido de fornecimento

---
+++ app/helpers/report/contract_per_resource_source_helper.rb
@@ -0,0 +1,16 @@
+module Report::ContractPerResourceSourceHelper
+  def self.sum_value_items_supply_order(supply_order)
+    value = 0
+
+    supply_order.items.each do |item|
+      licitation_process_items = supply_order.licitation_process.items.where(material_id: item.material_id)
+
+      item_value = licitation_process_items.first.unit_price if licitation_process_items.any?
+      item_total = supply_order.items.where(material_id: item.material_id).sum(:quantity)
+
+      value += item_value.to_f * item_total.to_f
+    end
+
+    value
+  end
+end
