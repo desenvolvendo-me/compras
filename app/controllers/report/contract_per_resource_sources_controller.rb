3e6ab18ea2446cac6b53a1ee2aacfb97623a8ee9
wallison <wallisongoncalves1105ibc@gmail.com>
2020-06-29 18:08:12 -0400

    fix erro 404

--- app/controllers/report/contract_per_resource_sources_controller.rb
+++ app/controllers/report/contract_per_resource_sources_controller.rb
@@ -3,13 +3,13 @@ class Report::ContractPerResourceSourcesController < Report::BaseController
   report_class ContractPerResourceSourceReport, :repository => ContractPerResourceSourceSearcher
 
   def show
-    @contract = Contract.find(contract_per_resource_source_params["contract_id"])
-    @resource_sources = ResourceSource.joins(expenses:[:contract_financials])
-                           .where(compras_contract_financials:{contract_id:contract_per_resource_source_params["contract_id"]}).uniq(:id)
-
     @report = report_instance
 
     if @report.valid?
+      @contract = Contract.find(contract_per_resource_source_params["contract_id"])
+      @resource_sources = ResourceSource.joins(expenses:[:contract_financials])
+                              .where(compras_contract_financials:{contract_id:contract_per_resource_source_params["contract_id"]}).uniq(:id)
+
       render layout: 'report'
     else
       render :new
--- app/reports/contract_per_resource_source_report.rb
+++ app/reports/contract_per_resource_source_report.rb
@@ -2,5 +2,5 @@ class ContractPerResourceSourceReport < Report
 
   attr_accessor :contract_id, :contract, :resource_source_id, :resource_source
 
-
+  validates :contract_id, presence: true
 end
