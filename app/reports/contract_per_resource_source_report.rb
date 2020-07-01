e562f62fac9716b02b4b544f33ba6b7cca5d0ee0
wallison <wallisongoncalves1105ibc@gmail.com>
2020-07-01 13:42:26 -0400

    refactor busca

--- app/reports/contract_per_resource_source_report.rb
+++ app/reports/contract_per_resource_source_report.rb
@@ -1,6 +1,24 @@
 class ContractPerResourceSourceReport < Report
-
   attr_accessor :contract_id, :contract, :resource_source_id, :resource_source
 
-  validates :contract_id, presence: true
+  def contracts
+    if contract_id.present?
+      Contract.find(contract_id)
+    else
+      Contract.all
+    end
+  end
+
+  def resource_sources param
+    if contract_id.present?
+      ResourceSource.by_contract(contract_id)
+          .by_id(resource_source_id).uniq(:id)
+    else
+      if resource_source_id.present?
+        ResourceSource.by_id(resource_source_id)
+      else
+        ResourceSource.joins{ expenses.contract_financials }.where{ expenses.contract_financials.contract_id.eq(param) }.uniq(:id)
+      end
+    end
+  end
 end
