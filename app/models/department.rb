c1ca26b2b1c772577d945bb24e240b48a25b9b48
wallison <wallisongoncalves1105ibc@gmail.com>
2020-07-01 19:26:43 -0400

    adicionado filtros

--- app/models/department.rb
+++ app/models/department.rb
@@ -1,7 +1,7 @@
 class Department < Compras::Model
   attr_accessible :description, :purchasing_unit_id,
                   :department_people_attributes,:secretary_id
-  attr_modal :description
+  attr_modal :description, :secretary_id, :purchasing_unit_id
 
   belongs_to :purchasing_unit
   belongs_to :secretary
