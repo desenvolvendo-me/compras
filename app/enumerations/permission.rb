eb77aa801ec419622bb8fb833cd2e34b54acff3e
Marco Castro <marcodotcastro@gmail.com>
2020-07-02 10:18:43 -0300

    feat(profile): criado estrutura

--- app/enumerations/permission.rb
+++ app/enumerations/permission.rb
@@ -1,3 +1,12 @@
 class Permission < EnumerateIt::Base
   associate_values :deny, :read, :modify, :access
+
+  def self.ordered
+    {
+        I18n.t('enumerations.permission.deny') => :deny,
+        I18n.t('enumerations.permission.read') => :read,
+        I18n.t('enumerations.permission.modify') => :modify,
+        I18n.t('enumerations.permission.access') => :access
+    }
+  end
 end
