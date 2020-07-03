23258cc8f396dc79f72587747230975a72c70ed9
wallison <wallisongoncalves1105ibc@gmail.com>
2020-07-03 15:11:43 -0400

    helper para selecionar o tipo da pessoa

--- app/helpers/people_helper.rb
+++ app/helpers/people_helper.rb
@@ -36,4 +36,36 @@ module PeopleHelper
   def object_name(form_builder)
     form_builder.object_name
   end
+
+  def page_title_plural
+    if params[:by_legal_people] === 'true'
+      "Empresas"
+    elsif params[:by_physical_people] === 'true'
+      "Pessoas"
+    end
+  end
+
+  def page_title_singular
+    if params[:company] === 'true'
+      "Empresa"
+    elsif params[:people] === 'true'
+      "Pessoa"
+    end
+  end
+
+  def edit_link resource
+    if params[:by_legal_people] === 'true'
+      edit_resource_path(resource, company: true )
+    elsif params[:by_physical_people] === 'true'
+      edit_resource_path(resource, people: true )
+    end
+  end
+
+  def people_type
+    if params[:by_legal_people] === 'true'
+      { company: true }
+    elsif params[:by_physical_people] === 'true'
+      { people: true }
+    end
+  end
 end
