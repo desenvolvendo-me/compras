d659966e0c063a7186f6d964383799d9c3e14b44
wallison <wallisongoncalves1105ibc@gmail.com>
2020-07-03 17:18:18 -0400

    adicionado redirect após salvar

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -10,6 +10,32 @@ class PeopleController < CrudController
     super
   end
 
+  def create
+    create! do |success, failure|
+      if resource.personable_type == PersonableType::INDIVIDUAL
+        success.html { redirect_to physical_peoples_path }
+        @person_type = PersonableType::INDIVIDUAL
+      else
+        success.html { redirect_to legal_peoples_path }
+        @person_type = PersonableType::COMPANY
+      end
+      failure.html { render 'new' }
+    end
+  end
+
+  def update
+    update! do |success, failure|
+      if resource.personable_type == PersonableType::INDIVIDUAL
+        success.html { redirect_to physical_peoples_path }
+        @person_type = PersonableType::INDIVIDUAL
+      else
+        success.html { redirect_to legal_peoples_path }
+        @person_type = PersonableType::COMPANY
+      end
+      failure.html { render 'edit' }
+    end
+  end
+
   def new
     person_type = Individual.new if params[:by_physical_people]
     person_type = Company.new if params[:by_legal_people]
@@ -24,7 +50,7 @@ class PeopleController < CrudController
       @person_type = PersonableType::INDIVIDUAL
     end
     if params[:filter] && params[:filter][:personable_type] == PersonableType::COMPANY ||  params[:by_legal_people] || params[:company]
-    @person_type = PersonableType::COMPANY
+      @person_type = PersonableType::COMPANY
     end
   end
 
