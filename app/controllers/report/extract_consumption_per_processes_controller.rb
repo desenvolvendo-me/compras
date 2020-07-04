e5688a9de254f3c3f7443957594abb3330699b0c
wallison <wallisongoncalves1105ibc@gmail.com>
2020-07-03 22:34:16 -0400

    adicionado busca por solicitação de compra

--- app/controllers/report/extract_consumption_per_processes_controller.rb
+++ app/controllers/report/extract_consumption_per_processes_controller.rb
@@ -19,11 +19,13 @@ class Report::ExtractConsumptionPerProcessesController < Report::BaseController
   private
 
   def get_extract_consumption_per_process
+    @purchase_solicitation = extract_consumption_per_process_report_params['purchase_solicitation_id']
     @licitation_process = extract_consumption_per_process_report_params["licitation_process_id"]
     @contract = extract_consumption_per_process_report_params["contract_id"]
     @creditor = extract_consumption_per_process_report_params["creditor_id"]
 
-    @licitation_processes = LicitationProcess.joins(contracts: [:creditors])
+    @licitation_processes = LicitationProcess.joins(contracts: [:creditors]).joins(:purchase_solicitations)
+    @licitation_processes = @licitation_processes.where('compras_list_purchase_solicitation.purchase_solicitation_id = ?', @purchase_solicitation) if @purchase_solicitation.present?
     @licitation_processes = @licitation_processes.where(id: @licitation_process) if @licitation_process.present?
     @licitation_processes = @licitation_processes.where("compras_contracts.id = #{@contract}") if @contract.present?
     @licitation_processes = @licitation_processes.where("compras_contracts_unico_creditors.creditor_id = #{@creditor}") if @creditor.present?
