9fd65eca58fae43f7e943992b5d81a4c79d07b6d
wallison <wallisongoncalves1105ibc@gmail.com>
2020-07-05 16:08:09 -0400

    refactor status Pedido de Fornecimento
    alterado opções do enum

--- app/enumerations/supply_request_status.rb
+++ app/enumerations/supply_request_status.rb
@@ -1,3 +1,3 @@
 class SupplyRequestStatus < EnumerateIt::Base
-  associate_values :sent,:in_service,:delivered,:pending,:financial_analysis
+  associate_values :sent,:in_service,:delivered,:pending,:financial_analysis,:secretary_analysis, :finished
 end
