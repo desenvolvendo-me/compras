ed92749cb0fe050bf9860570cd47c385f2ae26ee
wallison <wallisongoncalves1105ibc@gmail.com>
2020-07-06 20:21:18 -0400

    enumerations

--- app/models/auction.rb
+++ app/models/auction.rb
@@ -1,4 +1,4 @@
-class Agreement < Compras::Model
+class Auction < Compras::Model
   attr_modal :auction_type,:licitation_number,:process_number,:year,:dispute_type,
              :judment_form,:covid_law,:purchase_value,:items_quantity,
              :object,:object_management,:employee_id
@@ -6,6 +6,8 @@ class Agreement < Compras::Model
   belongs_to :employee
 
   has_enumeration_for :auction_type, :with => AuctionType
+  has_enumeration_for :dispute_type, :with => AuctionDisputeType
+  has_enumeration_for :judment_form, :with => AuctionJudmentForm
 
   orderize :description
   filterize
