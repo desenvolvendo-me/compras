0876d2b143af13cc6536ba8ed50250bf6386803c
wallison <wallisongoncalves1105ibc@gmail.com>
2020-07-07 15:12:31 -0400

    controller

--- app/controllers/auction/auctions_controller.rb
+++ app/controllers/auction/auctions_controller.rb
@@ -1,3 +1,20 @@
 class Auction::AuctionsController < Auction::BaseController
+  def create
+    create! do |success, failure|
+      success.html { redirect_to collection_path }
+      failure.html { render :new }
 
+      success.js { render content_type: 'text/json' }
+      failure.js { render :form_errors, content_type: 'text/json', status: :unprocessable_entity }
+    end
+  end
+  def update
+    update! do |success, failure|
+      success.html { redirect_to collection_path }
+      failure.html { render :edit }
+
+      success.js { render content_type: 'text/json' }
+      failure.js { render :form_errors, content_type: 'text/json', status: :unprocessable_entity }
+    end
+  end
 end
