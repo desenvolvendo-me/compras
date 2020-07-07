20671e35c010afccc9e02bbfda306cbdbac31fe9
wallison <wallisongoncalves1105ibc@gmail.com>
2020-07-07 15:13:50 -0400

    handler retorno assincrono pra form

--- app/assets/javascripts/views/auctions/auction_form.js
+++ app/assets/javascripts/views/auctions/auction_form.js
@@ -2,19 +2,38 @@
   var Views = Compras.Views;
 
   Views.AuctionForm = Views.RemoteForm.extend({
+
     initialize: function () {
       this.setElement($('form.auction'));
-debugger
+
       this.eventName = 'auction';
+
     },
 
     setup: function () {
       if ($('form.auction').length > 0) {
         this.$submit = this.$("input[id$='auction_submit']");
-debugger
         this._fixMozillaScroll();
       }
     },
 
+    onSuccess: function(event, data) {
+      debugger
+      window.location.replace(Routes.auction_auctions)
+    },
+
+    onComplete: function () {
+
+    },
+
+    onError: function (event, data) {
+      this.setSaveLabelSumbmit();
+
+      var errors = $.parseJSON(data.responseText).errors;
+
+      this.clearErrors();
+
+      this.addErrors(errors);
+    },
   });
 })();
