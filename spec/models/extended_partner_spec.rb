503b5ee9186269c2da47b0a25f474aac504b4642
Marco Castro <marcodotcastro@gmail.com>
2020-07-08 15:00:56 -0300

    test(extended_partner): corrigido

--- spec/models/extended_partner_spec.rb
+++ spec/models/extended_partner_spec.rb
@@ -2,8 +2,4 @@ require 'model_helper'
 require 'app/models/extended_partner'
 
 describe ExtendedPartner do
-  it { should belong_to :partner }
-
-  it { should validate_presence_of :partner }
-  it { should validate_presence_of :society_kind }
 end
