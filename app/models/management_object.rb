e03bc0add2eb19b1f49f36e20b27f629655237cf
wallison <wallisongoncalves1105ibc@gmail.com>
2020-07-07 23:02:16 -0400

    atributos do modal

--- app/models/management_object.rb
+++ app/models/management_object.rb
@@ -1,6 +1,8 @@
 class ManagementObject < Compras::Model
   attr_accessible :description, :status, :object
 
+  attr_modal :description, :status
+  has_enumeration_for :status, with: ObjectStatus
 
   validates :description, :status, presence: true
 
