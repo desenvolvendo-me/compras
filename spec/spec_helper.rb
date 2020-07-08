0546c0e01776313265f1b42f6ead42538563cb95
Marco Castro <marcodotcastro@gmail.com>
2020-07-08 16:52:21 -0300

    test: pendencia do customer

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -64,6 +64,7 @@ RSpec.configure do |config|
     # Forces all threads to share the same connection. This works on
     # Capybara because it starts the web server in a thread.
     ActiveRecord::Base.shared_connection = ActiveRecord::Base.connection
+    Customer.create!(:name => "Test", :domain => "test.host", :database => Customer.connection_config)
   end
 
   config.before(:each, type: :feature) do
