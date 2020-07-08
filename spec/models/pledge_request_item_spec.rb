c4485d041eb7ae756f77150432a16eb22ad4d5cb
Marco Castro <marcodotcastro@gmail.com>
2020-07-08 15:07:17 -0300

    test(pledge_request_item): corrigido

--- spec/models/pledge_request_item_spec.rb
+++ spec/models/pledge_request_item_spec.rb
@@ -3,18 +3,8 @@ require 'app/models/accounting_cost_center'
 require 'app/models/pledge_request_item'
 
 describe PledgeRequestItem do
+  it { should belong_to :purchase_solicitation }
+  it { should belong_to :material }
   it { should belong_to :pledge_request }
   it { should belong_to :purchase_process_item }
-
-  describe 'delegations' do
-    it { should delegate(:lot).to(:purchase_process_item).allowing_nil(true).prefix(true) }
-    it { should delegate(:item_number).to(:purchase_process_item).allowing_nil(true).prefix(true) }
-    it { should delegate(:quantity).to(:purchase_process_item).allowing_nil(true).prefix(true) }
-    it { should delegate(:additional_information).to(:purchase_process_item).allowing_nil(true).prefix(true) }
-  end
-
-  describe 'validataions' do
-    it { should validate_presence_of :purchase_process_item }
-    it { should validate_presence_of :quantity }
-  end
 end
