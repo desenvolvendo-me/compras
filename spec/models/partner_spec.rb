1f36a6aa9c523e68686deb2e524953852f17086a
Marco Castro <marcodotcastro@gmail.com>
2020-07-08 15:04:36 -0300

    test(partner): corrigido

--- spec/models/partner_spec.rb
+++ spec/models/partner_spec.rb
@@ -4,11 +4,5 @@ require 'app/models/extended_partner'
 require 'app/models/partner'
 
 describe Partner do
-  it { should have_one(:extended_partner).dependent(:destroy) }
-
-  it { should delegate(:society_kind).to(:extended_partner).allowing_nil(true) }
-  it { should delegate(:society_kind_humanize).to(:extended_partner).allowing_nil(true) }
-  it { should delegate(:society_kind_legal_representative?).to(:extended_partner).allowing_nil(true) }
-  it { should delegate(:society_kind_other_company_shareholder_members?).to(:extended_partner).allowing_nil(true) }
   it { should delegate(:cpf).to(:person).allowing_nil(true) }
 end
