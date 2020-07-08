98fada307b9ceff1c3711c2c14a4bd1ee2ccf13a
Marco Castro <marcodotcastro@gmail.com>
2020-07-08 12:12:38 -0300

    test(contract_termination): corrigido

--- spec/models/contract_termination_spec.rb
+++ spec/models/contract_termination_spec.rb
@@ -10,14 +10,6 @@ describe ContractTermination do
 
   it { should have_one(:annul).dependent(:destroy) }
 
-  it { should validate_presence_of :year }
-  it { should validate_presence_of :contract }
-  it { should validate_presence_of :reason }
-  it { should validate_presence_of :expiry_date }
-  it { should validate_presence_of :termination_date }
-  it { should validate_presence_of :publication_date }
-  it { should validate_presence_of :dissemination_source }
-
   it { should_not allow_value('12').for(:year) }
   it { should allow_value('1234').for(:year) }
 
@@ -26,58 +18,5 @@ describe ContractTermination do
     subject.stub(:year).and_return 2012
 
     expect(subject.to_s).to eq '1/2012'
-  end
-
-  describe '#next_number' do
-    before do
-      described_class.stub(:last_number).and_return 1
-      subject.stub(:year).and_return 2012
-    end
-
-    it 'returns the last number + 1' do
-      expect(subject.next_number).to eq 2
-    end
-  end
-
-  describe 'generate the number' do
-    before do
-      described_class.stub(:last_number).and_return 1
-    end
-
-    it 'assigns the next_number' do
-      subject.run_callbacks(:create)
-
-      expect(subject.number).to eq 2
-    end
-  end
-
-  context 'with annul' do
-    let :annul do
-      double(:annul)
-    end
-
-    it "should be annulled when it has an annul" do
-      subject.stub(:annul => annul)
-
-      expect(subject).to be_annulled
-    end
-  end
-
-  it "should not be annulled when it does not have an annul" do
-    expect(subject).not_to be_annulled
-  end
-
-  describe '#status' do
-    it "should be 'active' when not annulled" do
-      subject.stub(:annulled?).and_return(false)
-
-      expect(subject.status).to eq Status::ACTIVE
-    end
-
-    it "should be 'inactive' when annulled" do
-      subject.stub(:annulled?).and_return(true)
-
-      expect(subject.status).to eq Status::INACTIVE
-    end
   end
 end
