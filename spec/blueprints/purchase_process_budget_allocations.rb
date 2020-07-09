e9b3a0456848a0a1da28a0da140d2271f3ca9e01
Marco Castro <marcodotcastro@gmail.com>
2020-07-09 15:54:05 -0300

    test(integration): descomentado PurchaseProcessBudgetAllocation

--- spec/blueprints/purchase_process_budget_allocations.rb
+++ spec/blueprints/purchase_process_budget_allocations.rb
@@ -1,14 +1,14 @@
-# PurchaseProcessBudgetAllocation.blueprint(:alocacao) do
-#   budget_allocation_id { 1 }
-#   value { 20.0 }
-# end
-#
-# PurchaseProcessBudgetAllocation.blueprint(:alocacao_com_itens) do
-#   budget_allocation_id { 1 }
-#   value { 20.0 }
-# end
-#
-# PurchaseProcessBudgetAllocation.blueprint(:alocacao_com_2_itens) do
-#   budget_allocation_id { 1 }
-#   value { 30.0 }
-# end
+PurchaseProcessBudgetAllocation.blueprint(:alocacao) do
+  budget_allocation_id { 1 }
+  value { 20.0 }
+end
+
+PurchaseProcessBudgetAllocation.blueprint(:alocacao_com_itens) do
+  budget_allocation_id { 1 }
+  value { 20.0 }
+end
+
+PurchaseProcessBudgetAllocation.blueprint(:alocacao_com_2_itens) do
+  budget_allocation_id { 1 }
+  value { 30.0 }
+end
