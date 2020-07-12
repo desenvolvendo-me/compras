26d2ed990324fe1f0adeed6b301c2f030cf34891
wallison <wallisongoncalves1105ibc@gmail.com>
2020-07-12 18:07:53 -0400

    fix: parametro faltando

--- app/reports/creditor_material_report.rb
+++ app/reports/creditor_material_report.rb
@@ -4,7 +4,7 @@ class CreditorMaterialReport < Report
 
   def materials
     supply_request = SupplyRequest.find(supply_request_id)
-    Material.material_of_supply_request([supply_request.licitation_process_id, supply_request.contract_id]).order(:description)
+    Material.material_of_supply_request([supply_request.licitation_process_id, supply_request.contract_id, supply_request.creditor.id]).order(:description)
   end
 
 end
