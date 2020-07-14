cd02499216d63736eea916113a193d1b0af4fedd
Marco Castro <marcodotcastro@gmail.com>
2020-07-14 15:35:35 -0300

    build(unico): corrigido migration

---
+++ config/initializers/active_record.rb
@@ -0,0 +1,10 @@
+require 'shellwords'
+
+module ActiveRecord
+  module Tasks # :nodoc:
+    class PostgreSQLDatabaseTasks # :nodoc:
+      def structure_dump(filename)
+      end
+    end
+  end
+end
