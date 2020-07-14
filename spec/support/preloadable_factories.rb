1c876ccfc92d1f41dcc99d8579ca05d231e70eca
Marco Castro <marcodotcastro@gmail.com>
2020-07-14 16:34:00 -0300

    build(unico): removido factory com erro

--- spec/support/preloadable_factories.rb
+++ spec/support/preloadable_factories.rb
@@ -28,22 +28,12 @@ FactoryGirl.preload do
   factory(:solar_da_serra)    { create :land_subdivision }
   factory(:horizonte_a_vista) { create :land_subdivision, name: 'Horizonte a Vista' }
 
-  factory(:itau)      { create :bank }
-  factory(:santander) { create :bank, name: 'Santander', code: 33, acronym: 'ST' }
-
   factory(:software)          { create(:material_class, :imported => true) }
   factory(:arames)            { create(:material_class, masked_number: "02.44.65.430.000", description: "Arames", details: "Arames de aço e ferro", has_children: true) }
   factory(:comp_eletricos)    { create(:material_class, masked_number: "03.05.33.000.00000", description: "Componentes elétricos", details: "Componentes elétricos", has_children: true) }
 
   factory(:rua)     { create(:street_type) }
   factory(:avenida) { create(:street_type, acronym: 'AVE', name: 'Avenida') }
-
-  factory(:itau)      { create(:agency, bank: banks(:itau)) }
-  factory(:santander) { create(:agency, digit:'5', email: 'agency_email@santander.com.br', fax:'(11) 9090-7070', name: 'Agência Santander', number: '10099', phone: '(11) 7070-7070', bank: banks(:santander)) }
-
-  factory(:leve)  { create :risk_degree }
-  factory(:medio) { create :risk_degree, level: '2', name: 'Médio'}
-  factory(:grave) { create :risk_degree, level: '3', name: 'Grave'}
 
   factory(:general)                 { create :warehouse }
   factory(:modality_limit) { create :modality_limit }
