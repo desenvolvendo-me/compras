aa4825c7380090196bbe8916c50544b89adb1a64
wallison <wallisongoncalves1105ibc@gmail.com>
2020-07-14 09:52:42 -0400

    helper: refactor buscas

--- app/helpers/report/extract_consumption_per_process_helper.rb
+++ app/helpers/report/extract_consumption_per_process_helper.rb
@@ -1,18 +1,13 @@
 module Report::ExtractConsumptionPerProcessHelper
 
   def self.get_contracts(licitation_process, contract)
-    contracts = licitation_process.contracts.joins(:supply_orders)
+    contracts = licitation_process.contracts
     contracts = contracts.where(id: contract) if contract.present?
     contracts.uniq
   end
 
   def self.get_purchase_solicitation(contract, purchase_solicitation)
-    supply_orders = contract.supply_orders
-    supply_orders = supply_orders.where{ purchase_solicitation_id.eq(purchase_solicitation) } if purchase_solicitation.present?
-
-    purchase_solicitation_ids = supply_orders.pluck(:purchase_solicitation_id).uniq if supply_orders.present?
-
-    PurchaseSolicitation.where{ id.in(purchase_solicitation_ids) }
+    PurchaseSolicitation.where(id: contract&.licitation_process&.purchase_solicitations&.pluck(:purchase_solicitation_id))
   end
 
 end
