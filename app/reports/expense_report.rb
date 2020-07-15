1be637fc11fe84d42893df19dc23e3b4e17c52e4
wallison <wallisongoncalves1105ibc@gmail.com>
2020-07-14 20:07:03 -0400

    report: group actions

--- app/reports/expense_report.rb
+++ app/reports/expense_report.rb
@@ -1,11 +1,20 @@
 class ExpenseReport < Report
-  attr_accessor :organ, :organ_id, :purchasing_unit, :purchasing_unit_id
+  attr_accessor :organ, :organ_id, :unity, :unity_id
 
+  def group_unity
+    Organ.where(id: records.pluck(:unity_id).reject(&:blank?).uniq)
+  end
+
+  def group_project_activity expenses
+    ProjectActivity.where(id: expenses.pluck(:project_activity_id).reject(&:blank?).uniq)
+  end
+
+  private
   def normalize_attributes
     params = {}
 
-    params[:licitation_process_id] = organ_id if organ_id.present?
-    params[:creditor_id] = purchasing_unit_id if purchasing_unit_id.present?
+    params[:organ_id] = organ_id if organ_id.present?
+    params[:unity_id] = unity_id if unity_id.present?
 
     params
   end
