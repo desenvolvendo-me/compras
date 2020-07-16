8e0c8214c8d59e3d2c29ca206ad9c37869e6683d
wallison <wallisongoncalves1105ibc@gmail.com>
2020-07-15 22:43:34 -0400

    tabela: objetos da solicitação de compra

---
+++ app/models/purchase_solicitation_object.rb
@@ -0,0 +1,12 @@
+class PurchaseSolicitationObject < Compras::Model
+  attr_accessible :purchase_solicitation_id, :management_object_id
+
+  belongs_to :purchase_solicitation
+  belongs_to :management_object
+
+  orderize "id DESC"
+  filterize
+
+  validates :purchase_solicitation_id, :management_object_id, presence: true
+
+end
