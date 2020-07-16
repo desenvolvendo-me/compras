ee573d88c67d35ef725a1553ff771b003f932e09
wallison <wallisongoncalves1105ibc@gmail.com>
2020-07-15 22:44:45 -0400

    models: relacionamento solicitação de compra -> objects

--- app/models/management_object.rb
+++ app/models/management_object.rb
@@ -1,8 +1,13 @@
 class ManagementObject < Compras::Model
-  attr_accessible :description, :status, :object
+  attr_accessible :description, :status, :object, :purchase_solicitation_objects_attributes
 
   attr_modal :description, :status
   has_enumeration_for :status, with: ObjectStatus
+
+  has_many :purchase_solicitation_objects
+  has_many :purchase_solicitations, through: :purchase_solicitation_objects
+
+  accepts_nested_attributes_for :purchase_solicitation_objects, allow_destroy: true
 
   validates :description, :status, presence: true
 
