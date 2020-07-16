02942d6f2faf7fc927fa3bf5c8cc9317e152959e
Marco Castro <marcodotcastro@gmail.com>
2020-07-16 15:10:29 -0300

    build(unico): ajustes

--- app/models/dissemination_source.rb
+++ app/models/dissemination_source.rb
@@ -1,4 +1,6 @@
 class DisseminationSource < Unico::DisseminationSource
+  attr_accessible  :description, :communication_source_id
+
   belongs_to :communication_source
 
   has_and_belongs_to_many :regulatory_acts, join_table: :unico_dissemination_sources_unico_regulatory_acts
