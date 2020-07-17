e060c093e21300d2e3d7ce298fa37621ba5155c2
wallison <wallisongoncalves1105ibc@gmail.com>
2020-07-17 15:27:14 -0400

    model: auction items

--- app/models/auction_item.rb
+++ app/models/auction_item.rb
@@ -1,8 +1,9 @@
 class AuctionItem < Compras::Model
-  attr_accessible :purchase_solicitation_id,:material_id, :reference_unit_id, :description,
-                  :detailed_description, :estimated_value, :max_value , :benefit_type,
+  attr_accessible :purchase_solicitation_item_id,:material_id, :reference_unit_id, :description, :lot,
+                  :detailed_description, :estimated_value, :max_value , :benefit_type, :auction_id
 
-  belongs_to :purchase_solicitation
+  belongs_to :auction
+  belongs_to :purchase_solicitation_item
   belongs_to :material
   belongs_to :reference_unit
 
--- db/migrate/20200717141736_create_table_auction_itens.rb
+++ db/migrate/20200717141736_create_table_auction_itens.rb
@@ -1,27 +1,31 @@
 class CreateTableAuctionItens < ActiveRecord::Migration
   def change
-    create_table :compras_auction_itens do |t|
-      t.integer :purchase_solicitation_id
+    create_table :compras_auction_items do |t|
+      t.integer :auction_id
+      t.integer :purchase_solicitation_item_id
       t.integer :material_id
       t.integer :reference_unit_id
-      t.string  :description
-      t.text    :detailed_description
+      t.integer :lot
       t.decimal :estimated_value, precision: 16, scale: 2
       t.decimal :max_value, precision: 16, scale: 2
       t.string  :benefit_type
     end
 
-    add_index :compras_auction_itens, :purchase_solicitation_id
-    add_index :compras_auction_itens, :material_id
-    add_index :compras_auction_itens, :reference_unit_id
+    add_index :compras_auction_items, :auction_id
+    add_index :compras_auction_items, :purchase_solicitation_item_id
+    add_index :compras_auction_items, :material_id
+    add_index :compras_auction_items, :reference_unit_id
 
-    add_foreign_key :compras_auction_itens, :compras_purchase_solicitations,
-                    column: :purchase_solicitation_id, name: :auction_itens_purchase_solicitation_id_fk
+    add_foreign_key :compras_auction_items, :compras_auctions,
+                    column: :auction_id, name: :auction_items_auction_id_fk
 
-    add_foreign_key :compras_auction_itens, :compras_materials,
-                    column: :material_id, name: :auction_itens_material_id_fk
+    add_foreign_key :compras_auction_items, :compras_purchase_solicitations,
+                    column: :purchase_solicitation_item_id, name: :auction_items_purchase_solicitation_item_id_fk
 
-    add_foreign_key :compras_auction_itens, :compras_reference_units,
+    add_foreign_key :compras_auction_items, :compras_materials,
+                    column: :material_id, name: :auction_items_material_id_fk
+
+    add_foreign_key :compras_auction_items, :compras_reference_units,
                     column: :reference_unit_id, name: :auction_reference_unit_id_fk
   end
 end
