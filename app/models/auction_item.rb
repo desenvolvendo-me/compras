399e1d4633993c3dbbbb25a5f77c11d30de14bef
wallison <wallisongoncalves1105ibc@gmail.com>
2020-07-17 23:27:56 -0400

    removido solicitação de compra

--- app/models/auction_item.rb
+++ app/models/auction_item.rb
@@ -1,9 +1,8 @@
 class AuctionItem < Compras::Model
-  attr_accessible :purchase_solicitation_item_id,:material_id, :reference_unit_id, :description, :lot,
+  attr_accessible :material_id, :reference_unit_id, :description, :lot,
                   :detailed_description, :estimated_value, :max_value , :benefit_type, :auction_id
 
   belongs_to :auction
-  belongs_to :purchase_solicitation_item
   belongs_to :material
   belongs_to :reference_unit
 
