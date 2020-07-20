3d4fed02abc68127314fce6914a1b02c912465ac
wallison <wallisongoncalves1105ibc@gmail.com>
2020-07-20 14:29:38 -0400

    models

--- app/models/auction_group_item.rb
+++ app/models/auction_group_item.rb
@@ -1,10 +1,10 @@
 class AuctionGroupItem < Compras::Model
-  attr_accessible :auction_id, :purchase_type, :group, :quantity, :judment_form, :total_value, :max_value, :benefit_type
+  attr_accessible :auction_id, :purchase_type, :group_form, :group, :quantity, :judment_form, :total_value, :max_value, :benefit_type
 
   belongs_to :auction
 
   has_enumeration_for :benefit_type, :with => BenefitType
-  has_enumeration_for :group, :with => AuctionGroupForm
+  has_enumeration_for :group_form, :with => AuctionGroupForm
   has_enumeration_for :judment_form, :with => AuctionJudmentForm
   has_enumeration_for :purchase_type, :with => AuctionPurchaseType
 end
