b897a818552715d0c5a4c7afb10a2331a54d14fc
wallison <wallisongoncalves1105ibc@gmail.com>
2020-07-20 14:28:47 -0400

    refactor: migration

--- db/migrate/20200720151619_create_table_auction_group_itens.rb
+++ db/migrate/20200720151619_create_table_auction_group_itens.rb
@@ -3,6 +3,7 @@ class CreateTableAuctionGroupItens < ActiveRecord::Migration
     create_table :compras_auction_group_items do |t|
       t.integer  :auction_id
       t.string   :purchase_type
+      t.string   :group_form
       t.string   :group
       t.integer  :quantity
       t.string   :judment_form
@@ -11,7 +12,7 @@ class CreateTableAuctionGroupItens < ActiveRecord::Migration
       t.string   :benefit_type
     end
 
-    add_index :compras_auction_items, :auction_id
+    add_index :compras_auction_group_items, :auction_id
 
     add_foreign_key :compras_auction_group_items, :compras_auctions,
                     column: :auction_id, name: :auction_group_items_auction_id_fk
