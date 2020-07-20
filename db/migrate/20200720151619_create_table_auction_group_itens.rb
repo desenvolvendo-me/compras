b0c911c802b1f26b19a927d7dd09b5c5d1c339bc
wallison <wallisongoncalves1105ibc@gmail.com>
2020-07-20 13:13:15 -0400

    crud: pregão grupo de itens

---
+++ db/migrate/20200720151619_create_table_auction_group_itens.rb
@@ -0,0 +1,19 @@
+class CreateTableAuctionGroupItens < ActiveRecord::Migration
+  def change
+    create_table :compras_auction_group_items do |t|
+      t.integer  :auction_id
+      t.string   :purchase_type
+      t.string   :group
+      t.integer  :quantity
+      t.string   :judment_form
+      t.decimal  :total_value, precision: 16, scale: 2
+      t.decimal  :max_value, precision: 16, scale: 2
+      t.string   :benefit_type
+    end
+
+    add_index :compras_auction_items, :auction_id
+
+    add_foreign_key :compras_auction_group_items, :compras_auctions,
+                    column: :auction_id, name: :auction_group_items_auction_id_fk
+  end
+end
