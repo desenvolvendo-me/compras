4df42461c6d2d26e52494f58e69c9e65697e82b6
wallison <wallisongoncalves1105ibc@gmail.com>
2020-07-23 18:09:33 -0400

    fix: botão adicionar

--- app/decorators/supply_request_decorator.rb
+++ app/decorators/supply_request_decorator.rb
@@ -12,11 +12,11 @@ class SupplyRequestDecorator
   end
 
   def creditor
-    self.contract.nil? || self.contract.creditors.blank? || self.contract.creditors.first.person.nil? ? '' : self.contract.creditors.first.person.name
+    self.contract.nil? || self.contract.creditor.blank? || self.contract.creditor.person.nil? ? '' : self.contract.creditor.person.name
   end
 
   def creditor_id
-    self.contract.nil? || self.contract.creditors.blank? || self.contract.creditors.first.person.nil? ? '' : self.contract.creditors.first.id
+    self.contract.nil? || self.contract.creditor.blank? || self.contract.creditor.person.nil? ? '' : self.contract.creditor.id
   end
 
   def value
