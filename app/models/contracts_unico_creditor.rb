9400461c11ad089c2121b27f6d185ec73700368e
Hugo Silva <hugocardososilva@gmail.com>
2020-07-23 10:26:56 -0300

    adicionando campos person

--- app/models/contracts_unico_creditor.rb
+++ app/models/contracts_unico_creditor.rb
@@ -1,9 +1,10 @@
 class ContractsUnicoCreditor < Compras::Model
   attr_accessible :contract_id, :creditor_id
 
+  attr_accessor :person, :person_id
+
   set_primary_key :contract_id
 
   belongs_to :creditor
   belongs_to :contract
-
 end
