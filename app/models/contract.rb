bb707ce24bb0cf682c76dd63243039756930cd14
wallison <wallisongoncalves1105ibc@gmail.com>
2020-07-23 20:40:25 -0400

    model: add contract_validations

--- app/models/contract.rb
+++ app/models/contract.rb
@@ -8,7 +8,7 @@ class Contract < Compras::Model
                   :cancellation_date, :cancellation_reason, :delivery_schedules_attributes,
                   :dissemination_source_id, :contract_type_id, :contract_additives_attributes,
                   :licitation_process_id, :start_date, :budget_structure_responsible_id,
-                  :lawyer_id, :parent_id, :additives_attributes, :penalty_fine,
+                  :lawyer_id, :parent_id, :additives_attributes, :penalty_fine, :contract_validations_attributes,
                   :default_fine, :execution_type, :contract_guarantees, :occurrence_contractual_historics_attributes,
                   :consortium_agreement, :department_id, :balance_control_type, :authorized_areas_attributes,
                   :purchasing_unit_id, :financials_attributes, :balance
@@ -59,6 +59,7 @@ class Contract < Compras::Model
   accepts_nested_attributes_for :financials, allow_destroy: true
   accepts_nested_attributes_for :contract_additives, allow_destroy: true
   accepts_nested_attributes_for :occurrence_contractual_historics, allow_destroy: true
+  accepts_nested_attributes_for :contract_validations, allow_destroy: true
 
   delegate :execution_type_humanize, :contract_guarantees_humanize, :contract_guarantees,
            :to => :licitation_process, :allow_nil => true
