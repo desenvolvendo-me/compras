b4290962fd4fe3b14e2c42346ff231690642f259
wallison <wallisongoncalves1105ibc@gmail.com>
2020-07-23 21:21:05 -0400

    decorator: contract_terminator

--- app/decorators/contract_termination_decorator.rb
+++ app/decorators/contract_termination_decorator.rb
@@ -1,7 +1,10 @@
 class ContractTerminationDecorator
   include Decore
   include Decore::Proxy
+  include Decore::Header
   include ActionView::Helpers::TranslationHelper
+
+  attr_header :number, :year, :status, :expiry_date, :termination_date
 
   def is_annulled_message
     t('contract_termination.messages.is_annulled') if annulled?
