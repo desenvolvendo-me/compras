b1bdcee4a563154d8e0e71aa73578fbf0a05e92a
Marco Castro <marcodotcastro@gmail.com>
2020-07-27 17:42:47 -0300

    build(unico): deploy
    - Circular dependency detected while autoloading constant

--- app/helpers/report/supply_requests_helper.rb
+++ app/helpers/report/supply_requests_helper.rb
@@ -1,8 +1,9 @@
+module Report
+  module SupplyRequests
 
-module Report::SupplyRequests
+    def self.secretary_settings?(object)
+      object&.secretary_settings&.blank? ? "Por favor cadastre sua assinatura digital para aprovar o pedido." : ''
+    end
 
-  def self.secretary_settings?(object)
-    object&.secretary_settings&.blank? ? "Por favor cadastre sua assinatura digital para aprovar o pedido." : ''
   end
-
 end
