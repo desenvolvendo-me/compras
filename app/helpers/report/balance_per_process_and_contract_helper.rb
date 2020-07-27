dfb08bc69a568d63f22733b7aeb26c9bbfef0585
wallison <wallisongoncalves1105ibc@gmail.com>
2020-07-27 14:53:00 -0400

    helpers(balance_per_process_and_contract): fix relação creditor

--- app/helpers/report/balance_per_process_and_contract_helper.rb
+++ app/helpers/report/balance_per_process_and_contract_helper.rb
@@ -1,9 +1,9 @@
 module Report::BalancePerProcessAndContractHelper
 
-  def self.get_contracts(licitation_process, contract, creditor)
+  def self.get_contracts(licitation_process, contract, creditor_id)
     contracts = licitation_process.contracts
     contracts = contracts.where(id: contract) if contract.present?
-    contracts = contracts.joins(:creditors).where("compras_contracts_unico_creditors.creditor_id = #{creditor}") if creditor.present?
+    contracts = contracts.joins{ creditor }.where{creditor.id.eq(creditor_id) } if creditor_id.present?
     contracts
   end
 
