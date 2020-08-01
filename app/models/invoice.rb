543d2a51d7e7b6edfdc9992047360940d9598b28
wallison <wallisongoncalves1105ibc@gmail.com>
2020-07-31 21:02:29 -0400

    model(invoice): add itens da nota fiscal

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -1,6 +1,11 @@
 class Invoice < Compras::Model
-  attr_accessible :number,:release_date,:date,:value
+  attr_accessible :number, :release_date, :date, :value, :supply_order_item_invoices_attributes
 
   belongs_to :supply_order
+  has_many :supply_order_item_invoices
+
+  validates :number, presence: true
+
+  accepts_nested_attributes_for :supply_order_item_invoices, allow_destroy: true, reject_if: proc { |att| att['quantity_supplied'].blank?}
 
 end
