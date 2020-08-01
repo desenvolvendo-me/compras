4c5a0e55fcadaebde9eb15d82c9046b2515034a4
wallison <wallisongoncalves1105ibc@gmail.com>
2020-07-31 21:07:44 -0400

    helper(supply_order): initializer forms

--- app/helpers/supply_orders_helper.rb
+++ app/helpers/supply_orders_helper.rb
@@ -2,7 +2,15 @@ module SupplyOrdersHelper
   def init_object object, material
     aux = object
     object = object.items.where(material_id: material.id).last
-    object = aux.items.build if object.blank?
+    object = SupplyOrderItem.new(supply_order_id: aux.id) if object.blank?
+
+    object
+  end
+
+  def init_item_invoices object, material
+    aux = object.supply_order
+    object = object.supply_order_item_invoices.where(supply_order_item_id: material.id).last
+    object = SupplyOrderItemInvoice.new(supply_order_id: aux.id) if object.blank?
 
     object
   end
