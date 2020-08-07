33fabf6c03ebe15807855d4cdbd976e093690faa
wallison <wallisongoncalves1105ibc@gmail.com>
2020-08-07 16:49:31 -0400

    js(supply_order): refactor

--- app/assets/javascripts/supply_orders.js
+++ app/assets/javascripts/supply_orders.js
@@ -471,6 +471,18 @@ $(document).ready(function () {
     setCompetenceMonth();
   });
 
+  $(".remove-invoice").click(function(){
+    var $el = $(this);
+    var btn = {"Apagar": function() {
+        $el.closest('td').find('.remove-nested-record').click();
+        $( this ).dialog( "close" );
+      },
+      Cancelar: function() {
+        $( this ).dialog( "close" );
+      }};
+    custom_alert("Você tem certeza que deseja apagar o registro?", "Remover Registro", btn)
+  });
+
   setCompetenceMonth();
   /** tab Nota Fiscal **/
 
@@ -491,14 +503,4 @@ $(document).ready(function () {
     $('form.supply_order').submit();
   });
 
-  $(".remove-invoice").click(function(){
-    var $el = $(this);
-    var btn = {"Apagar": function() {
-                $el.closest('td').find('.remove-nested-record').click();
-              },
-              Cancelar: function() {
-                $( this ).dialog( "close" );
-              }};
-    custom_alert("Você tem certeza que deseja apagar o registro?", "Remover Registro", btn)
-  })
 });
