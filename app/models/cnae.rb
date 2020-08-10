bb5daa77c0a86bb13155bdf356dacc77bda63c90
Hugo Silva <hugocardososilva@gmail.com>
2020-08-10 18:02:34 -0300

    adicionando relacionamentos

--- app/models/cnae.rb
+++ app/models/cnae.rb
@@ -1,7 +1,8 @@
 class Cnae < Unico::Cnae
   attr_modal :name, :code
 
-  has_many :creditors_with_main_cnae, :class_name => 'Creditor', :dependent => :restrict, :foreign_key => :main_cnae_id
+  has_many :creditors_with_main_cnae, :class_name => "Creditor", :dependent => :restrict, :foreign_key => :main_cnae_id
+  has_many :companies_with_main_cnae, :class_name => "Company", :dependent => :restrict, :foreign_key => :main_cnae_id
   has_many :creditor_secondary_cnaes, :dependent => :restrict
   has_many :creditors, :through => :creditor_secondary_cnaes
 
--- app/models/company.rb
+++ app/models/company.rb
@@ -1,12 +1,23 @@
 # frozen_string_literal: true
 
 class Company < Persona::Company
+  attr_accessible :responsible_name
+
+  attr_accessor :cnae_ids
+
   delegate :city, :zip_code, to: :address, allow_nil: true
 
   validate :at_least_one_partner
+  belongs_to :main_cnae, class_name: "::Cnae"
+
+  delegate :code, to: :main_cnae, prefix: true, allow_nil: true
 
   orderize
   filterize
+
+  def selected_cnaes
+    cnae_ids | [main_cnae_id]
+  end
 
   protected
 
