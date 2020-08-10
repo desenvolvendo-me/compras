ffc82f35a24ed31bc0420dc67b7c976a420bae49
wallison <wallisongoncalves1105ibc@gmail.com>
2020-08-10 17:20:07 -0400

    removido metodos inuteis

--- app/decorators/purchase_solicitation_item_decorator.rb
+++ app/decorators/purchase_solicitation_item_decorator.rb
@@ -5,27 +5,4 @@ class PurchaseSolicitationItemDecorator
 
   attr_header :material, :lot, :reference_unit, :brand, :quantity, :unit_price, :estimated_total_price
 
-  def material_grid_mustache
-    material || "{{material}}"
-  end
-
-  def lot_grid_mustache
-    lot || "{{lot}}"
-  end
-
-  def reference_unit_grid_mustache
-    reference_unit || "{{reference_unit}}"
-  end
-
-  def brand_grid_mustache
-    brand || "{{brand}}"
-  end
-
-  def unit_price_grid_mustache
-    unit_price || "{{unit_price}}"
-  end
-
-  def estimated_total_price_grid_mustache
-    estimated_total_price || "{{estimated_total_price}}"
-  end
 end
