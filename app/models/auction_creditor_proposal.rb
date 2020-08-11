9f3854d0bcd9d1b4a57c9054f9fff65e11844ba1
wallison <wallisongoncalves1105ibc@gmail.com>
2020-08-11 10:11:10 -0400

    model(auction_creditor_proposal): data envio doc proposta

--- app/models/auction_creditor_proposal.rb
+++ app/models/auction_creditor_proposal.rb
@@ -1,6 +1,6 @@
 class AuctionCreditorProposal < Compras::Model
   attr_accessible :auction_id,:term,:impediment,:proposal_independent,:art_5,:art_93_pcd,:art_529_clt, :user_id,
-                  :proposal_doc,:proposal_qualification_doc, :auction_creditor_proposal_items_attributes
+                  :proposal_doc,:proposal_qualification_doc, :auction_creditor_proposal_items_attributes, :proposal_send_date
 
   mount_uploader :proposal_doc, UnicoUploader
   mount_uploader :proposal_qualification_doc, UnicoUploader
@@ -12,4 +12,13 @@ class AuctionCreditorProposal < Compras::Model
 
   accepts_nested_attributes_for :auction_creditor_proposal_items, :allow_destroy => true
 
+  before_save :set_proposal_send_date
+
+  private
+
+  def set_proposal_send_date
+    unless self.proposal_doc
+      self.proposal_send_date = Date.today
+    end
+  end
 end
