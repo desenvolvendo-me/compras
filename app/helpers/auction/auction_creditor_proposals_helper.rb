bf152abd0a472251283a115e6156e249cfc77c6e
wallison <wallisongoncalves1105ibc@gmail.com>
2020-08-18 15:16:27 -0400

    view(auction_creditor_proposals): fix campo quantidade

--- app/helpers/auction/auction_creditor_proposals_helper.rb
+++ app/helpers/auction/auction_creditor_proposals_helper.rb
@@ -3,7 +3,7 @@ module Auction::AuctionCreditorProposalsHelper
   def init_item(object, item)
     aux = object
     object = object.auction_creditor_proposal_items.where(auction_item_id: item.id).last
-    object = AuctionCreditorProposalItem.new(auction_creditor_proposal_id: aux.id, auction_item_id: item.id) if object.blank?
+    object = AuctionCreditorProposalItem.new(auction_creditor_proposal_id: aux.id, auction_item_id: item.id, quantity: item.quantity) if object.blank?
 
     object
   end
