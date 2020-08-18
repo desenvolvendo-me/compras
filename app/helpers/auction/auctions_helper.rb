7da79eef09ffe0a5a2cafd13d6be5b920f4d29f6
Hugo Silva <hugocardososilva@gmail.com>
2020-08-18 12:03:07 -0300

    removendo bug

--- app/helpers/auction/auctions_helper.rb
+++ app/helpers/auction/auctions_helper.rb
@@ -1,31 +1,27 @@
 module Auction::AuctionsHelper
-
   def address(auction)
-     "#{auction.street},   #{auction.neighborhood},  #{auction.city}"
+    "#{auction.street},   #{auction.neighborhood},  #{auction.city}"
   end
 
   def contact(auction)
     "#{auction.telephone} / #{auction.cell_phone}"
   end
 
-  def proposal_link resource
-    proposal = resource.creditor_proposals.where(user_id: current_user.id).last
+  def proposal_link(resource)
+    proposal = resource.creditor_proposals.where(user_id: current_user.try(:id)).last
     if proposal
       route = edit_auction_auction_creditor_proposal_path(proposal.id)
     else
       route = new_auction_auction_creditor_proposal_path(auction_id: resource.id)
     end
 
-    link_to "Proposta", route , class: 'btn btn-primary'
+    link_to "Proposta", route, class: "btn btn-primary"
   end
 
-
-  def disput_link resource
+  def disput_link(resource)
     route = auction_providers_register_external_path unless user_signed_in?
     route = "#" if user_signed_in?
 
-    link_to "Entrar Disputa", route, class: 'button primary'
+    link_to "Entrar Disputa", route, class: "button primary"
   end
-
-
 end
