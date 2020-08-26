5f10b8f77ff4e100c5ce49837e667c43bc2971a3
wallison <wallisongoncalves1105ibc@gmail.com>
2020-08-26 17:18:23 -0400

    fix migration

--- db/migrate/20200826183649_create_table_auction_bids.rb
+++ db/migrate/20200826183649_create_table_auction_bids.rb
@@ -7,8 +7,5 @@ class CreateTableAuctionBids < ActiveRecord::Migration
 
     add_index :compras_auction_bids, :auction_id
     add_foreign_key :compras_auction_bids, :compras_auctions, column: :auction_id
-
-    add_index :compras_auction_bids, :creditor_id
-    add_foreign_key :compras_auction_bids, :unico_creditors, column: :creditor_id
   end
 end
