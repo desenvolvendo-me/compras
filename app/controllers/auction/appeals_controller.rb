51e53acc88ac83ebef8f90c6b858caa349a119e2
wallison <wallisongoncalves1105ibc@gmail.com>
2020-08-28 15:50:02 -0400

    crud auction appeals

---
+++ app/controllers/auction/appeals_controller.rb
@@ -0,0 +1,25 @@
+class Auction::AppealsController <  Auction::BaseController
+  defaults resource_class: AuctionAppeal
+
+  def new
+    object = build_resource
+    object.auction_id = params[:auction_id]
+    object.person = current_user.authenticable.try(:person)
+
+    super
+  end
+
+  def create
+    create! do |success, failure|
+      success.html { redirect_to edit_resource_path }
+      failure.html { render :new }
+    end
+  end
+
+  def update
+    update! do |success, failure|
+      success.html { redirect_to edit_resource_path }
+      failure.html { render :edit }
+    end
+  end
+end
---
+++ db/migrate/20200836140913_create_table_compras_auction_appeals.rb
@@ -0,0 +1,24 @@
+class CreateTableComprasAuctionAppeals < ActiveRecord::Migration
+  def change
+    create_table :compras_auction_appeals do |t|
+      t.integer :auction_id
+      t.date    :appeal_date
+      t.string  :related
+      t.integer :person_id
+      t.text    :valid_reason
+      t.text    :auction_committee_opinion
+      t.string  :situation
+      t.date    :new_envelope_opening_date
+      t.time    :new_envelope_opening_time
+      t.datetime :created_at,                   :null => false
+      t.datetime :updated_at,                   :null => false
+    end
+
+    add_index :compras_auction_appeals, :auction_id, :name => :ca_auction_id
+    add_index :compras_auction_appeals, :person_id, :name => :ca_person_id
+
+    add_foreign_key :compras_auction_appeals, :compras_auctions, column: :auction_id
+    add_foreign_key :compras_auction_appeals, :unico_people, column: :person_id
+  end
+
+end
