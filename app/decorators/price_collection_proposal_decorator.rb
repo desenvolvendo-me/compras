a3355fb7ac07356f13b50fe2181cdecd9ed63801
wallison <wallisongoncalves1105ibc@gmail.com>
2020-08-31 17:57:04 -0400

    price_collection: adicionado valor total do lot ao listde propostas

--- app/decorators/price_collection_proposal_decorator.rb
+++ app/decorators/price_collection_proposal_decorator.rb
@@ -5,7 +5,7 @@ class PriceCollectionProposalDecorator
   include ActionView::Helpers::NumberHelper
   include ActionView::Helpers::TranslationHelper
 
-  attr_header :code_and_year, :creditor, :price_collection_date, :status
+  attr_header :code_and_year, :creditor, :total_lot_value, :price_collection_date, :status
 
   def code_and_year
     "#{price_collection_code}/#{price_collection_year}"
@@ -21,5 +21,14 @@ class PriceCollectionProposalDecorator
 
   def only_creditor_is_authorized_message(user)
     t('price_collection_proposal.messages.only_creditor_is_authorized') unless editable_by?(user)
+  end
+
+  def total_lot_value
+    str = []
+    price_collection_lots.each do |lot|
+      str << 'lote '+ (lot.to_s) +': '+ number_to_currency(item_total_value_by_lot(lot))
+    end
+
+    str.join('/ ')
   end
 end
