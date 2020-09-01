18a33d265ccf06961d5a58615fc0505efa6c6245
wallison <wallisongoncalves1105ibc@gmail.com>
2020-09-01 00:37:41 -0400

    validate email

--- app/models/price_collection_proposal.rb
+++ app/models/price_collection_proposal.rb
@@ -16,12 +16,14 @@ class PriceCollectionProposal < Compras::Model
 
   accepts_nested_attributes_for :items, :allow_destroy => true
 
-  validates :creditor, :email, :presence => true
-  validates :email,  mail: true, :presence => true
+  validates :creditor, :presence => true
+  validates :email, mail: true, presence: true
   validate :must_have_a_valid_creditor_user, if: :user
+
 
   orderize "id DESC"
   filterize
+
 
   scope :not_invited, lambda { where { email_invitation.eq(false) } }
 
