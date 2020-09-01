5521cdc8574df8b8a0c5b34168ded3a109583674
andbri321 <andbri321@gmail.com>
2020-09-01 20:18:02 -0300

    [u] reports/licitation_process_repor

--- app/reports/licitation_process_report.rb
+++ app/reports/licitation_process_report.rb
@@ -3,8 +3,8 @@ class LicitationProcessReport < Report
   attr_accessor :modality,:year,:process,
                 :process_date,:notice_availability_date,
                 :creditor,:creditor_id,
-                :process_date_start,:process_date_final
+                :process_date_start,:process_date_end,:licitation_processes
 
-  has_enumeration_for :modality,:with => Modality
+  has_enumeration_for :modality,:with => TypeOfRemoval
 
 end
