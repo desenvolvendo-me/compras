b3b5b942ec6cf6bdd0d78584efdb307b21d65fc8
andbri321 <andbri321@gmail.com>
2020-09-02 10:07:34 -0300

    [u] enum type_of_removal, remover uma opção

--- app/enumerations/type_of_removal.rb
+++ app/enumerations/type_of_removal.rb
@@ -2,7 +2,6 @@ class TypeOfRemoval < EnumerateIt::Base
   associate_values :removal_by_limit,
                    :removal_justified,
                    :removal_by_ineligibility,
-                   :other_reasons_to_removal,
                    :unenforceability_accreditation,
                    :dispensation_justified_accreditation,
                    :call,
