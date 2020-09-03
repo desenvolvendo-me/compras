05e7a4584d15c7b01deb858ac35f8190026f2180
wallison <wallisongoncalves1105ibc@gmail.com>
2020-09-02 21:46:13 -0400

    purchase_process_accreditation_creditor: representante do fornecedor

--- app/models/purchase_process_accreditation_creditor.rb
+++ app/models/purchase_process_accreditation_creditor.rb
@@ -1,29 +1,26 @@
 class PurchaseProcessAccreditationCreditor < Compras::Model
   attr_accessible :purchase_process_accreditation_id, :creditor_id, :company_size_id,
-                  :creditor_representative_id, :kind, :has_power_of_attorney
+                  :kind, :has_power_of_attorney
 
   has_enumeration_for :kind, :with => PurchaseProcessAccreditationCreditorKind
 
   belongs_to :purchase_process_accreditation
   belongs_to :creditor
   belongs_to :company_size
-  belongs_to :creditor_representative
 
   has_many :trading_item_bids, class_name: 'PurchaseProcessTradingItemBid',
     dependent: :restrict
 
   delegate :personable_type_humanize, :address, :city, :state, :identity_document,
            :neighborhood, :zip_code, :phone, :person_email, :individual?, :company?,
-           :proposal_by_item, :proposal_by_lot,
+           :proposal_by_item, :proposal_by_lot, :creditor_representative,
            :to => :creditor, :allow_nil => true, :prefix => true
-  delegate :identity_document, :phone, :email, :identity_number,
-           :to => :creditor_representative, :allow_nil => true, :prefix => true
   delegate :benefited?, to: :company_size, allow_nil: true
   delegate :judgment_form_item?, to: :purchase_process_accreditation, allow_nil: true
 
   validates :kind, :presence => true, :if => :creditor_representative_present?
   validates :creditor, :purchase_process_accreditation, :presence => true
-  # validates :company_size_id, presence: true, if: :creditor_company?
+
 
   scope :selected_creditors, lambda {
     joins { creditor.person }.
