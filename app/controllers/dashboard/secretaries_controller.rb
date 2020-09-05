cd656d8cdc3e75540f5a71ed5bc0ac49a41ab74f
andbri321 <andbri321@gmail.com>
2020-09-04 22:30:10 -0300

    dashboard/secretaries_controller

--- app/controllers/dashboard/secretaries_controller.rb
+++ app/controllers/dashboard/secretaries_controller.rb
@@ -4,18 +4,18 @@ class Dashboard::SecretariesController < CrudController
 
 
   def index
-    @contracts = Contract.by_days_finish(30).page(params[:page]).per(5)
+    @contracts = Contract.between_days_finish(0,30).page(params[:page]).per(3)
     @contracts_periods = Contract.count_contracts_finishing
     @approval_requests = SupplyRequest.to_secretary_approv(current_user.authenticable.id).page(params[:page_approval]).per(5)
-    @linked_contracts = apply_scopes(LinkedContract).page(params[:page]).per(1)
+    @linked_contracts = apply_scopes(LinkedContract).page(params[:page]).per(2)
   end
 
   def contracts
-    @contracts = apply_scopes(Contract).page(params[:page]).per(5)
+    @contracts = apply_scopes(Contract).page(params[:page]).per(3)
   end
   
   def linked_contracts
-    @linked_contracts = apply_scopes(LinkedContract).page(params[:page]).per(1)
+    @linked_contracts = apply_scopes(LinkedContract).page(params[:page]).per(2)
   end
   
   def approval_requests
