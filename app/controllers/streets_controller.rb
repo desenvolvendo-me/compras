ff5801a6b43002fbf54b903d2d93e05f96fccfcc
wallison <wallisongoncalves1105ibc@gmail.com>
2020-09-11 20:39:43 -0400

    make index public

--- app/controllers/streets_controller.rb
+++ app/controllers/streets_controller.rb
@@ -1,5 +1,6 @@
 class StreetsController < CrudController
-  skip_before_filter :authorize_resource!, only: [:index, :show]
+  skip_before_filter :authenticate_user!, :only => [:index]
+  skip_before_filter :authorize_resource!, only: [:index]
   has_scope :neighborhood
   has_scope :term
 end
