bd53ffb0a96f8c86221971e6da7e64381409db41
wallison <wallisongoncalves1105ibc@gmail.com>
2020-09-11 20:39:28 -0400

    make index public

--- app/controllers/cnaes_controller.rb
+++ app/controllers/cnaes_controller.rb
@@ -1,6 +1,6 @@
 class CnaesController < CrudController
-  skip_before_filter :authorize_resource!, only: [:index]
-  skip_before_filter :authenticate_user!, only: [:index]
+  skip_before_filter :authorize_resource!, only: [:modal]
+  skip_before_filter :authenticate_user!, only: [:modal]
   actions :modal
   has_scope :term
   has_scope :cnaes_remainder, :type => :array
