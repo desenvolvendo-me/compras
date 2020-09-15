bce70536fdd0ff626d2de42b6e619da34cabc5bf
wallison <wallisongoncalves1105ibc@gmail.com>
2020-09-15 14:48:49 -0400

    new attributes

--- app/decorators/auction_appeal_decorator.rb
+++ app/decorators/auction_appeal_decorator.rb
@@ -3,6 +3,10 @@ class AuctionAppealDecorator
   include Decore::Proxy
   include Decore::Header
 
-  attr_header :auction, :person, :appeal_date
+  attr_header :auction, :person, :appeal_date, :new_envelope_opening_date,
+              :new_envelope_opening_time, :situation
 
+  def new_envelope_opening_time
+    super.strftime('%H:%M') if super
+  end
 end
