aa7bb7ee1572b1f7c6e75f4b2a28e80c64ba7ba8
wallison <wallisongoncalves1105ibc@gmail.com>
2020-09-16 15:37:59 -0400

    making create street public

--- app/controllers/streets_controller.rb
+++ app/controllers/streets_controller.rb
@@ -1,6 +1,6 @@
 class StreetsController < CrudController
-  skip_before_filter :authenticate_user!, :only => [:index, :show]
-  skip_before_filter :authorize_resource!, only: [:index, :show]
+  skip_before_filter :authenticate_user!, :only => [:index, :show, :create]
+  skip_before_filter :authorize_resource!, only: [:index, :show, :create]
   has_scope :neighborhood, allow_blank: true
   has_scope :term, allow_blank: true
   has_scope :by_zip_code, allow_blank: true
