1cc8e0489dac880e3495d81b4f00a71e97ef221b
wallison <wallisongoncalves1105ibc@gmail.com>
2020-09-16 15:38:13 -0400

    making create neighborhood public

--- app/controllers/neighborhoods_controller.rb
+++ app/controllers/neighborhoods_controller.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
 class NeighborhoodsController < CrudController
-  skip_before_filter :authenticate_user!, :only => [:index]
-  skip_before_filter :authorize_resource!, :only => [:index]
+  skip_before_filter :authenticate_user!, :only => [:index, :create]
+  skip_before_filter :authorize_resource!, :only => [:index, :create]
   has_scope :street_id
   has_scope :by_city
   has_scope :term
