d9dd036abf1b9a822c3c5dc030c93890aed7b6d1
wallison <wallisongoncalves1105ibc@gmail.com>
2020-09-17 10:18:31 -0400

    fix decorator

--- app/decorators/demand_decorator.rb
+++ app/decorators/demand_decorator.rb
@@ -3,9 +3,9 @@ class DemandDecorator
   include Decore::Proxy
   include Decore::Header
 
-  attr_header :year, :name_number, :status, :initial_date, :final_date, :link => [:year, :name_number]
+  attr_header :year, :name, :status, :initial_date, :final_date, :link => [:year, :name_number]
 
-  def name_number
-    name+" / "+id.to_s
+  def name
+    (super) +" / "+id.to_s
   end
 end
