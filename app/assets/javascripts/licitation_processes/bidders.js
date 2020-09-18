9d9c236026087a096f0952757f19761367a84553
wallison <wallisongoncalves1105ibc@gmail.com>
2020-09-17 22:14:31 -0400

    javascript organized

---
+++ app/assets/javascripts/licitation_processes/bidders.js
@@ -0,0 +1,46 @@
+function initMustache(){
+  $("#bidder_documents_template").nestedForm({
+    add: '.add_bidder_document',
+    remove: '.remove_bidder_document',
+    fieldsWrapper: '.bidder_document',
+    target: '#bidder_documents',
+    uuid: 'uuid_documents',
+    appendItem: true
+  });
+
+  $(".tabs").tabs();
+  $("#bidders-records, #add-bidder").hide();
+  $("#cancel-bidder").show();
+}
+
+$("#cancel-bidder").click(function(){
+  $("#bidders-records, #add-bidder").show();
+  $(this).hide();
+  $("#bidder-fields").html('');
+});
+
+$(function(){
+
+  $(document).on('click', 'button.remove_bidder_document, button.add_bidder_document', function(event) {
+    event.preventDefault()
+  });
+
+  $("#add-bidder").click(function() {
+    $('#bidder-fields').append($('#bidders-template').mustache({uuid_bidder: _.uniqueId('fresh-')}));
+    initMustache();
+  });
+
+  $(".edit-bidder").click(function() {
+    var url = Routes.add_bidders_licitation_processes,
+      params = {
+        id: $("#licitation_process_id").val(), bidder_id: $(this).data('id')
+  };
+
+    url += "?" + $.param(params);
+
+    $.get(url, function (data) {
+      $("#bidder-fields").html($(data).html());
+      initMustache();
+    })
+  })
+})
