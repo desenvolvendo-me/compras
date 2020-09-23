dc1c968a7ad088c7fd736d4ef67be5537a61c9fb
wallison <wallisongoncalves1105ibc@gmail.com>
2020-09-22 20:06:52 -0400

    report: fix return

--- app/reports/minute_purchase_process_report.rb
+++ app/reports/minute_purchase_process_report.rb
@@ -83,6 +83,7 @@ class MinutePurchaseProcessReport < Report
     end
   end
 
+  #retorna os fornecedores vencedores e o valor total das propostas
   def creditor_ratified
     ratified_items = licitation_process.ratifications_items.includes(:licitation_process_ratification)
     creditors = ratified_items.map{|x| [x.licitation_process_ratification.creditor, x.licitation_process_ratification_id]}.uniq
@@ -96,19 +97,14 @@ class MinutePurchaseProcessReport < Report
           sum += (item&.quantity || 0) * (item&.unit_price || 0)
         end
       end
-
       result.push([creditor[0], sum])
-
-      return result
     end
+
+    result
   end
 
   def modality
     licitation_process.type_of_removal_humanize
-  end
-
-  def sum_total
-
   end
 
   protected
