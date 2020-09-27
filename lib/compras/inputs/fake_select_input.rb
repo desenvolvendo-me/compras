fd26d7b3d656034e80ddc32c69e21e4337475f73
wallison <wallisongoncalves1105ibc@gmail.com>
2020-09-27 19:24:31 -0400

    fake_select_input: fix field name html attribute

--- lib/compras/inputs/fake_select_input.rb
+++ lib/compras/inputs/fake_select_input.rb
@@ -7,7 +7,7 @@ module Compras
         merged_input_options = merge_wrapper_options(input_html_options, wrapper_options).merge(input_options.slice(:multiple, :include_blank, :disabled, :prompt))
 
         template.select_tag(
-            attribute_name,
+            "#{sanitized_object_name}[#{attribute_name}]",
             template.options_from_collection_for_select(collection, value_method, label_method, selected: input_options[:selected], disabled: input_options[:disabled]),
             merged_input_options
         )
