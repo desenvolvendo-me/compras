913a564528b85b4c35a161bfeac3be8b14e40649
andbri321 <andbri321@gmail.com>
2020-09-29 00:15:55 -0300

    [u] contract_report

--- app/reports/contract_report.rb
+++ app/reports/contract_report.rb
@@ -3,6 +3,8 @@ class ContractReport < Report
   attr_accessor :contract_number,
                 :year,:creditor,:creditor_id,:publication_date,
                 :content,:contract_value,:modality_humanize,
-                :start_date,:end_date,:content
+                :start_date,:end_date,:content,:status
+
+  has_enumeration_for :status, with: ContractStatus, create_helpers: true
 
 end
