49d397cf721281b295622b87d6b7899a4fdcf97a
wallison <wallisongoncalves1105ibc@gmail.com>
2020-10-06 07:51:26 -0400

    linked_contract_decorator: date pt-br

--- app/decorators/linked_contract_decorator.rb
+++ app/decorators/linked_contract_decorator.rb
@@ -4,4 +4,12 @@ class LinkedContractDecorator
   include Decore::Header
 
   attr_header :contract_number,:start_date_contract,:end_date_contract,:contract_value
+
+  def start_date_contract
+    I18n.l(super) if super
+  end
+
+  def end_date_contract
+    I18n.l(super) if super
+  end
 end
