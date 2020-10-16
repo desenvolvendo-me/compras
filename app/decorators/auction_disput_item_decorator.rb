bc118835ff157bf9696ed5ded77682d2912c9fb9
wallison <wallisongoncalves1105ibc@gmail.com>
2020-10-16 19:14:34 -0400

    auction_disput_item_decorator: refactor function names

--- app/decorators/auction_disput_item_decorator.rb
+++ app/decorators/auction_disput_item_decorator.rb
@@ -5,15 +5,27 @@ class AuctionDisputItemDecorator
   include ActionView::Helpers::TranslationHelper
 
   def lowest_proposal_amount
-    return '-' unless lowest_item_proposal_amount
+    return '-' unless lowest_item_proposal
 
-    number_with_precision lowest_item_proposal_amount.unit_price
+    number_with_precision lowest_item_proposal.unit_price
   end
 
   def lowest_proposal_creditor
-    return '-' unless lowest_item_proposal_amount
+    return '-' unless lowest_item_proposal
 
-    number_with_precision lowest_item_proposal_amount.creditor
+    number_with_precision lowest_item_proposal.creditor
+  end
+
+  def lowest_bid_amount
+    return '-' unless lowest_bid.present?
+
+    number_with_precision lowest_bid.amount
+  end
+
+  def lowest_bid_creditor
+    return '-' unless lowest_bid.present?
+
+    lowest_bid.creditor
   end
 
   def bids_finished?
