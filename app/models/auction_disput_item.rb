2bd8d198cd42ad2098842f0c683f8aabe2df25da
wallison <wallisongoncalves1105ibc@gmail.com>
2020-10-16 19:15:15 -0400

    auction_disput_item: refactor function names and association with bids

--- app/models/auction_disput_item.rb
+++ app/models/auction_disput_item.rb
@@ -6,10 +6,16 @@ class AuctionDisputItem < Compras::Model
 
   has_one :material, :through => :auction_item
 
+  has_many :bids, class_name: 'AuctionDisputItemBid'
+
   has_enumeration_for :status, with: AuctionDisputItemStatus,
                       create_helpers: true, create_scopes: true
 
-  def lowest_item_proposal_amount
-    auction.auction_creditor_proposal_items.lowest_proposal_by_item(id).first
+  def lowest_item_proposal
+    auction.auction_creditor_proposal_items.proposals_ordered_by_item(id).first
+  end
+
+  def lowest_bid
+    bids.bids_ordered_by_item(id).first
   end
 end
