df3de253854490cda8dbea3168866358e5ac486b
wallison <wallisongoncalves1105ibc@gmail.com>
2020-10-16 16:00:43 -0400

    auction_disput_item: association material

--- app/models/auction_disput_item.rb
+++ app/models/auction_disput_item.rb
@@ -4,6 +4,8 @@ class AuctionDisputItem < Compras::Model
   belongs_to :auction
   belongs_to :auction_item
 
+  has_one :material, :through => :auction_item
+
   has_enumeration_for :status, with: AuctionDisputItemStatus,
                       create_helpers: true, create_scopes: true
 
