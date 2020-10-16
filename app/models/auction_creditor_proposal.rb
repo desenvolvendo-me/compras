0e878ec23d4f11e5045c0ed9e9f618d8a6e659e6
wallison <wallisongoncalves1105ibc@gmail.com>
2020-10-16 15:55:30 -0400

    auction_creditor_proposal: add creditor

--- app/models/auction_creditor_proposal.rb
+++ app/models/auction_creditor_proposal.rb
@@ -1,17 +1,20 @@
 class AuctionCreditorProposal < Compras::Model
   attr_accessible :auction_id,:term,:impediment,:proposal_independent,:art_5,:art_93_pcd,:art_529_clt, :user_id,
                   :proposal_doc,:proposal_qualification_doc, :auction_creditor_proposal_items_attributes,
-                  :proposal_send_date, :qualification_send_date
+                  :proposal_send_date, :qualification_send_date, :creditor_id
 
   mount_uploader :proposal_doc, UnicoUploader
   mount_uploader :proposal_qualification_doc, UnicoUploader
 
   belongs_to :auction
   belongs_to :user
+  belongs_to :creditor
 
   has_many :auction_creditor_proposal_items, dependent: :destroy
 
   accepts_nested_attributes_for :auction_creditor_proposal_items, :allow_destroy => true
+
+  validates :creditor, presence: true
 
   before_save :set_proposal_send_date
   before_save :set_qualification_send_date
