adc1bee11c54249db53c40a48ddf51ce34520c5d
wallison <wallisongoncalves1105ibc@gmail.com>
2020-10-21 21:53:51 -0400

    auction_decorator: include number helper

--- app/decorators/auction_decorator.rb
+++ app/decorators/auction_decorator.rb
@@ -3,6 +3,7 @@ class AuctionDecorator
   include Decore::Proxy
   include Decore::Header
   include ActionView::Helpers::TranslationHelper
+  include ActionView::Helpers::NumberHelper
 
   def licitation_opened?
     bid_opening <= Date.today && bid_opening_time.try(:seconds_since_midnight) <= Time.now.try(:seconds_since_midnight)
