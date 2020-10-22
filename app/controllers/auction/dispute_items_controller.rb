a5ff9da80ffd0016b86881838808b94be76fa485
wallison <wallisongoncalves1105ibc@gmail.com>
2020-10-21 23:28:19 -0400

    dispute_items_controller: check if creditor has proposal

--- app/controllers/auction/dispute_items_controller.rb
+++ app/controllers/auction/dispute_items_controller.rb
@@ -3,6 +3,7 @@ class Auction::DisputeItemsController < Auction::BaseController
 
   before_filter :set_auction, except: :closed_items
   before_filter :set_auction_eager_load, only: :closed_items
+  before_filter :check_proposal, if: proc{ current_user.creditor? }
 
   def closed_items
     @creditor_proposal = @auction.creditor_proposal(current_user.authenticable.id)
@@ -16,5 +17,11 @@ class Auction::DisputeItemsController < Auction::BaseController
 
   def set_auction
     @auction = Auction.find(params[:auction_id])
+  end
+
+  def check_proposal
+    unless @auction.creditor_proposal(current_user.authenticable_id)
+      redirect_to auction_auctions_path, :alert => I18n.t('activerecord.auction.messages.must_have_registered_proposal')
+    end
   end
 end
