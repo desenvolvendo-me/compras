e6dd33c9306cc4dd6ebbebf49dfe73be63309620
andbri321 <andbri321@gmail.com>
2020-10-29 15:23:25 -0300

    [u] compras/form_builder, add metodo errors

--- lib/compras/form_builder.rb
+++ lib/compras/form_builder.rb
@@ -116,5 +116,15 @@ module Compras
 
       template.link_to value, options.delete(:href), options
     end
+
+    def errors(association)
+      # self.object.errors.select{ |key, value| key if key.to_s.include? "#{association}."}.transform_keys{ |key| key.to_s.sub("#{association}.",'') }      
+      # keys = self.object.errors.if_keep{ |key, value| key if key.to_s.include? "#{association}."}      
+      # keys.transform_keys{ |key| key.to_s.sub("#{association}.",'') }
+
+      keys = self.object.errors.keys.select{ |key| key if key.to_s.include? "#{association}."}
+      self.object.errors.to_hash.slice(*keys)
+    end
+
   end
 end
