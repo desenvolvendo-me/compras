cab811d0c1a1b899a5cd98be5bf2bdbbd30bfe5e
andbri321 <andbri321@gmail.com>
2020-11-05 19:12:38 -0300

    [u] contract_additive_report_decorator

--- app/decorators/contract_additive_report_decorator.rb
+++ app/decorators/contract_additive_report_decorator.rb
@@ -7,10 +7,13 @@ class ContractAdditiveReportDecorator
   def self.header_attributes
     [
       "#{ContractAdditive.human_attribute_name :number}",
-      "#{ContractAdditive.human_attribute_name :additive_kind}",
-      "#{ContractAdditive.human_attribute_name :additive_type}",
+      "#{Contract.human_attribute_name :creditor}",
+      "#{Contract.human_attribute_name :content}".mb_chars.upcase.sub!(' DO CONTRATO/ATA',''),
+      "#{Contract.human_attribute_name :contract_value}",
+      "#{Contract.human_attribute_name :modality_humanize}",
       "#{ContractAdditive.human_attribute_name :start_validity}",
-      "#{ContractAdditive.human_attribute_name :end_validity}"
+      "#{ContractAdditive.human_attribute_name :end_validity}",
+      "#{Contract.human_attribute_name :status}"
     ]
   end
 end
