2028db4d2ffdc5dd2d03296771f760f1efdb9ae0
andbri321 <andbri321@gmail.com>
2020-11-05 19:12:55 -0300

    [u] contract_report_decorator

--- app/decorators/contract_report_decorator.rb
+++ app/decorators/contract_report_decorator.rb
@@ -5,16 +5,15 @@ class ContractReportDecorator
 
 
   def self.header_attributes
-    [I18n.t('activemodel.attributes.contract_report.contract_number').mb_chars.upcase,
-     I18n.t('activemodel.attributes.contract_report.year').mb_chars.upcase,
-     I18n.t('activemodel.attributes.contract_report.creditor').mb_chars.upcase,
-     I18n.t('activemodel.attributes.contract_report.publication_date').mb_chars.upcase,
-     I18n.t('activemodel.attributes.contract_report.contract_validity').mb_chars.upcase,
-     I18n.t('activemodel.attributes.contract_report.content').mb_chars.upcase,
-     I18n.t('activemodel.attributes.contract_report.contract_value').mb_chars.upcase,
-     I18n.t('activemodel.attributes.contract_report.modality').mb_chars.upcase,
-     I18n.t('activemodel.attributes.contract_report.start_date').mb_chars.upcase,
-     I18n.t('activemodel.attributes.contract_report.end_date').mb_chars.upcase,
-     I18n.t('activemodel.attributes.contract_report.status').mb_chars.upcase]
+    [
+      (Contract.human_attribute_name :contract_number).mb_chars.upcase,
+      (Contract.human_attribute_name :creditor).mb_chars.upcase,
+      (Contract.human_attribute_name :content).mb_chars.upcase.sub!(' DO CONTRATO/ATA',''), 
+      (Contract.human_attribute_name :contract_value).mb_chars.upcase, 
+      (Contract.human_attribute_name :modality_humanize).mb_chars.upcase,
+      (Contract.human_attribute_name :start_date).mb_chars.upcase,
+      (Contract.human_attribute_name :end_date).mb_chars.upcase,
+      (Contract.human_attribute_name :status).mb_chars.upcase
+    ]
   end
 end
