89334f2523b00b25de7bc73cbd00befc2077c7bc
wallison <wallisongoncalves1105ibc@gmail.com>
2020-11-12 22:03:44 -0400

    fix

--- db/migrate/20201104030640_create_table_auction_conversations.rb
+++ db/migrate/20201104030640_create_table_auction_conversations.rb
@@ -7,7 +7,5 @@ class CreateTableAuctionConversations < ActiveRecord::Migration
 
     add_index :compras_auction_conversations, :auction_id, name: :index_c_auct_conver_on_auction_id
 
-    add_foreign_key :compras_auction_conversations, :compras_auctions, column: :auction_id,
-                    name: :c_auct_chat_auction_fk
   end
 end
