12ebce0645d25d818c2e1f26d02d363cc791e223
wallison-souza <wallisongoncalves105ibc@gmail.com>
2020-12-13 15:36:14 -0400

    refactor remove models  and controllers

--- app/controllers/auction/auction_items_controller.rb
+++
@@ -1,8 +0,0 @@
-class Auction::AuctionItemsController <  Auction::BaseController
-
-  def group_lot
-    items = AuctionItem.by_lot
-
-    render :json => items.to_json
-  end
-end
--- app/models/creditor_proposal_terms.rb
+++ app/models/creditor_proposal_terms.rb
@@ -1,7 +1,6 @@
-class AuctionCreditorProposal < Compras::Model
+class CreditorProposalTerms < Compras::Model
   attr_accessible :auction_id,:term,:impediment,:proposal_independent,:art_5,:art_93_pcd,:art_529_clt, :user_id,
-                  :proposal_doc,:proposal_qualification_doc, :auction_creditor_proposal_items_attributes,
-                  :proposal_send_date, :qualification_send_date, :creditor_id
+                  :proposal_doc,:proposal_qualification_doc, :proposal_send_date, :qualification_send_date, :creditor_id
 
   mount_uploader :proposal_doc, UnicoUploader
   mount_uploader :proposal_qualification_doc, UnicoUploader
