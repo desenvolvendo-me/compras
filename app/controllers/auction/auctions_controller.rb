bd087f7565b1594ed9f393a7235df96635ddc221
wallison-souza <wallisongoncalves105ibc@gmail.com>
2020-12-13 19:54:33 -0400

    save auction parte one

--- app/controllers/auction/auctions_controller.rb
+++ app/controllers/auction/auctions_controller.rb
@@ -11,22 +11,7 @@ class Auction::AuctionsController < Auction::BaseController
     render layout: "document"
   end
 
-  def new
-    object = build_resource
-    object.year = Time.now.year
 
-    super
-  end
-
-  def create
-    create! do |success, failure|
-      success.html { redirect_to collection_path }
-      failure.html { render :new }
-
-      success.js { render content_type: "text/json" }
-      failure.js { render :form_errors, content_type: "text/json", status: :unprocessable_entity }
-    end
-  end
 
   def update
     update! do |success, failure|
--- app/models/auction.rb
+++ app/models/auction.rb
@@ -1,48 +1,47 @@
 class Auction < Compras::Model
   attr_accessible :auction_type, :process_number, :year, :dispute_type, :zip_code,
-                  :judment_form, :covid_law, :purchase_value, :items_quantity, :group_items_attributes,
-                  :object_management, :employee_id, :items_attributes, :sensitive_value,
+                  :covid_law, :purchase_value, :items_quantity, :group_items_attributes,
+                  :object_management, :employee_id, :items_attributes, :sensitive_value, :licitation_process_id,
                   :decree_treatment, :document_edict, :disclosure_date, :responsible_dissemination_id, :notice_availability,
                   :proposal_delivery, :bid_opening, :internet_address, :city, :chat_activated, :status,
                   :neighborhood, :street, :telephone, :cell_phone, :user_id, :bid_opening_time,
                   :end_dispute_date, :end_dispute_time, :restart_dispute_date, :restart_dispute_time
 
-  attr_modal :process, :process_number, :proposal_delivery, :bid_opening, :object
+  attr_modal :process, :process_number, :proposal_delivery, :bid_opening, :description
 
   belongs_to :licitation_process
   belongs_to :employee
   belongs_to :user
   belongs_to :responsible_dissemination, class_name: "Employee"
-  has_many :items, class_name: "AuctionItem"
+
   has_many :group_items, class_name: "AuctionGroupItem"
-  has_many :creditor_proposals, class_name: "AuctionCreditorProposal"
-  has_many :auction_creditor_proposal_items, through: :creditor_proposals
+
   has_many :auction_support_teams
   has_many :bids, class_name: 'AuctionBid'
-  has_many :dispute_items, class_name: 'AuctionDisputeItem'
+
 
   has_one :appeal, class_name: 'AuctionAppeal'
   has_one :suspension, class_name: 'AuctionSuspension'
   has_one :conversation, class_name: 'AuctionConversation'
 
-  delegate :year, :process, to: :licitation_process
+  delegate :year, :process, :judgment_form, :description, to: :licitation_process, allow_nil: true
 
   mount_uploader :document_edict, UnicoUploader
 
   has_enumeration_for :auction_type, :with => AuctionType
   has_enumeration_for :dispute_type, :with => AuctionDisputeType
-  has_enumeration_for :judment_form, :with => AuctionJudmentForm
+  # has_enumeration_for :judment_form, :with => AuctionJudmentForm
   has_enumeration_for :status, :with => AuctionStatus
 
 
-  accepts_nested_attributes_for :items, :allow_destroy => true
   accepts_nested_attributes_for :group_items, :allow_destroy => true
   accepts_nested_attributes_for :auction_support_teams, :allow_destroy => true
 
-  validates :bid_opening, :bid_opening_time, presence: true
-  validates :zip_code, mask: "99999-999", allow_blank: true
-  validates :cell_phone, mask: "(99) 99999-9999", :allow_blank => true
-  validates :telephone, mask: "(99) 9999-9999", :allow_blank => true
+  # validates :bid_opening, :bid_opening_time, presence: true
+  # validates :zip_code, mask: "99999-999", allow_blank: true
+  # validates :cell_phone, mask: "(99) 99999-9999", :allow_blank => true
+  # validates :telephone, mask: "(99) 9999-9999", :allow_blank => true
+  validates :licitation_process_id, :dispute_type, :employee_id, presence: true
 
   before_save :clear_session_status
   after_create :create_conversation
@@ -55,7 +54,7 @@ class Auction < Compras::Model
 
 
   scope :term, lambda {|q|
-    join{ licitation_process }.where{ licitation_process.process.matches "%#{q}%" }
+    joins(:licitation_process).where{ licitation_process.process.matches "%#{q}%" }
   }
 
   def to_s
