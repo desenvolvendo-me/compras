cc8a102d1da581fff6d19cc4a17623fcc5c1cfb7
wallison <wallisongoncalves1105ibc@gmail.com>
2020-12-16 23:51:41 -0400

    fix proposals path

--- app/helpers/auction/auctions_helper.rb
+++ app/helpers/auction/auctions_helper.rb
@@ -9,20 +9,11 @@ module Auction::AuctionsHelper
   end
 
   def proposal_path resource
-    if current_user.try(:authenticable_type) == 'Employee'
-      return auctioneer_view_auction_auction_creditor_proposals_path(auction_id: resource.id)
-    end
+    return auction_providers_register_external_path unless current_user.present?
 
-    proposal = resource.creditor_proposals.where(creditor_id: current_user&.authenticable&.id).last
-    if proposal
-      route = edit_auction_auction_creditor_proposal_path(proposal.id)
-    elsif current_user.present?
-      route = new_auction_auction_creditor_proposal_path(auction_id: resource.id)
-    else
-      route = auction_providers_register_external_path
-    end
+    return auctioneer_view_auction_proposals_path(auction_id: resource.id) if current_user.employee?
 
-    route
+    edit_auction_proposal_path(resource.licitation_process_id, auction_id: resource)
   end
 
 
