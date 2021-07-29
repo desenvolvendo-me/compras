aec0b158c732181324ad6c89cda5b245ffa0bfde
carlosveigadev <carlosveiga1618@gmail.com>
2021-07-29 20:12:43 +0000

    create new department_number column to compras departments migration and add to model

--- app/models/department.rb
+++ app/models/department.rb
@@ -1,6 +1,10 @@
 class Department < Compras::Model
-  attr_accessible :description, :purchasing_unit_id,
-                  :department_people_attributes,:secretary_id
+  attr_accessible :description,
+                  :purchasing_unit_id,
+                  :department_people_attributes,
+                  :secretary_id,
+                  :department_number
+
   attr_modal :description, :secretary_id, :purchasing_unit_id
 
   belongs_to :purchasing_unit
---
+++ db/migrate/20210729195723_add_department_number_to_compras_departments.rb
@@ -0,0 +1,5 @@
+class AddDepartmentNumberToComprasDepartments < ActiveRecord::Migration
+  def change
+    add_column :compras_departments, :department_number, :integer
+  end
+end
