2882ea42d810fc2c93866a15fae5c4dd8490e647
wrack <carlosveiga1618@gmail.com>
2021-07-30 14:00:45 -0300

    add function to hide fuel field when medicine is selected, also make sure to disable uncheck fuel if user selected it before hiding with the selection of medicine.

---
+++ app/assets/javascripts/materials.js
@@ -0,0 +1,16 @@
+$(document).ready(function (){
+    whenMedicineDisableFuel();
+
+    function whenMedicineDisableFuel(){
+        var medicineCheckbox = document.getElementById('material_medicine');
+        var fuelCheckbox = document.querySelector('.material_combustible');
+        var fuelCheckboxValue = document.getElementById('material_combustible');
+
+        $(medicineCheckbox).change(function(){
+            if(medicineCheckbox.checked){
+                fuelCheckboxValue.checked = false;
+            }
+            $(fuelCheckbox).toggle();
+        });
+    }
+});
